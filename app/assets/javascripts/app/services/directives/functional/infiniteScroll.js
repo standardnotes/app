69b636f2b446ba277dc6cfeec8fcd816a4f6532b
Mo Bitar <me@bitar.io>
2017-01-31 10:59:50 -0600

    notes list scroll css fix

--- app/assets/javascripts/app/services/directives/functional/infiniteScroll.js
+++ app/assets/javascripts/app/services/directives/functional/infiniteScroll.js
@@ -2,8 +2,8 @@ angular.module('app.frontend').directive('infiniteScroll', [
 '$rootScope', '$window', '$timeout', function($rootScope, $window, $timeout) {
   return {
     link: function(scope, elem, attrs) {
-      elem.css('overflow-x', 'hidden');
-      elem.css('height', 'inherit');
+      // elem.css('overflow-x', 'hidden');
+      // elem.css('height', 'inherit');
 
       var offset = parseInt(attrs.threshold) || 0;
       var e = elem[0]
