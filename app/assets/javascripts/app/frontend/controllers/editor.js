f05ddb5cd1c794dc85d13b5dbf2d092c39f9e32f
Mo Bitar <me@bitar.io>
2017-03-10 14:31:17 -0600

    fixes change editor glitch

--- app/assets/javascripts/app/frontend/controllers/editor.js
+++ app/assets/javascripts/app/frontend/controllers/editor.js
@@ -51,6 +51,7 @@ angular.module('app.frontend')
     }.bind(this));
 
     this.setNote = function(note, oldNote) {
+      this.noteReady = false;
       var currentEditor = this.customEditor;
       this.customEditor = null;
       this.showExtensions = false;
@@ -60,6 +61,7 @@ angular.module('app.frontend')
       var setEditor = function(editor) {
         this.customEditor = editor;
         this.postNoteToExternalEditor();
+        this.noteReady = true;
       }.bind(this)
 
       var editor = this.editorForNote(note);
@@ -75,6 +77,7 @@ angular.module('app.frontend')
         }
       } else {
         this.customEditor = null;
+        this.noteReady = true;
       }
 
       if(note.safeText().length == 0 && note.dummy) {
