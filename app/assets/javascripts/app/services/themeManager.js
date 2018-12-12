cd8e94104e8364181e5ceaf750f9a0fde5cff161
Mo Bitar <me@bitar.io>
2018-12-12 14:46:24 -0600

    Cache active themes

--- app/assets/javascripts/app/services/themeManager.js
+++ app/assets/javascripts/app/services/themeManager.js
@@ -1,9 +1,27 @@
 class ThemeManager {
 
-  constructor(componentManager, desktopManager) {
+  constructor(componentManager, desktopManager, storageManager) {
     this.componentManager = componentManager;
+    this.storageManager = storageManager;
+    this.desktopManager = desktopManager;
+    this.activeThemes = [];
 
-    desktopManager.registerUpdateObserver((component) => {
+    ThemeManager.CachedThemesKey = "cachedThemes";
+
+    this.registerObservers();
+    this.activateCachedThemes();
+  }
+
+  activateCachedThemes() {
+    let cachedThemes = this.getCachedThemes();
+    let writeToCache = false;
+    for(var theme of cachedThemes) {
+      this.activateTheme(theme, writeToCache);
+    }
+  }
+
+  registerObservers() {
+    this.desktopManager.registerUpdateObserver((component) => {
       // Reload theme if active
       if(component.active && component.isTheme()) {
         this.deactivateTheme(component);
@@ -13,7 +31,7 @@ class ThemeManager {
       }
     })
 
-    componentManager.registerHandler({identifier: "themeManager", areas: ["themes"], activationHandler: (component) => {
+    this.componentManager.registerHandler({identifier: "themeManager", areas: ["themes"], activationHandler: (component) => {
       if(component.active) {
         this.activateTheme(component);
       } else {
@@ -33,9 +51,19 @@ class ThemeManager {
         this.componentManager.deactivateComponent(theme);
       }
     }
+
+    this.decacheThemes();
   }
 
-  activateTheme(theme) {
+  activateTheme(theme, writeToCache = true) {
+    if(_.find(this.activeThemes, {uuid: theme.uuid})) {
+      return;
+    }
+
+    console.log("Activating theme", theme.uuid);
+
+    this.activeThemes.push(theme);
+
     var url = this.componentManager.urlForComponent(theme);
     var link = document.createElement("link");
     link.href = url;
@@ -44,6 +72,10 @@ class ThemeManager {
     link.media = "screen,print";
     link.id = theme.uuid;
     document.getElementsByTagName("head")[0].appendChild(link);
+
+    if(writeToCache) {
+      this.cacheThemes();
+    }
   }
 
   deactivateTheme(theme) {
@@ -51,6 +83,37 @@ class ThemeManager {
     if(element) {
       element.disabled = true;
       element.parentNode.removeChild(element);
+    }
+
+    _.remove(this.activeThemes, {uuid: theme.uuid});
+
+    this.cacheThemes();
+  }
+
+  async cacheThemes() {
+    let mapped = await Promise.all(this.activeThemes.map(async (theme) => {
+      let transformer = new SFItemParams(theme);
+      let params = await transformer.paramsForLocalStorage();
+      return params;
+    }));
+    let data = JSON.stringify(mapped);
+    console.log("Caching themes", data);
+    return this.storageManager.setItem(ThemeManager.CachedThemesKey, data, StorageManager.Fixed);
+  }
+
+  async decacheThemes() {
+    return this.storageManager.removeItem(ThemeManager.CachedThemesKey, StorageManager.Fixed);
+  }
+
+  getCachedThemes() {
+    let cachedThemes = this.storageManager.getItemSync(ThemeManager.CachedThemesKey, StorageManager.Fixed);
+    if(cachedThemes) {
+      let parsed = JSON.parse(cachedThemes);
+      return parsed.map((theme) => {
+        return new SNTheme(theme);
+      });
+    } else {
+      return [];
     }
   }
 }
