117d414d6b9c0ecb7f93879230418a7fd168c625
Baptiste Grob <60621355+baptiste-grob@users.noreply.github.com>
2021-02-02 17:38:44 +0100

    fix: wording

--- app/assets/javascripts/components/NoAccountWarning.tsx
+++ app/assets/javascripts/components/NoAccountWarning.tsx
@@ -18,7 +18,7 @@ function NoAccountWarning({
     <div className="mt-5 p-5 rounded-md shadow-sm grid grid-template-cols-1fr">
       <h1 className="sk-h3 m-0 font-semibold">Data not backed up</h1>
       <p className="m-0 mt-1 col-start-1 col-end-3">
-        Sign in or register to back up your notes
+        Sign in or register to back up your notes.
       </p>
       <button
         className="sn-btn mt-3 col-start-1 col-end-3 justify-self-start"
