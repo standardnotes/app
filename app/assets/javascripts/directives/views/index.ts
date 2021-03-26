5cd3a7cb093e5332d1515c1e0d570aea06eb5567
Baptiste Grob <60621355+baptiste-grob@users.noreply.github.com>
2021-03-26 12:24:36 +0100

    fix: restore permissions modal

--- app/assets/javascripts/directives/views/index.ts
+++ app/assets/javascripts/directives/views/index.ts
@@ -7,6 +7,7 @@ export { InputModal } from './inputModal';
 export { MenuRow } from './menuRow';
 export { PanelResizer } from './panelResizer';
 export { PasswordWizard } from './passwordWizard';
+export { PermissionsModal } from './permissionsModal';
 export { RevisionPreviewModal } from './revisionPreviewModal';
 export { HistoryMenu } from './historyMenu';
 export { SyncResolutionMenu } from './syncResolutionMenu';
