52f8acfb0e9992bb5ec311a074ddb2bf08d91e07
Baptiste Grob <60621355+baptiste-grob@users.noreply.github.com>
2021-03-30 11:29:16 +0200

    fix: correctly check for pushState function
    fixes #523

--- app/assets/javascripts/routes.ts
+++ app/assets/javascripts/routes.ts
@@ -3,7 +3,7 @@ import { isDesktopApplication } from './utils';
 /* @ngInject */
 export function configRoutes($locationProvider: ng.ILocationProvider) {
   if (!isDesktopApplication()) {
-    if (window.history && window.history.pushState) {
+    if (typeof window?.history?.pushState === 'function') {
       $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
