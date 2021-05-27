d255057cbaa5a38f756fce77aa0daf50aa86e012
Antonella Sgarlatta <antonella@standardnotes.org>
2021-05-27 11:18:46 -0300

    styles: constraint tags width

--- app/assets/javascripts/components/NoteTags.tsx
+++ app/assets/javascripts/components/NoteTags.tsx
@@ -35,7 +35,9 @@ const NoteTags = observer(({ application, appState }: Props) => {
           }}
         >
           <Icon type="hashtag" className="sn-icon--small color-neutral mr-1" />
-          {tag.title}
+          <span className="max-w-md whitespace-nowrap overflow-hidden overflow-ellipsis">
+            {tag.title}
+          </span>
         </button>
       ))}
       <AutocompleteTagInput application={application} appState={appState} lastTagRef={lastTagRef} />
