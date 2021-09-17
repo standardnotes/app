a86411306e7d9c0811cf228bd4b7543904c0f552
Gorjan <mogi57@gmail.com>
2021-09-17 18:10:19 +0200

    feat: don't autocomplete passwords for DecoratedInput

--- app/assets/javascripts/components/DecoratedInput.tsx
+++ app/assets/javascripts/components/DecoratedInput.tsx
@@ -10,6 +10,7 @@ interface Props {
   text?: string;
   placeholder?: string;
   onChange?: (text: string) => void;
+  autocomplete?: boolean;
 }
 
 /**
@@ -24,6 +25,7 @@ export const DecoratedInput: FunctionalComponent<Props> = ({
   text,
   placeholder = '',
   onChange,
+  autocomplete = false,
 }) => {
   const baseClasses =
     'rounded py-1.5 px-3 text-input my-1 h-8 flex flex-row items-center';
@@ -53,6 +55,8 @@ export const DecoratedInput: FunctionalComponent<Props> = ({
           onChange={(e) =>
             onChange && onChange((e.target as HTMLInputElement).value)
           }
+          data-lpignore={type !== 'password' ? true : false}
+          autocomplete={autocomplete ? 'on' : 'off'}
         />
       </div>
       {right !== undefined && <div className="min-w-7 min-h-1" />}
