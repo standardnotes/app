cd46f1cfb58634f1378a1da5e6bb41324b79e2aa
Gorjan <mogi57@gmail.com>
2021-09-21 16:50:18 +0200

    feat: implement session logout success dialog

--- app/assets/javascripts/components/OtherSessionsLogout.tsx
+++ app/assets/javascripts/components/OtherSessionsLogout.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'preact/hooks';
+import { useRef, useState } from 'preact/hooks';
 import {
   AlertDialog,
   AlertDialogDescription,
@@ -7,6 +7,7 @@ import {
 import { WebApplication } from '@/ui_models/application';
 import { AppState } from '@/ui_models/app_state';
 import { observer } from 'mobx-react-lite';
+import { FunctionComponent } from 'preact';
 
 type Props = {
   application: WebApplication;
@@ -20,16 +21,50 @@ export const OtherSessionsLogoutContainer = observer((props: Props) => {
   return <ConfirmOtherSessionsLogout {...props} />;
 });
 
+const SessionsLogoutSuccess: FunctionComponent<{ closeDialog: () => void }> = ({ closeDialog }) => {
+  const cancelRevokeRef = useRef<HTMLButtonElement>();
+  return <AlertDialog
+    onDismiss={() => {
+      closeDialog();
+    }}
+    leastDestructiveRef={cancelRevokeRef}
+  >
+    <div className="sk-modal-content">
+      <div className="sn-component">
+        <div className="sk-panel">
+          <div className="sk-panel-content">
+            <div className="sk-panel-section">
+              <AlertDialogDescription className="sk-panel-row mt-1">
+                You have successfully revoked your sessions from other devices.
+              </AlertDialogDescription>
+              <div className="flex my-1 gap-2">
+                <button
+                  className="sn-button small info mt-4 sk-label"
+                  ref={cancelRevokeRef}
+                  onClick={() => closeDialog()}
+                >
+                  <span>Finish</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </AlertDialog>;
+};
+
 const ConfirmOtherSessionsLogout = observer(
   ({ application, appState }: Props) => {
+    const [hasLoggedOut, setHasLoggedOut] = useState(false);
 
     const cancelRef = useRef<HTMLButtonElement>();
-    function close() {
+    function closeDialog() {
       appState.accountMenu.setOtherSessionsLogout(false);
     }
 
-    return (
-      <AlertDialog onDismiss={close} leastDestructiveRef={cancelRef}>
+    return hasLoggedOut ? <SessionsLogoutSuccess closeDialog={closeDialog} /> :
+      <AlertDialog onDismiss={closeDialog} leastDestructiveRef={cancelRef}>
         <div className="sk-modal-content">
           <div className="sn-component">
             <div className="sk-panel">
@@ -40,16 +75,16 @@ const ConfirmOtherSessionsLogout = observer(
                   </AlertDialogLabel>
                   <AlertDialogDescription className="sk-panel-row">
                     <p className="color-foreground">
-                      The associated apps will be signed out and all data removed from the
-                      device when it is next launched. You can sign back in on that device at
-                      any time.
+                      This action will sign out all other devices signed into your account,
+                      and remove your data from those devices when they next regain connection
+                      to the internet. You may sign back in on those devices at any time.
                     </p>
                   </AlertDialogDescription>
                   <div className="flex my-1 mt-4">
                     <button
                       className="sn-button small neutral"
                       ref={cancelRef}
-                      onClick={close}
+                      onClick={closeDialog}
                     >
                       Cancel
                     </button>
@@ -57,10 +92,10 @@ const ConfirmOtherSessionsLogout = observer(
                       className="sn-button small danger ml-2"
                       onClick={() => {
                         application.revokeAllOtherSessions();
-                        close();
+                        setHasLoggedOut(true);
                       }}
                     >
-                      Sign Out
+                      End Sessions
                     </button>
                   </div>
                 </div>
@@ -68,7 +103,6 @@ const ConfirmOtherSessionsLogout = observer(
             </div>
           </div>
         </div>
-      </AlertDialog>
-    );
+      </AlertDialog>;
   }
 );
