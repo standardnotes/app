31bb258f250949f4c042fe025a186a0521735bc6
Gorjan <mogi57@gmail.com>
2021-09-21 18:55:23 +0200

    fix: make OtherSessionsLogout easier to read

--- app/assets/javascripts/components/OtherSessionsLogout.tsx
+++ app/assets/javascripts/components/OtherSessionsLogout.tsx
@@ -29,50 +29,52 @@ const ConfirmOtherSessionsLogout = observer(
       appState.accountMenu.setOtherSessionsLogout(false);
     }
 
-    return <AlertDialog onDismiss={closeDialog} leastDestructiveRef={cancelRef}>
-      <div className="sk-modal-content">
-        <div className="sn-component">
-          <div className="sk-panel">
-            <div className="sk-panel-content">
-              <div className="sk-panel-section">
-                <AlertDialogLabel className="sk-h3 sk-panel-section-title capitalize">
-                  End all other sessions?
-                </AlertDialogLabel>
-                <AlertDialogDescription className="sk-panel-row">
-                  <p className="color-foreground">
-                    This action will sign out all other devices signed into your account,
-                    and remove your data from those devices when they next regain connection
-                    to the internet. You may sign back in on those devices at any time.
-                  </p>
-                </AlertDialogDescription>
-                <div className="flex my-1 mt-4">
-                  <button
-                    className="sn-button small neutral"
-                    ref={cancelRef}
-                    onClick={closeDialog}
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    className="sn-button small danger ml-2"
-                    onClick={() => {
-                      application.revokeAllOtherSessions();
-                      closeDialog();
-                      application.alertService.alert(
-                        "You have successfully revoked your sessions from other devices.",
-                        undefined,
-                        "Finish"
-                      );
-                    }}
-                  >
-                    End Sessions
-                  </button>
+    return (
+      <AlertDialog onDismiss={closeDialog} leastDestructiveRef={cancelRef}>
+        <div className="sk-modal-content">
+          <div className="sn-component">
+            <div className="sk-panel">
+              <div className="sk-panel-content">
+                <div className="sk-panel-section">
+                  <AlertDialogLabel className="sk-h3 sk-panel-section-title capitalize">
+                    End all other sessions?
+                  </AlertDialogLabel>
+                  <AlertDialogDescription className="sk-panel-row">
+                    <p className="color-foreground">
+                      This action will sign out all other devices signed into your account,
+                      and remove your data from those devices when they next regain connection
+                      to the internet. You may sign back in on those devices at any time.
+                    </p>
+                  </AlertDialogDescription>
+                  <div className="flex my-1 mt-4">
+                    <button
+                      className="sn-button small neutral"
+                      ref={cancelRef}
+                      onClick={closeDialog}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      className="sn-button small danger ml-2"
+                      onClick={() => {
+                        application.revokeAllOtherSessions();
+                        closeDialog();
+                        application.alertService.alert(
+                          "You have successfully revoked your sessions from other devices.",
+                          undefined,
+                          "Finish"
+                        );
+                      }}
+                    >
+                      End Sessions
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </AlertDialog>;
+      </AlertDialog>
+    );
   }
 );
