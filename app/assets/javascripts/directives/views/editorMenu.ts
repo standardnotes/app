1a92fffcd46941de944f8416060c27e6624a157d
Aman Harwara <amanharwara@protonmail.com>
2021-10-01 11:46:56 +0530

    feat: Remove Set Default & Undefault buttons

--- app/assets/javascripts/directives/views/editorMenu.ts
+++ app/assets/javascripts/directives/views/editorMenu.ts
@@ -4,34 +4,30 @@ import { SNComponent, SNItem, ComponentArea } from '@standardnotes/snjs';
 import { isDesktopApplication } from '@/utils';
 import template from '%/directives/editor-menu.pug';
 import { PureViewCtrl } from '@Views/abstract/pure_view_ctrl';
-import { ComponentMutator } from '@standardnotes/snjs';
 
 interface EditorMenuScope {
-  callback: (component: SNComponent) => void
-  selectedEditorUuid: string
-  currentItem: SNItem
-  application: WebApplication
+  callback: (component: SNComponent) => void;
+  selectedEditorUuid: string;
+  currentItem: SNItem;
+  application: WebApplication;
 }
 
 class EditorMenuCtrl extends PureViewCtrl implements EditorMenuScope {
-
-  callback!: () => (component: SNComponent) => void
-  selectedEditorUuid!: string
-  currentItem!: SNItem
-  application!: WebApplication
+  callback!: () => (component: SNComponent) => void;
+  selectedEditorUuid!: string;
+  currentItem!: SNItem;
+  application!: WebApplication;
 
   /* @ngInject */
-  constructor(
-    $timeout: ng.ITimeoutService,
-  ) {
+  constructor($timeout: ng.ITimeoutService) {
     super($timeout);
     this.state = {
-      isDesktop: isDesktopApplication()
+      isDesktop: isDesktopApplication(),
     };
   }
 
   public isEditorSelected(editor: SNComponent) {
-    if(!this.selectedEditorUuid) {
+    if (!this.selectedEditorUuid) {
       return false;
     }
     return this.selectedEditorUuid === editor.uuid;
@@ -43,14 +39,15 @@ class EditorMenuCtrl extends PureViewCtrl implements EditorMenuScope {
 
   $onInit() {
     super.$onInit();
-    const editors = this.application.componentManager!.componentsForArea(ComponentArea.Editor)
+    const editors = this.application
+      .componentManager!.componentsForArea(ComponentArea.Editor)
       .sort((a, b) => {
         return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
       });
     const defaultEditor = editors.filter((e) => e.isDefaultEditor())[0];
     this.setState({
       editors: editors,
-      defaultEditor: defaultEditor
+      defaultEditor: defaultEditor,
     });
   }
 
@@ -67,45 +64,8 @@ class EditorMenuCtrl extends PureViewCtrl implements EditorMenuScope {
     });
   }
 
-  toggleDefaultForEditor(editor: SNComponent) {
-    if (this.state.defaultEditor === editor) {
-      this.removeEditorDefault(editor);
-    } else {
-      this.makeEditorDefault(editor);
-    }
-  }
-
   offlineAvailableForComponent(component: SNComponent) {
     return component.local_url && this.state.isDesktop;
-  }
-
-  makeEditorDefault(component: SNComponent) {
-    const currentDefault = this.application.componentManager!
-      .componentsForArea(ComponentArea.Editor)
-      .filter((e) => e.isDefaultEditor())[0];
-    if (currentDefault) {
-      this.application.changeItem(currentDefault.uuid, (m) => {
-        const mutator = m as ComponentMutator;
-        mutator.defaultEditor = false;
-      });
-    }
-    this.application.changeAndSaveItem(component.uuid, (m) => {
-      const mutator = m as ComponentMutator;
-      mutator.defaultEditor = true;
-    });
-    this.setState({
-      defaultEditor: component
-    });
-  }
-
-  removeEditorDefault(component: SNComponent) {
-    this.application.changeAndSaveItem(component.uuid, (m) => {
-      const mutator = m as ComponentMutator;
-      mutator.defaultEditor = false;
-    });
-    this.setState({
-      defaultEditor: null
-    });
   }
 }
 
@@ -121,7 +81,7 @@ export class EditorMenu extends WebDirective {
       callback: '&',
       selectedEditorUuid: '=',
       currentItem: '=',
-      application: '='
+      application: '=',
     };
   }
 }
