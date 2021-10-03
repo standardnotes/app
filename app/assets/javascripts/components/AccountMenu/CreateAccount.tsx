a4292f52cab4349bc115da32b4d7d504c193fed6
Aman Harwara <amanharwara@protonmail.com>
2021-10-03 20:33:52 +0530

    feat: Implement new design for Create Account

--- app/assets/javascripts/components/AccountMenu/CreateAccount.tsx
+++ app/assets/javascripts/components/AccountMenu/CreateAccount.tsx
@@ -2,10 +2,12 @@ import { WebApplication } from '@/ui_models/application';
 import { AppState } from '@/ui_models/app_state';
 import { observer } from 'mobx-react-lite';
 import { FunctionComponent } from 'preact';
-import { StateUpdater, useState } from 'preact/hooks';
+import { StateUpdater, useEffect, useRef, useState } from 'preact/hooks';
 import { AccountMenuPane } from '.';
 import { Button } from '../Button';
+import { Checkbox } from '../Checkbox';
 import { Icon } from '../Icon';
+import { InputWithIcon } from '../InputWithIcon';
 
 type Props = {
   appState: AppState;
@@ -15,7 +17,46 @@ type Props = {
 
 export const CreateAccount: FunctionComponent<Props> = observer(
   ({ application, appState, setMenuPane }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [syncServer, setSyncServer] = useState(
+      'https://api.standardnotes.com'
+    );
+    const [enableCustomServer, setEnableCustomServer] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
     const [showAdvanced, setShowAdvanced] = useState(false);
+
+    const emailInputRef = useRef<HTMLInputElement>();
+    const passwordInputRef = useRef<HTMLInputElement>();
+
+    useEffect(() => {
+      if (emailInputRef?.current) {
+        emailInputRef.current.focus();
+      }
+    }, []);
+
+    const handleEmailChange = (e: Event) => {
+      if (e.target instanceof HTMLInputElement) {
+        setEmail(e.target.value);
+      }
+    };
+
+    const handlePasswordChange = (e: Event) => {
+      if (e.target instanceof HTMLInputElement) {
+        setPassword(e.target.value);
+      }
+    };
+
+    const handleCustomServerChange = () => {
+      setEnableCustomServer(!enableCustomServer);
+    };
+
+    const handleSyncServerChange = (e: Event) => {
+      if (e.target instanceof HTMLInputElement) {
+        setSyncServer(e.target.value);
+        application.setCustomHost(e.target.value);
+      }
+    };
 
     return (
       <>
@@ -29,24 +70,36 @@ export const CreateAccount: FunctionComponent<Props> = observer(
           <div className="sn-account-menu-headline">Create account</div>
         </div>
         <div className="px-3 mb-1">
-          <input
-            className="sk-input contrast"
-            type="email"
-            name="email"
+          <InputWithIcon
+            className="mb-2"
+            icon="email"
+            inputType="email"
             placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+            ref={emailInputRef}
           />
-          <input
-            className="sk-input contrast"
-            type="password"
-            name="password"
+          <InputWithIcon
+            className="mb-2"
+            icon="password"
+            inputType={showPassword ? 'text' : 'password'}
             placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+            toggle={{
+              toggleOnIcon: 'eye',
+              toggleOffIcon: 'eye',
+              title: 'Show password',
+              onClick: (toggled) => setShowPassword(toggled),
+            }}
+            ref={passwordInputRef}
           />
           <Button
             className="btn-w-full mt-1"
             label="Next"
             type="primary"
             onClick={() => {
-              //
+              /** @TODO */
             }}
           />
         </div>
@@ -67,23 +120,19 @@ export const CreateAccount: FunctionComponent<Props> = observer(
         </button>
         {showAdvanced ? (
           <div className="px-3 mt-2">
-            <label
-              htmlFor="custom-sync-server"
-              className="flex item-center mb-1"
-            >
-              <input
-                className="mr-2"
-                type="checkbox"
-                name="custom-sync-server"
-                id="custom-sync-server"
-              />
-              Custom sync server
-            </label>
-            <input
-              className="sk-input contrast"
-              type="text"
-              name="sync-server"
-              placeholder="https://sync.standardnotes.org"
+            <Checkbox
+              name="custom-sync-server"
+              label="Custom sync server"
+              checked={enableCustomServer}
+              onChange={handleCustomServerChange}
+            />
+            <InputWithIcon
+              inputType="text"
+              icon="server"
+              placeholder="https://api.standardnotes.com"
+              value={syncServer}
+              onChange={handleSyncServerChange}
+              disabled={!enableCustomServer}
             />
           </div>
         ) : null}
