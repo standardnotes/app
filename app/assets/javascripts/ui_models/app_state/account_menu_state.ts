cfb33ea612da74b7f0cd7f768cf3332886e01c21
Aman Harwara <amanharwara@protonmail.com>
2021-10-10 19:25:18 +0530

    refactor: Use action for changing value

--- app/assets/javascripts/ui_models/app_state/account_menu_state.ts
+++ app/assets/javascripts/ui_models/app_state/account_menu_state.ts
@@ -52,6 +52,7 @@ export class AccountMenuState {
       shouldAnimateCloseMenu: observable,
 
       setShow: action,
+      setShouldAnimateClose: action,
       toggleShow: action,
       setSigningOut: action,
       setIsEncryptionEnabled: action,
@@ -95,11 +96,15 @@ export class AccountMenuState {
     this.show = show;
   };
 
+  setShouldAnimateClose = (shouldAnimateCloseMenu: boolean): void => {
+    this.shouldAnimateCloseMenu = shouldAnimateCloseMenu;
+  };
+
   closeAccountMenu = (): void => {
-    this.shouldAnimateCloseMenu = true;
+    this.setShouldAnimateClose(true);
     setTimeout(() => {
       this.setShow(false);
-      this.shouldAnimateCloseMenu = false;
+      this.setShouldAnimateClose(false);
       this.setCurrentPane(AccountMenuPane.GeneralMenu);
     }, 150);
   };
