36608487ce53169506b3297536a699adc0da7537
Karol Sójko <karolsojko@protonmail.com>
2021-10-26 13:54:13 +0200

    feat: upgrade to latest active node LTS version

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,9 +17,9 @@ case "$COMMAND" in
     echo "Prestart Step 2/5 - Cleaning assets"
     bundle exec rails assets:clobber
     echo "Prestart Step 3/5 - Installing dependencies"
-    npm install
+    yarn install --pure-lockfile
     echo "Prestart Step 4/5 - Creating Webpack bundle"
-    npm run bundle
+    yarn run bundle
     echo "Prestart Step 5/5 - Compiling assets"
     bundle exec rails assets:precompile
     echo "Starting Server..."
