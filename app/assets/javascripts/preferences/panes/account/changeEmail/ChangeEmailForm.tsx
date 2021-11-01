d87ffa7b7b0434aec5d45050ee2651b1a3edb100
Aman Harwara <amanharwara@protonmail.com>
2021-11-01 16:05:49 +0530

    feat: Update "Change Email" modal design

--- app/assets/javascripts/preferences/panes/account/changeEmail/ChangeEmailForm.tsx
+++ app/assets/javascripts/preferences/panes/account/changeEmail/ChangeEmailForm.tsx
@@ -1,37 +1,47 @@
-import { DecoratedInput } from '@/components/DecoratedInput';
 import { StateUpdater } from 'preact/hooks';
 import { FunctionalComponent } from 'preact';
-import { HtmlInputTypes } from '@/enums';
 
 type Props = {
-  setNewEmail: StateUpdater<string>
-  setCurrentPassword: StateUpdater<string>
-}
+  setNewEmail: StateUpdater<string>;
+  setCurrentPassword: StateUpdater<string>;
+};
+
+const labelClassName = `block mb-1`;
+
+const inputClassName = `w-full p-2 border-1 border-solid border-neutral bg-default rounded-0.5 focus:ring-info focus:outline-none`;
+
 export const ChangeEmailForm: FunctionalComponent<Props> = ({
   setNewEmail,
-  setCurrentPassword
+  setCurrentPassword,
 }) => {
   return (
-    (
-      <>
-        <div className={'mt-2 mb-3'}>
-          <DecoratedInput
-            onChange={(newEmail) => {
-              setNewEmail(newEmail);
-            }}
-            placeholder={'New Email'}
-          />
-        </div>
-        <div className={'mt-2 mb-3'}>
-          <DecoratedInput
-            type={HtmlInputTypes.Password}
-            onChange={(currentPassword) => {
-              setCurrentPassword(currentPassword);
-            }}
-            placeholder={'Current Password'}
-          />
-        </div>
-      </>
-    )
+    <div className="w-full flex flex-col">
+      <div className="mt-2 mb-3">
+        <label className={labelClassName} htmlFor="change-email-email-input">
+          New Email:
+        </label>
+        <input
+          id="change-email-email-input"
+          className={inputClassName}
+          type="email"
+          onChange={({ target }) => {
+            setNewEmail((target as HTMLInputElement).value);
+          }}
+        />
+      </div>
+      <div className="mb-2">
+        <label className={labelClassName} htmlFor="change-email-password-input">
+          Current Password:
+        </label>
+        <input
+          id="change-email-password-input"
+          className={inputClassName}
+          type="password"
+          onChange={({ target }) => {
+            setCurrentPassword((target as HTMLInputElement).value);
+          }}
+        />
+      </div>
+    </div>
   );
 };
