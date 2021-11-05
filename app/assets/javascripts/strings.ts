261bb65a85769c93ff95d613683c447549500dc2
Johnny A <5891646+johnny243@users.noreply.github.com>
2021-11-05 11:31:23 -0400

    fix: show alert when importing a zip file as a backup (#720)
    * fix: show alert when importing a zip file as a backup
    
    * Update app/assets/javascripts/strings.ts
    
    Co-authored-by: Mo <mo@standardnotes.org>
    
    Co-authored-by: Johnny Almonte <johnny243@users.noreply.github.com>
    Co-authored-by: Mo <mo@standardnotes.org>

--- app/assets/javascripts/strings.ts
+++ app/assets/javascripts/strings.ts
@@ -80,6 +80,8 @@ export const STRING_GENERATING_LOGIN_KEYS = 'Generating Login Keys...';
 export const STRING_GENERATING_REGISTER_KEYS = 'Generating Account Keys...';
 export const STRING_INVALID_IMPORT_FILE =
   'Unable to open file. Ensure it is a proper JSON file and try again.';
+export const STRING_IMPORTING_ZIP_FILE =
+  'The file you selected is not a valid backup file. Please extract the contents of the zip file, then upload the contained .txt file.';
 export function StringImportError(errorCount: number) {
   return `Import complete. ${errorCount} items were not imported because there was an error decrypting them. Make sure the password is correct and try again.`;
 }
