11b97caee3d8da544895390b3097edc40c534389
Aman Harwara <amanharwara@protonmail.com>
2021-11-05 20:36:31 +0530

    fix: Change bg-neutral to bg-border

--- app/assets/javascripts/components/shared/HorizontalSeparator.tsx
+++ app/assets/javascripts/components/shared/HorizontalSeparator.tsx
@@ -2,9 +2,9 @@ import { FunctionalComponent } from 'preact';
 
 type Props = {
   classes?: string;
-}
+};
 export const HorizontalSeparator: FunctionalComponent<Props> = ({
-  classes = ''
+  classes = '',
 }) => {
-  return <hr className={`h-1px w-full bg-neutral no-border ${classes}`} />;
+  return <hr className={`h-1px w-full bg-border no-border ${classes}`} />;
 };
