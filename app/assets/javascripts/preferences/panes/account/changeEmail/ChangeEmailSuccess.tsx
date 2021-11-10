574ed885d59da59e50146917b8f0830ebcf0c786
vardanhakobyan <vardan_live@live.com>
2021-11-10 20:01:08 +0400

    fix: correct top styling for title

--- app/assets/javascripts/preferences/panes/account/changeEmail/ChangeEmailSuccess.tsx
+++ app/assets/javascripts/preferences/panes/account/changeEmail/ChangeEmailSuccess.tsx
@@ -3,7 +3,7 @@ import { FunctionalComponent } from 'preact';
 export const ChangeEmailSuccess: FunctionalComponent = () => {
   return (
     <div>
-      <div className={'sk-label sk-bold info'}>Your email has been successfully changed.</div>
+      <div className={'sk-label sk-bold info mt-2'}>Your email has been successfully changed.</div>
       <p className={'sk-p'}>
         Please ensure you are running the latest version of Standard Notes on all platforms to ensure maximum compatibility.
       </p>
