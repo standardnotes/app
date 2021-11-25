0b80ea8f575578d6f5e9f0a4f3f978abcd3c2383
Laurent Senta <laurent@singulargarden.com>
2021-11-25 09:40:11 +0100

    refactor: extract TagsListItem and simplify hooks

--- app/assets/javascripts/components/TagsList.tsx
+++ app/assets/javascripts/components/TagsList.tsx
@@ -4,160 +4,15 @@ import { WebApplication } from '@/ui_models/application';
 import { AppState } from '@/ui_models/app_state';
 import { SNTag, TagMutator } from '@standardnotes/snjs';
 import { observer } from 'mobx-react-lite';
-import { FunctionComponent, JSX } from 'preact';
-import { useCallback, useEffect, useRef, useState } from 'preact/hooks';
+import { FunctionComponent } from 'preact';
+import { useCallback } from 'preact/hooks';
+import { TagsListItem } from './TagsListItem';
 import { toDirective } from './utils';
 
 type Props = {
     application: WebApplication;
     appState: AppState;
 };
-
-type ItemProps = {
-    tag: SNTag;
-    selectTag: (tag: SNTag) => void;
-    removeTag: (tag: SNTag) => void;
-    saveTag: (tag: SNTag, newTitle: string) => void;
-    appState: TagsListState;
-}
-
-type TagsListState = {
-    readonly selectedTag: SNTag | undefined;
-    editingTag: SNTag | undefined;
-}
-
-const TagItem: FunctionComponent<ItemProps> = observer(
-    ({ tag, selectTag, saveTag, removeTag, appState }) => {
-        const [title, setTitle] = useState(tag.title);
-        const inputRef = useRef<HTMLInputElement>(null);
-
-        const isSelected = appState.selectedTag === tag;
-        const isEditing = appState.editingTag === tag;
-        const noteCounts = tag.noteCount; // TODO: Check that the SNTag is an observable.
-
-        useEffect(() => {
-            setTitle(tag.title);
-        }, [tag]);
-
-        const selectCurrentTag = useCallback(() => {
-            selectTag(tag);
-        }, [selectTag, tag]);
-
-        const onBlur = useCallback(() => {
-            appState.editingTag = undefined;
-            saveTag(tag, title);
-        }, [appState, tag, saveTag, title]);
-
-        const onInput = useCallback((e: JSX.TargetedEvent<HTMLInputElement>) => {
-            const value = (e.target as HTMLInputElement).value;
-            setTitle(value);
-        }, [setTitle]);
-
-        const onKeyUp = useCallback((e: KeyboardEvent) => {
-            if (e.code === 'Enter') {
-                inputRef.current?.blur();
-                e.preventDefault();
-            }
-        }, [inputRef]);
-
-        useEffect(() => {
-            // NOTE(laurent): before we had 2 ways to focus,
-            // sn-autofocus and the `document.getElementById('tag-' + tag.uuid)!.focus();` in tags_view.ts l373
-            // I am remplacing this by a single focus trigger here.
-            // TODO: Check I am not missing any behavior
-            if (isEditing) {
-                inputRef.current?.focus();
-            }
-        }, [isEditing]);
-
-        const onClickRename = useCallback(() => {
-            appState.editingTag = tag;
-        }, [appState, tag]);
-
-        const onClickSave = useCallback(() => {
-            // NOTE(laurent): I use an implicit blur which trigger the main save code path.
-            inputRef.current?.blur();
-        }, []);
-
-        const onClickDelete = useCallback(() => {
-            removeTag(tag);
-        }, [removeTag, tag]);
-
-        return <div className={`tag ${isSelected ? 'selected' : ''}`} onClick={selectCurrentTag}>
-            {
-                // TODO: test this case.
-                !tag.errorDecrypting
-                    ? <div className="tag-info">
-                        <div className="tag-icon">
-                            #
-                        </div>
-                        <input className={`title ${isEditing ? 'editing' : ''}`}
-                            id={`react-tag-${tag.uuid}`}
-                            onBlur={onBlur}
-                            onInput={onInput}
-                            onClick={selectCurrentTag}
-                            value={title}
-                            onKeyUp={onKeyUp}
-                            spellCheck={false}
-                            ref={inputRef}
-                        />
-                        <div className="count">
-                            {noteCounts}
-                        </div>
-                    </div>
-                    : null
-            }
-            {
-                // TODO: how to test this?
-                tag.conflictOf
-                    ? <div className="danger small-text font-bold">
-                        Conflicted Copy {tag.conflictOf}
-                    </div>
-                    : null
-            }
-            {
-                // TODO: how to test this?
-                (tag.errorDecrypting && !tag.waitingForKey)
-                    ? <div className="danger small-text font-bold">
-                        Missing Keys
-                    </div>
-                    : null
-            }
-            {
-                // TODO: how to test this?
-                (tag.errorDecrypting && tag.waitingForKey)
-                    ? <div className="info small-text font-bold">
-                        Waiting For Keys
-                    </div>
-                    : null
-            }
-            {
-                isSelected
-                    ? <div className="menu">
-                        {
-                            !isEditing
-                                ? <a className="item" onClick={onClickRename}>
-                                    Rename
-                                </a>
-                                : null
-                        }
-                        {
-                            isEditing
-                                ? <a className="item" onClick={onClickSave}>
-                                    Save
-                                </a>
-                                : null
-                        }
-                        <a className="item" onClick={onClickDelete}>
-                            Delete
-                        </a>
-                    </div>
-                    : null
-            }
-
-        </div>;
-    }
-);
 
 const withTemplate = (template: SNTag | undefined, tags: SNTag[]): SNTag[] => {
     if (!template) {
@@ -169,7 +24,12 @@ const withTemplate = (template: SNTag | undefined, tags: SNTag[]): SNTag[] => {
 
 export const TagsList: FunctionComponent<Props> = observer(
     ({ application, appState }) => {
+        const templateTag = appState.templateTag;
+        const allTags = withTemplate(templateTag, appState.tags.tags);
+
         const selectTag = useCallback((tag: SNTag) => {
+            // TODO(laurent): is this something we could move to the app state?
+            // I am not familiar how conflict are managed, but it looks like we just "ignore" the conflict on select.
             if (tag.conflictOf) {
                 application.changeAndSaveItem(tag.uuid, (mutator) => {
                     mutator.conflictOf = undefined;
@@ -178,7 +38,7 @@ export const TagsList: FunctionComponent<Props> = observer(
             appState.setSelectedTag(tag);
         }, [application, appState]);
 
-        const saveTag = async (tag: SNTag, newTitle: string) => {
+        const saveTag = useCallback(async (tag: SNTag, newTitle: string) => {
             const templateTag = appState.templateTag;
 
             const hasEmptyTitle = newTitle.length === 0;
@@ -186,55 +46,40 @@ export const TagsList: FunctionComponent<Props> = observer(
             const isTemplateChange = templateTag && tag.uuid === templateTag.uuid;
             const hasDuplicatedTitle = !!application.findTagByTitle(newTitle);
 
+            // End the edition mode
+            appState.templateTag = undefined;
+            appState.editingTag = undefined;
+
+            // Abort early if the user inputs invalid data
+            if (hasEmptyTitle || hasNotChangedTitle) {
+                return;
+            }
+
+            if (hasDuplicatedTitle) {
+                application.alertService?.alert(
+                    "A tag with this name already exists."
+                );
+                return;
+            }
+
+            // Update the store
             if (isTemplateChange) {
-                appState.templateTag = undefined;
-                appState.editingTag = undefined;
-
-                if (hasEmptyTitle) {
-                    return;
-                }
-
-                // saveNewTag
-                if (hasDuplicatedTitle) {
-                    application.alertService?.alert(
-                        "A tag with this name already exists."
-                    );
-                    return;
-                }
-
                 const insertedTag = await application.insertItem(templateTag);
                 const changedTag = await application.changeItem<TagMutator>(insertedTag.uuid, (m) => {
                     m.title = newTitle;
                 });
+
                 selectTag(changedTag as SNTag);
 
-                if (!changedTag) {
-                    throw new Error('The tag could not be updated');
-                }
-
-                await application.saveItem(changedTag.uuid);
-                // END
+                await application.saveItem(insertedTag.uuid);
             } else {
-                appState.editingTag = undefined;
-
-                if (hasEmptyTitle || hasNotChangedTitle) {
-                    return;
-                }
-
-                if (hasDuplicatedTitle) {
-                    application.alertService?.alert(
-                        "A tag with this name already exists."
-                    );
-                    return;
-                }
-
                 await application.changeAndSaveItem<TagMutator>(tag.uuid, (mutator) => {
                     mutator.title = newTitle;
                 });
             }
-        };
+        }, [appState, application, selectTag]);
 
-        const removeTag = async (tag: SNTag) => {
+        const removeTag = useCallback(async (tag: SNTag) => {
             if (await confirmDialog({
                 text: STRING_DELETE_TAG,
                 confirmButtonStyle: 'danger'
@@ -242,21 +87,17 @@ export const TagsList: FunctionComponent<Props> = observer(
                 application.deleteItem(tag);
                 selectTag(allTags[0]);
             }
-        };
-
-        const templateTag = appState.templateTag;
-        const allTags = withTemplate(templateTag, appState.tags.tags);
+        }, [allTags, application, selectTag]);
 
         return (
             <>
-                {(allTags.length === 0 && !templateTag)
+                {allTags.length === 0
                     ? <div className="no-tags-placeholder">
                         | No tags. Create one using the add button above.
                     </div>
                     : <>
                         {allTags.map(tag => {
-                            console.log(appState.selectedTag?.uuid);
-                            return <TagItem
+                            return <TagsListItem
                                 key={tag.uuid}
                                 tag={tag}
                                 selectTag={selectTag}
---
+++ app/assets/javascripts/components/TagsListItem.tsx
@@ -0,0 +1,150 @@
+import { SNTag } from '@standardnotes/snjs';
+import { observer } from 'mobx-react-lite';
+import { FunctionComponent, JSX } from 'preact';
+import { useCallback, useEffect, useRef, useState } from 'preact/hooks';
+
+type Props = {
+    tag: SNTag;
+    selectTag: (tag: SNTag) => void;
+    removeTag: (tag: SNTag) => void;
+    saveTag: (tag: SNTag, newTitle: string) => void;
+    appState: TagsListState;
+}
+
+export type TagsListState = {
+    readonly selectedTag: SNTag | undefined;
+    editingTag: SNTag | undefined;
+}
+
+export const TagsListItem: FunctionComponent<Props> = observer(
+    ({ tag, selectTag, saveTag, removeTag, appState }) => {
+        const [title, setTitle] = useState(tag.title);
+        const inputRef = useRef<HTMLInputElement>(null);
+
+        const isSelected = appState.selectedTag === tag;
+        const isEditing = appState.editingTag === tag;
+        const noteCounts = tag.noteCount; // TODO: Check that the SNTag is an observable.
+
+        useEffect(() => {
+            setTitle(tag.title);
+        }, [tag]);
+
+        const selectCurrentTag = useCallback(() => {
+            selectTag(tag);
+        }, [selectTag, tag]);
+
+        const onBlur = useCallback(() => {
+            appState.editingTag = undefined;
+            saveTag(tag, title);
+        }, [appState, tag, saveTag, title]);
+
+        const onInput = useCallback((e: JSX.TargetedEvent<HTMLInputElement>) => {
+            const value = (e.target as HTMLInputElement).value;
+            setTitle(value);
+        }, [setTitle]);
+
+        const onKeyUp = useCallback((e: KeyboardEvent) => {
+            if (e.code === 'Enter') {
+                inputRef.current?.blur();
+                e.preventDefault();
+            }
+        }, [inputRef]);
+
+        useEffect(() => {
+            // NOTE(laurent): before we had 2 ways to focus,
+            // sn-autofocus and the `document.getElementById('tag-' + tag.uuid)!.focus();` in tags_view.ts l373
+            // I am remplacing this by a single focus trigger here.
+            // TODO: Check I am not missing any behavior
+            if (isEditing) {
+                inputRef.current?.focus();
+            }
+        }, [isEditing]);
+
+        const onClickRename = useCallback(() => {
+            appState.editingTag = tag;
+        }, [appState, tag]);
+
+        const onClickSave = useCallback(() => {
+            // NOTE(laurent): I use an implicit blur which trigger the main save code path.
+            inputRef.current?.blur();
+        }, []);
+
+        const onClickDelete = useCallback(() => {
+            removeTag(tag);
+        }, [removeTag, tag]);
+
+        return <div className={`tag ${isSelected ? 'selected' : ''}`} onClick={selectCurrentTag}>
+            {
+                // TODO: test this case.
+                !tag.errorDecrypting
+                    ? <div className="tag-info">
+                        <div className="tag-icon">
+                            #
+                        </div>
+                        <input className={`title ${isEditing ? 'editing' : ''}`}
+                            id={`react-tag-${tag.uuid}`}
+                            onBlur={onBlur}
+                            onInput={onInput}
+                            onClick={selectCurrentTag}
+                            value={title}
+                            onKeyUp={onKeyUp}
+                            spellCheck={false}
+                            ref={inputRef}
+                        />
+                        <div className="count">
+                            {noteCounts}
+                        </div>
+                    </div>
+                    : null
+            }
+            {
+                // TODO: how to test this?
+                tag.conflictOf
+                    ? <div className="danger small-text font-bold">
+                        Conflicted Copy {tag.conflictOf}
+                    </div>
+                    : null
+            }
+            {
+                // TODO: how to test this?
+                (tag.errorDecrypting && !tag.waitingForKey)
+                    ? <div className="danger small-text font-bold">
+                        Missing Keys
+                    </div>
+                    : null
+            }
+            {
+                // TODO: how to test this?
+                (tag.errorDecrypting && tag.waitingForKey)
+                    ? <div className="info small-text font-bold">
+                        Waiting For Keys
+                    </div>
+                    : null
+            }
+            {
+                isSelected
+                    ? <div className="menu">
+                        {
+                            !isEditing
+                                ? <a className="item" onClick={onClickRename}>
+                                    Rename
+                                </a>
+                                : null
+                        }
+                        {
+                            isEditing
+                                ? <a className="item" onClick={onClickSave}>
+                                    Save
+                                </a>
+                                : null
+                        }
+                        <a className="item" onClick={onClickDelete}>
+                            Delete
+                        </a>
+                    </div>
+                    : null
+            }
+
+        </div>;
+    }
+);
