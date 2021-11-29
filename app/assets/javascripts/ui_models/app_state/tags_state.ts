f8638954696d3098f9b5264d7c4c7105e898ecdd
Laurent Senta <laurent@singulargarden.com>
2021-11-29 14:06:26 +0100

    feat: add drag & drop draft

--- app/assets/javascripts/ui_models/app_state/tags_state.ts
+++ app/assets/javascripts/ui_models/app_state/tags_state.ts
@@ -1,10 +1,17 @@
 import { ContentType, SNSmartTag, SNTag } from '@standardnotes/snjs';
-import { computed, makeObservable, observable, runInAction } from 'mobx';
+import {
+  action,
+  computed,
+  makeObservable,
+  observable,
+  runInAction,
+} from 'mobx';
 import { WebApplication } from '../application';
 
 export class TagsState {
   tags: SNTag[] = [];
   smartTags: SNSmartTag[] = [];
+  hasFolders = true;
 
   constructor(
     private application: WebApplication,
@@ -13,7 +20,11 @@ export class TagsState {
     makeObservable(this, {
       tags: observable,
       smartTags: observable,
+      hasFolders: observable,
 
+      assignParent: action,
+
+      rootTags: computed,
       tagsCount: computed,
     });
 
@@ -22,6 +33,8 @@ export class TagsState {
         [ContentType.Tag, ContentType.SmartTag],
         () => {
           runInAction(() => {
+            // TODO: what is getDisplayableItems? Does it means I can't just getChildren for a tag but I have to go through this call?
+            // TODO: what about sorting, is the data sorted? (get children is not from what I can see).
             this.tags = this.application.getDisplayableItems(
               ContentType.Tag
             ) as SNTag[];
@@ -30,6 +43,46 @@ export class TagsState {
         }
       )
     );
+  }
+
+  getChildren(tag: SNTag): SNTag[] {
+    if (!this.hasFolders) {
+      return [];
+    }
+
+    const children = this.application.getTagChildren(tag);
+    const childrenUuids = children.map((x) => x.uuid);
+    const childrenTags = this.tags.filter((x) =>
+      childrenUuids.includes(x.uuid)
+    );
+    return childrenTags;
+  }
+
+  isValidParent(tagUuid: string, parentUuid: string): boolean {
+    const tag = this.application.findItem(tagUuid) as SNTag;
+    const parent = this.application.findItem(parentUuid) as SNTag;
+    return this.application.isValidParent(parent, tag);
+  }
+
+  assignParent(tagUuid: string, parentUuid: string | undefined): void {
+    const tag = this.application.findItem(tagUuid) as SNTag;
+
+    const parent =
+      parentUuid && (this.application.findItem(parentUuid) as SNTag);
+
+    if (!parent) {
+      this.application.unsetTagParent(tag);
+    } else {
+      this.application.setTagParent(parent, tag);
+    }
+  }
+
+  get rootTags(): SNTag[] {
+    if (!this.hasFolders) {
+      return this.tags;
+    }
+
+    return this.tags.filter((tag) => !this.application.getTagParent(tag));
   }
 
   get tagsCount(): number {
