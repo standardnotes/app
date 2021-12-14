c10a425ceb7cac2ea9e17835a12d41e938ac5f2c
Laurent Senta <laurent@singulargarden.com>
2021-12-14 16:12:54 +0100

    fix: use add tags folder hierarchy & isTemplateItem (#13)
    * fix: use new snjs add tag hierarchy
    
    * fix: use new snjs isTemplateItem

--- app/assets/javascripts/ui_models/app_state/note_tags_state.ts
+++ app/assets/javascripts/ui_models/app_state/note_tags_state.ts
@@ -174,32 +174,11 @@ export class NoteTagsState {
     }
   }
 
-  public static addTagHierarchyToNote(
-    application: WebApplication,
-    tag: SNTag,
-    note: SNNote
-  ): Promise<unknown> {
-    const parentChainTags = application.getTagParentChain(tag);
-    const tagsToAdd = [...parentChainTags, tag];
-
-    return Promise.all(
-      tagsToAdd.map(async (tag) => {
-        await application.changeItem(tag.uuid, (mutator) => {
-          mutator.addItemAsRelationship(note);
-        });
-      })
-    );
-  }
-
   async addTagToActiveNote(tag: SNTag): Promise<void> {
     const { activeNote } = this;
 
     if (activeNote) {
-      await NoteTagsState.addTagHierarchyToNote(
-        this.application,
-        tag,
-        activeNote
-      );
+      await this.application.addTagHierarchyToNote(activeNote, tag);
       this.application.sync();
       this.reloadTags();
     }
