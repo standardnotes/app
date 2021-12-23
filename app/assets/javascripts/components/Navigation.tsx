42a0101c10a5077aa18acce9ee70809fdb6bc9c9
Laurent Senta <laurent@singulargarden.com>
2021-12-23 18:29:30 +0100

    fix: panel resizer ref

--- app/assets/javascripts/components/Navigation.tsx
+++ app/assets/javascripts/components/Navigation.tsx
@@ -27,19 +27,18 @@ type Props = {
 
 export const Navigation: FunctionComponent<Props> = observer(
   ({ application }) => {
-    const panelRef = useRef<HTMLDivElement>(null);
     const appState = useMemo(() => application.getAppState(), [application]); // TODO: define only one way to do this
     const component = appState.tagsListComponent;
     const enableNativeSmartTagsFeature =
       appState.features.enableNativeSmartTagsFeature;
-
-    // TODO: remove this hack to force a refresh and set the ref.
-    const [a, setA] = useState(0);
+    const [panelRef, setPanelRef] = useState<HTMLDivElement | null>(null);
 
     useEffect(() => {
-      setTimeout(() => setA(12), 100);
-    }, [setA]);
-    // end of hack
+      const elem = document.querySelector(
+        'navigation'
+      ) as HTMLDivElement | null;
+      setPanelRef(elem);
+    }, [setPanelRef]);
 
     const onCreateNewTag = useCallback(() => {
       appState.tags.createNewTemplate();
@@ -57,67 +56,63 @@ export const Navigation: FunctionComponent<Props> = observer(
       appState.noteTags.reloadTagsContainerMaxWidth(); // TODO: probably not updated
     }, [appState]);
 
-    console.log(panelRef, panelRef.current);
-
     return (
-      <PremiumModalProvider>
-        <div
-          id="tags-column"
-          ref={panelRef}
-          className="sn-component section tags"
-          data-aria-label="Tags"
-        >
-          {component ? (
-            <div className="component-view-container">
-              {/* TODO: add back class .component-view */}
-              <ComponentView
-                componentUuid={component.uuid}
-                application={application}
-                appState={appState}
-              />
-            </div>
-          ) : (
-            <div id="tags-content" className="content">
-              <div className="tags-title-section section-title-bar">
-                <div className="section-title-bar-header">
-                  <div className="sk-h3 title">
-                    <span className="sk-bold">Views</span>
-                  </div>
-                  {!enableNativeSmartTagsFeature && (
-                    <div
-                      className="sk-button sk-secondary-contrast wide"
-                      onClick={onCreateNewTag}
-                      title="Create a new tag"
-                    >
-                      <div className="sk-label">
-                        <i className="icon ion-plus add-button" />
-                      </div>
-                    </div>
-                  )}
-                </div>
-              </div>
-              <div className="scrollable">
-                <div className="infinite-scroll">
-                  <SmartTagsSection appState={appState} />
-                  <TagsSection appState={appState} />
-                </div>
-              </div>
-            </div>
-          )}
-          {panelRef.current && (
-            <PanelResizer
+      <div
+        id="tags-column"
+        ref={setPanelRef}
+        className="sn-component section tags"
+        data-aria-label="Tags"
+      >
+        {component ? (
+          <div className="component-view-container">
+            {/* TODO: add back class .component-view */}
+            <ComponentView
+              componentUuid={component.uuid}
               application={application}
-              collapsable={true}
-              defaultWidth={150}
-              panel={document.querySelector('navigation') as HTMLDivElement}
-              prefKey={PrefKey.TagsPanelWidth}
-              side={PanelSide.Right}
-              resizeFinishCallback={panelResizeFinishCallback}
-              widthEventCallback={panelWidthEventCallback}
+              appState={appState}
             />
-          )}
-        </div>
-      </PremiumModalProvider>
+          </div>
+        ) : (
+          <div id="tags-content" className="content">
+            <div className="tags-title-section section-title-bar">
+              <div className="section-title-bar-header">
+                <div className="sk-h3 title">
+                  <span className="sk-bold">Views</span>
+                </div>
+                {!enableNativeSmartTagsFeature && (
+                  <div
+                    className="sk-button sk-secondary-contrast wide"
+                    onClick={onCreateNewTag}
+                    title="Create a new tag"
+                  >
+                    <div className="sk-label">
+                      <i className="icon ion-plus add-button" />
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="scrollable">
+              <div className="infinite-scroll">
+                <SmartTagsSection appState={appState} />
+                <TagsSection appState={appState} />
+              </div>
+            </div>
+          </div>
+        )}
+        {panelRef && (
+          <PanelResizer
+            application={application}
+            collapsable={true}
+            defaultWidth={150}
+            panel={panelRef}
+            prefKey={PrefKey.TagsPanelWidth}
+            side={PanelSide.Right}
+            resizeFinishCallback={panelResizeFinishCallback}
+            widthEventCallback={panelWidthEventCallback}
+          />
+        )}
+      </div>
     );
   }
 );
