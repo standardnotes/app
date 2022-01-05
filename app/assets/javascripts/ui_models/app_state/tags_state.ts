f543bbe507f00032561f7a78cf96d3976f8634e9
Laurent Senta <laurent@singulargarden.com>
2022-01-05 06:53:28 +0100

    fix: system smart tags on empty account (#796)

--- app/assets/javascripts/ui_models/app_state/tags_state.ts
+++ app/assets/javascripts/ui_models/app_state/tags_state.ts
@@ -86,6 +86,8 @@ export class TagsState {
     this.previouslySelected_ = undefined;
     this.editing_ = undefined;
 
+    this.smartTags = this.application.getSmartTags();
+
     makeObservable(this, {
       tags: observable.ref,
       smartTags: observable.ref,
