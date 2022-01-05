122f8fc2669aa14d0c196d87f02834d400b05f95
Karol Sójko <karolsojko@protonmail.com>
2022-01-05 16:11:00 +0100

    Revert "feat: allows duplicate names in tags folder & smart tags (#792)"
    This reverts commit a165fa9b4eb83545102c5c78b2f9174f3e98acdf.

--- app/assets/javascripts/components/AutocompleteTagResult.tsx
+++ app/assets/javascripts/components/AutocompleteTagResult.tsx
@@ -21,9 +21,6 @@ export const AutocompleteTagResult = observer(
 
     const tagResultRef = useRef<HTMLButtonElement>(null);
 
-    const title = tagResult.title;
-    const prefixTitle = appState.noteTags.getPrefixTitle(tagResult);
-
     const onTagOptionClick = async (tag: SNTag) => {
       await appState.noteTags.addTagToActiveNote(tag);
       appState.noteTags.clearAutocompleteSearch();
@@ -89,10 +86,9 @@ export const AutocompleteTagResult = observer(
       >
         <Icon type="hashtag" className="color-neutral mr-2 min-h-5 min-w-5" />
         <span className="whitespace-nowrap overflow-hidden overflow-ellipsis">
-          {prefixTitle && <span className="grey-2">{prefixTitle}</span>}
           {autocompleteSearchQuery === ''
-            ? title
-            : title
+            ? tagResult.title
+            : tagResult.title
                 .split(new RegExp(`(${autocompleteSearchQuery})`, 'gi'))
                 .map((substring, index) => (
                   <span
--- app/assets/javascripts/ui_models/app_state/tags_state.ts
+++ app/assets/javascripts/ui_models/app_state/tags_state.ts
@@ -4,7 +4,6 @@ import {
   ComponentAction,
   ContentType,
   MessageData,
-  SNApplication,
   SNSmartTag,
   SNTag,
   TagMutator,
@@ -22,48 +21,6 @@ import { WebApplication } from '../application';
 import { FeaturesState, SMART_TAGS_FEATURE_NAME } from './features_state';
 
 type AnyTag = SNTag | SNSmartTag;
-
-const rootTags = (application: SNApplication): SNTag[] => {
-  const hasNoParent = (tag: SNTag) => !application.getTagParent(tag);
-
-  const allTags = application.getDisplayableItems(ContentType.Tag) as SNTag[];
-  const rootTags = allTags.filter(hasNoParent);
-
-  return rootTags;
-};
-
-const tagSiblings = (application: SNApplication, tag: SNTag): SNTag[] => {
-  const withoutCurrentTag = (tags: SNTag[]) =>
-    tags.filter((other) => other.uuid !== tag.uuid);
-
-  const isTemplateTag = application.isTemplateItem(tag);
-  const parentTag = !isTemplateTag && application.getTagParent(tag);
-
-  if (parentTag) {
-    const siblingsAndTag = application.getTagChildren(parentTag);
-    return withoutCurrentTag(siblingsAndTag);
-  }
-
-  return withoutCurrentTag(rootTags(application));
-};
-
-const isValidFutureSiblings = (
-  application: SNApplication,
-  futureSiblings: SNTag[],
-  tag: SNTag
-): boolean => {
-  const siblingWithSameName = futureSiblings.find(
-    (otherTag) => otherTag.title === tag.title
-  );
-
-  if (siblingWithSameName) {
-    application.alertService?.alert(
-      `A tag with the name ${tag.title} already exists at this destination. Please rename this tag before moving and try again.`
-    );
-    return false;
-  }
-  return true;
-};
 
 export class TagsState {
   tags: SNTag[] = [];
@@ -187,27 +144,12 @@ export class TagsState {
   ): Promise<void> {
     const tag = this.application.findItem(tagUuid) as SNTag;
 
-    const currentParent = this.application.getTagParent(tag);
-    const currentParentUuid = currentParent?.parentId;
-
-    if (currentParentUuid === parentUuid) {
-      return;
-    }
-
     const parent =
       parentUuid && (this.application.findItem(parentUuid) as SNTag);
 
     if (!parent) {
-      const futureSiblings = rootTags(this.application);
-      if (!isValidFutureSiblings(this.application, futureSiblings, tag)) {
-        return;
-      }
       await this.application.unsetTagParent(tag);
     } else {
-      const futureSiblings = this.application.getTagChildren(parent);
-      if (!isValidFutureSiblings(this.application, futureSiblings, tag)) {
-        return;
-      }
       await this.application.setTagParent(parent, tag);
     }
 
@@ -307,11 +249,7 @@ export class TagsState {
     const hasEmptyTitle = newTitle.length === 0;
     const hasNotChangedTitle = newTitle === tag.title;
     const isTemplateChange = this.application.isTemplateItem(tag);
-
-    const siblings = tagSiblings(this.application, tag);
-    const hasDuplicatedTitle = siblings.some(
-      (other) => other.title.toLowerCase() === newTitle.toLowerCase()
-    );
+    const hasDuplicatedTitle = !!this.application.findTagByTitle(newTitle);
 
     runInAction(() => {
       this.editing_ = undefined;
