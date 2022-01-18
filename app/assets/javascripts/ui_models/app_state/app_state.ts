482c8d9c141d29ba57783f2bcdfc08a2a188a137
Aman Harwara <amanharwara@protonmail.com>
2022-01-18 17:22:06 +0530

    feat: Automatically set theme according to device color scheme

--- app/assets/javascripts/ui_models/app_state/app_state.ts
+++ app/assets/javascripts/ui_models/app_state/app_state.ts
@@ -17,6 +17,7 @@ import {
   ComponentViewer,
   SNTag,
   NoteViewController,
+  SNTheme,
 } from '@standardnotes/snjs';
 import pull from 'lodash/pull';
 import {
@@ -400,6 +401,48 @@ export class AppState {
   }
 
   addAppEventObserver() {
+    const setThemeAsPerColorScheme = (prefersDarkColorScheme: boolean) => {
+      const useDeviceThemeSettings = this.application.getPreference(
+        PrefKey.UseSystemColorScheme,
+        false
+      );
+
+      if (useDeviceThemeSettings) {
+        const preference = prefersDarkColorScheme
+          ? PrefKey.AutoDarkThemeIdentifier
+          : PrefKey.AutoLightThemeIdentifier;
+        const themes = this.application.getDisplayableItems(
+          ContentType.Theme
+        ) as SNTheme[];
+
+        const enableDefaultTheme = () => {
+          const activeTheme = themes.find(
+            (theme) => theme.active && !theme.isLayerable()
+          );
+          if (activeTheme) this.application.toggleTheme(activeTheme);
+        };
+
+        const themeIdentifier = this.application.getPreference(
+          preference,
+          'None'
+        ) as string;
+        if (themeIdentifier === 'Default') {
+          enableDefaultTheme();
+        } else {
+          const theme = themes.find(
+            (theme) => theme.package_info.identifier === themeIdentifier
+          );
+          if (theme && !theme.active) {
+            this.application.toggleTheme(theme);
+          }
+        }
+      }
+    };
+
+    const colorSchemeEventHandler = (event: MediaQueryListEvent) => {
+      setThemeAsPerColorScheme(event.matches);
+    };
+
     this.unsubApp = this.application.addEventObserver(async (eventName) => {
       switch (eventName) {
         case ApplicationEvent.Started:
@@ -410,7 +453,17 @@ export class AppState {
           if (window.location.search.includes('purchase=true')) {
             this.purchaseFlow.openPurchaseFlow();
           }
+          window
+            .matchMedia('(prefers-color-scheme: dark)')
+            .addEventListener('change', colorSchemeEventHandler);
           break;
+        case ApplicationEvent.PreferencesChanged: {
+          const prefersDarkColorScheme = window.matchMedia(
+            '(prefers-color-scheme: dark)'
+          );
+          setThemeAsPerColorScheme(prefersDarkColorScheme.matches);
+          break;
+        }
         case ApplicationEvent.SyncStatusChanged:
           this.sync.update(this.application.getSyncStatus());
           break;
