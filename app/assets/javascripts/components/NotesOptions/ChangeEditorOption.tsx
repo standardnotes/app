6150f58c73b3c26eba327f324eb8557b3796b75d
Aman Harwara <amanharwara@protonmail.com>
2022-02-01 13:32:02 +0530

    feat: show alert when changing to non-interchangeable editor (#834)

--- app/assets/javascripts/components/NotesOptions/ChangeEditorOption.tsx
+++ app/assets/javascripts/components/NotesOptions/ChangeEditorOption.tsx
@@ -277,10 +277,10 @@ export const ChangeEditorOption: FunctionComponent<ChangeEditorOptionProps> = ({
           <EditorAccordionMenu
             application={application}
             closeOnBlur={closeOnBlur}
+            currentEditor={selectedEditor}
             groups={editorMenuGroups}
             isOpen={changeEditorMenuOpen}
             selectComponent={selectComponent}
-            selectedEditor={selectedEditor}
           />
         </PremiumModalProvider>
       </DisclosurePanel>
