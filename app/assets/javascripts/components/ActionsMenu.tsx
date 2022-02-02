381e3749394c5b317c4440d6cf91607dd3169dde
Mo <mo@standardnotes.com>
2022-02-02 00:05:52 -0600

    feat(wip): wip

--- app/assets/javascripts/components/ActionsMenu.tsx
+++ app/assets/javascripts/components/ActionsMenu.tsx
@@ -5,8 +5,7 @@ import {
   UuidString,
   CopyPayload,
   SNNote,
-  ListedAccountInfoToActionExtension,
-  ListedAccountInfo,
+  ListedAccount,
 } from '@standardnotes/snjs';
 import { ActionResponse } from '@standardnotes/snjs';
 import { render } from 'preact';
@@ -21,208 +20,230 @@ type ActionSubRow = {
   spinnerClass?: string;
 };
 
-type ExtensionState = {
-  loading: boolean;
-  error: boolean;
+type ActionItem = Action & {
+  running?: boolean;
+  subrows?: ActionSubRow[];
 };
 
-type MenuItem = {
+type MenuSection = {
   uuid: UuidString;
   name: string;
-  loading: boolean;
-  error: boolean;
-  hidden: boolean;
+  loading?: boolean;
+  error?: boolean;
+  hidden?: boolean;
   deprecation?: string;
-  extension: SNActionsExtension;
-  actions: (Action & {
-    subrows?: ActionSubRow[];
-  })[];
+  extension?: SNActionsExtension;
+  actions?: ActionItem[];
+  listedAccount?: ListedAccount;
 };
 
-type ActionState = {
-  error: boolean;
-  running: boolean;
-};
-
-type ActionsMenuState = {
-  extensions: SNActionsExtension[];
-  extensionsState: Record<UuidString, ExtensionState>;
-  hiddenExtensions: Record<UuidString, boolean>;
-  selectedActionId?: number;
-  menuItems: MenuItem[];
-  actionState: Record<number, ActionState>;
+type State = {
+  menuSections: MenuSection[];
+  selectedActionIdentifier?: string;
 };
 
 type Props = {
   application: WebApplication;
-  item: SNNote;
+  note: SNNote;
 };
 
-export class ActionsMenu extends PureComponent<Props, ActionsMenuState> {
+export class ActionsMenu extends PureComponent<Props, State> {
   constructor(props: Props) {
     super(props, props.application);
 
     this.state = {
-      extensions: [],
-      extensionsState: {},
-      hiddenExtensions: {},
-      menuItems: [],
-      actionState: {},
+      menuSections: [],
     };
 
     this.loadExtensions();
   }
 
   private async loadExtensions(): Promise<void> {
-    const resolvedExtensions = await Promise.all([
-      ...(await this.getResolvedNonListedExtensions()),
-      ...(await this.getResolvedListedExtensions()),
-    ]);
-
-    const extensionsState: Record<UuidString, ExtensionState> = {};
-    resolvedExtensions.map((extension) => {
-      extensionsState[extension.uuid] = {
-        loading: false,
-        error: false,
-      };
-    });
-
-    this.setState({
-      extensions: resolvedExtensions,
-      extensionsState: extensionsState,
-    });
-
-    this.rebuildMenuState({
-      extensions: resolvedExtensions,
-      extensionsState: extensionsState,
-    });
-  }
-
-  private extensionsByRemovingActions(
-    extension: SNActionsExtension
-  ): SNActionsExtension {
-    return new SNActionsExtension(
-      CopyPayload(extension.payload, {
-        content: {
-          ...extension.payload.safeContent,
-          actions: [],
-        },
-      })
+    const unresolvedListedSections =
+      await this.getNonresolvedListedMenuSections();
+    const unresolvedGenericSections =
+      await this.getNonresolvedGenericMenuSections();
+    this.setState(
+      {
+        menuSections: unresolvedListedSections.concat(
+          unresolvedGenericSections
+        ),
+      },
+      () => {
+        this.state.menuSections.forEach((menuSection) => {
+          this.resolveMenuSection(menuSection);
+        });
+      }
     );
   }
 
-  private async getResolvedNonListedExtensions(): Promise<
-    SNActionsExtension[]
-  > {
-    const nonListedExtensions = this.props.application.actionsManager
+  private async getNonresolvedGenericMenuSections(): Promise<MenuSection[]> {
+    const genericExtensions = this.props.application.actionsManager
       .getExtensions()
       .sort((a, b) => {
         return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
-      })
-      .map((extension) => {
-        return this.extensionsByRemovingActions(extension);
       });
 
-    const resolved = await Promise.all(
-      nonListedExtensions.map((ext) => {
-        return this.props.application.actionsManager.loadExtensionInContextOfItem(
-          ext,
-          this.props.item
-        );
-      })
-    );
-    return resolved.filter((ext) => ext != undefined) as SNActionsExtension[];
+    return genericExtensions.map((extension) => {
+      const menuSection: MenuSection = {
+        uuid: extension.uuid,
+        name: extension.name,
+        extension: extension,
+        loading: true,
+        hidden: this.appState.actionsMenu.hiddenSections[extension.uuid],
+      };
+      return menuSection;
+    });
   }
 
-  private async getResolvedListedExtensions(): Promise<SNActionsExtension[]> {
+  private async getNonresolvedListedMenuSections(): Promise<MenuSection[]> {
     const listedAccountEntries =
       await this.props.application.getListedAccounts();
-    const accountInfos = await Promise.all(
-      listedAccountEntries.map((account) => {
-        return this.props.application.getListedAccountInfo(
-          account,
-          this.props.item.uuid
-        );
-      })
-    );
-    const listedExtensions = (
-      accountInfos.filter((info) => info != undefined) as ListedAccountInfo[]
-    ).map((accountInfo) => ListedAccountInfoToActionExtension(accountInfo));
-    return listedExtensions;
-  }
-
-  componentDidMount() {
-    this.autorun(() => {
-      this.rebuildMenuState({
-        hiddenExtensions: this.appState.actionsMenu.hiddenExtensions,
-      });
+    return listedAccountEntries.map((entry) => {
+      const menuSection: MenuSection = {
+        uuid: entry.authorId,
+        name: `Listed ${entry.authorId}`,
+        loading: true,
+        listedAccount: entry,
+        hidden: this.appState.actionsMenu.hiddenSections[entry.authorId],
+      };
+      return menuSection;
     });
   }
 
-  rebuildMenuState({
-    extensions = this.state.extensions,
-    extensionsState = this.state.extensionsState,
-    selectedActionId = this.state.selectedActionId,
-    hiddenExtensions = this.appState.actionsMenu.hiddenExtensions,
-  } = {}) {
-    return this.setState({
-      extensions,
-      extensionsState,
-      selectedActionId,
-      menuItems: extensions.map((extension) => {
-        const state = extensionsState[extension.uuid];
-        const hidden = hiddenExtensions[extension.uuid];
-        const item: MenuItem = {
-          uuid: extension.uuid,
-          name: extension.name,
-          loading: state?.loading ?? false,
-          error: state?.error ?? false,
-          hidden: hidden ?? false,
-          extension,
-          deprecation: extension.deprecation!,
-          actions: extension
-            .actionsWithContextForItem(this.props.item)
+  private resolveMenuSection(menuSection: MenuSection): void {
+    if (menuSection.listedAccount) {
+      this.props.application
+        .getListedAccountInfo(menuSection.listedAccount, this.props.note.uuid)
+        .then((accountInfo) => {
+          if (!accountInfo) {
+            this.promoteMenuSection({
+              ...menuSection,
+              loading: false,
+            });
+            return;
+          }
+          const existingMenuSection = this.state.menuSections.find(
+            (item) => item.uuid === menuSection.listedAccount?.authorId
+          ) as MenuSection;
+          const resolvedMenuSection: MenuSection = {
+            ...existingMenuSection,
+            loading: false,
+            error: false,
+            name: accountInfo.display_name,
+            actions: accountInfo?.actions,
+          };
+          this.promoteMenuSection(resolvedMenuSection);
+        });
+    } else if (menuSection.extension) {
+      this.props.application.actionsManager
+        .loadExtensionInContextOfItem(menuSection.extension, this.props.note)
+        .then((resolvedExtension) => {
+          if (!resolvedExtension) {
+            this.promoteMenuSection({
+              ...menuSection,
+              loading: false,
+            });
+            return;
+          }
+
+          const actions = resolvedExtension
+            .actionsWithContextForItem(this.props.note)
             .map((action) => {
-              if (action.id === selectedActionId) {
-                return {
-                  ...action,
-                  subrows: this.subRowsForAction(action, extension),
-                };
-              } else {
-                return action;
-              }
-            }),
-        };
-        return item;
-      }),
-    });
+              return {
+                ...action,
+                subrows:
+                  this.idForAction(action) ===
+                  this.state.selectedActionIdentifier
+                    ? this.subRowsForAction(action, resolvedExtension)
+                    : [],
+              };
+            });
+
+          const resolvedMenuSection: MenuSection = {
+            ...menuSection,
+            actions: actions,
+            deprecation: resolvedExtension.deprecation,
+            loading: false,
+            error: false,
+          };
+          this.promoteMenuSection(resolvedMenuSection);
+        });
+    }
   }
 
-  executeAction = async (action: Action, extension: SNActionsExtension) => {
+  private promoteMenuSection(newItem: MenuSection): void {
+    const menuSections = this.state.menuSections.map((menuSection) => {
+      if (menuSection.uuid === newItem.uuid) {
+        return newItem;
+      } else {
+        return menuSection;
+      }
+    });
+    this.setState({ menuSections });
+  }
+
+  private promoteAction(newAction: Action, section: MenuSection): void {
+    const newSection: MenuSection = {
+      ...section,
+      actions: section.actions?.map((action) => {
+        if (action.url === newAction.url) {
+          return newAction;
+        } else {
+          return action;
+        }
+      }),
+    };
+    this.promoteMenuSection(newSection);
+  }
+
+  private idForAction(action: Action) {
+    return `${action.label}:${action.verb}:${action.desc}`;
+  }
+
+  executeAction = async (action: Action, section: MenuSection) => {
     if (action.verb === 'nested') {
-      this.rebuildMenuState({
-        selectedActionId: action.id,
-      });
+      this.setState(
+        {
+          selectedActionIdentifier: this.idForAction(action),
+        },
+        () => {
+          this.resolveMenuSection(section);
+        }
+      );
       return;
     }
-    this.updateActionState(action, { running: true, error: false });
+
+    this.promoteAction(
+      {
+        ...action,
+        running: true,
+      },
+      section
+    );
 
     const response = await this.props.application.actionsManager.runAction(
       action,
-      this.props.item,
+      this.props.note,
       async () => {
-        /** @todo */
         return '';
       }
     );
+
+    this.promoteAction(
+      {
+        ...action,
+        running: false,
+      },
+      section
+    );
+
     if (response.error) {
-      this.updateActionState(action, { error: true, running: false });
       return;
     }
 
-    this.updateActionState(action, { running: false, error: false });
     this.handleActionResponse(action, response);
-    await this.reloadExtension(extension);
+    this.resolveMenuSection(section);
   };
 
   handleActionResponse(action: Action, result: ActionResponse) {
@@ -250,104 +271,49 @@ export class ActionsMenu extends PureComponent<Props, ActionsMenuState> {
     }
     return parentAction.subactions.map((subaction) => {
       return {
-        id: subaction.id,
         onClick: () => {
           this.executeAction(subaction, extension);
         },
         label: subaction.label,
         subtitle: subaction.desc,
-        spinnerClass: this.getActionState(subaction).running
-          ? 'info'
-          : undefined,
       };
     });
   }
 
-  private updateActionState(action: Action, actionState: ActionState): void {
-    const state = this.state.actionState;
-    state[action.id] = actionState;
-    this.setState({ actionState: state });
-  }
-
-  private getActionState(action: Action): ActionState {
-    return this.state.actionState[action.id] || {};
-  }
-
-  private async updateExtension(extension: SNActionsExtension) {
-    const extensions = this.state.extensions.map((ext: SNActionsExtension) => {
-      if (extension.uuid === ext.uuid) {
-        return extension;
-      }
-      return ext;
-    });
-    this.rebuildMenuState({
-      extensions,
+  public toggleSectionVisibility(menuSection: MenuSection) {
+    this.appState.actionsMenu.toggleSectionVisibility(menuSection.uuid);
+    this.promoteMenuSection({
+      ...menuSection,
+      hidden: !menuSection.hidden,
     });
   }
 
-  private async reloadExtension(extension: SNActionsExtension) {
-    const extensionInContext =
-      await this.props.application.actionsManager.loadExtensionInContextOfItem(
-        extension,
-        this.props.item
-      );
-    const extensions = this.state.extensions.map((ext: SNActionsExtension) => {
-      if (extension.uuid === ext.uuid) {
-        return extensionInContext!;
-      }
-      return ext;
-    });
-    this.rebuildMenuState({
-      extensions,
-    });
-  }
-
-  public toggleExtensionVisibility(extensionUuid: UuidString) {
-    this.appState.actionsMenu.toggleExtensionVisibility(extensionUuid);
-  }
-
-  private setLoadingExtension(extensionUuid: UuidString, value = false) {
-    const { extensionsState } = this.state;
-    extensionsState[extensionUuid].loading = value;
-    this.rebuildMenuState({
-      extensionsState,
-    });
-  }
-
-  private setErrorExtension(extensionUuid: UuidString, value = false) {
-    const { extensionsState } = this.state;
-    extensionsState[extensionUuid].error = value;
-    this.rebuildMenuState({
-      extensionsState,
-    });
-  }
-
-  renderMenuItem(menuItem: MenuItem) {
+  renderMenuSection(section: MenuSection) {
     return (
       <div>
         <div
-          key={menuItem.uuid}
+          key={section.uuid}
           className="sk-menu-panel-header"
           onClick={($event) => {
-            this.toggleExtensionVisibility(menuItem.uuid);
+            this.toggleSectionVisibility(section);
             $event.stopPropagation();
           }}
         >
           <div className="sk-menu-panel-column">
-            <div className="sk-menu-panel-header-title">{menuItem.name}</div>
-            {menuItem.hidden && <div>…</div>}
-            {menuItem.deprecation && !menuItem.hidden && (
+            <div className="sk-menu-panel-header-title">{section.name}</div>
+            {section.hidden && <div>…</div>}
+            {section.deprecation && !section.hidden && (
               <div className="sk-menu-panel-header-subtitle">
-                {menuItem.deprecation}
+                {section.deprecation}
               </div>
             )}
           </div>
 
-          {menuItem.loading && <div className="sk-spinner small loading" />}
+          {section.loading && <div className="sk-spinner small loading" />}
         </div>
 
         <div>
-          {menuItem.error && !menuItem.hidden && (
+          {section.error && !section.hidden && (
             <MenuRow
               faded={true}
               label="Error loading actions"
@@ -355,25 +321,23 @@ export class ActionsMenu extends PureComponent<Props, ActionsMenuState> {
             />
           )}
 
-          {!menuItem.actions.length && !menuItem.hidden && (
+          {!section.actions?.length && !section.hidden && (
             <MenuRow faded={true} label="No Actions Available" />
           )}
 
-          {!menuItem.hidden &&
-            !menuItem.loading &&
-            !menuItem.error &&
-            menuItem.actions.map((action, index) => {
+          {!section.hidden &&
+            !section.loading &&
+            !section.error &&
+            section.actions?.map((action, index) => {
               return (
                 <MenuRow
                   key={index}
                   action={() => {
-                    this.executeAction(action, menuItem.extension);
+                    this.executeAction(action, section);
                   }}
                   label={action.label}
-                  disabled={this.getActionState(action).running}
-                  spinnerClass={
-                    this.getActionState(action).running ? 'info' : undefined
-                  }
+                  disabled={action.running}
+                  spinnerClass={action.running ? 'info' : undefined}
                   subRows={action.subrows}
                   subtitle={action.desc}
                 >
@@ -396,9 +360,11 @@ export class ActionsMenu extends PureComponent<Props, ActionsMenuState> {
     return (
       <div className="sn-component">
         <div className="sk-menu-panel dropdown-menu">
-          {this.state.extensions.length == 0 && <MenuRow label="No Actions" />}
-          {this.state.menuItems.map((extension) =>
-            this.renderMenuItem(extension)
+          {this.state.menuSections.length == 0 && (
+            <MenuRow label="No Actions" />
+          )}
+          {this.state.menuSections.map((extension) =>
+            this.renderMenuSection(extension)
           )}
         </div>
       </div>
