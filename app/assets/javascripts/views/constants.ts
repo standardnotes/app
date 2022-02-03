d4625da91a17cfda19f12175d943aeebb3a4bfcd
Aman Harwara <amanharwara@protonmail.com>
2022-02-03 20:27:19 +0530

    feat: add new constants

--- app/assets/javascripts/views/constants.ts
+++ app/assets/javascripts/views/constants.ts
@@ -9,3 +9,7 @@ export const MAX_MENU_SIZE_MULTIPLIER = 30;
 
 export const FOCUSABLE_BUT_NOT_TABBABLE = -1;
 export const NOTES_LIST_SCROLL_THRESHOLD = 200;
+
+export const MILLISECONDS_IN_A_DAY = 1000 * 60 * 60 * 24;
+export const DAYS_IN_A_WEEK = 7;
+export const DAYS_IN_A_YEAR = 365;
