093e2795e4f977ba1dbd4dc8f9093dcf29249b36
Aman Harwara <amanharwara@protonmail.com>
2022-02-07 15:13:32 +0530

    refactor: use useMemo instead of useState+useEffect combo

--- app/assets/javascripts/components/RevisionHistoryModal/SelectedRevisionContent.tsx
+++ app/assets/javascripts/components/RevisionHistoryModal/SelectedRevisionContent.tsx
@@ -1,14 +1,9 @@
 import { WebApplication } from '@/ui_models/application';
 import { AppState } from '@/ui_models/app_state';
-import {
-  ComponentViewer,
-  HistoryEntry,
-  SNComponent,
-  SNNote,
-} from '@standardnotes/snjs';
+import { HistoryEntry, SNComponent, SNNote } from '@standardnotes/snjs';
 import { observer } from 'mobx-react-lite';
 import { FunctionComponent } from 'preact';
-import { useEffect, useState } from 'preact/hooks';
+import { useEffect, useMemo } from 'preact/hooks';
 import { ComponentView } from '../ComponentView';
 
 const ABSOLUTE_CENTER_CLASSNAME =
@@ -17,8 +12,7 @@ const ABSOLUTE_CENTER_CLASSNAME =
 type SelectedRevisionContentProps = {
   application: WebApplication;
   appState: AppState;
-  isFetchingSelectedRevision: boolean;
-  selectedRevision: HistoryEntry | undefined;
+  selectedRevision: HistoryEntry;
   editorForCurrentNote: SNComponent | undefined;
   templateNoteForRevision: SNNote;
 };
@@ -28,14 +22,11 @@ export const SelectedRevisionContent: FunctionComponent<SelectedRevisionContentP
     ({
       application,
       appState,
-      isFetchingSelectedRevision,
       selectedRevision,
       editorForCurrentNote,
       templateNoteForRevision,
     }) => {
-      const [componentViewer, setComponentViewer] = useState<ComponentViewer>();
-
-      useEffect(() => {
+      const componentViewer = useMemo(() => {
         if (editorForCurrentNote) {
           const componentViewer =
             application.componentManager.createComponentViewer(
@@ -44,9 +35,9 @@ export const SelectedRevisionContent: FunctionComponent<SelectedRevisionContentP
           componentViewer.setReadonly(true);
           componentViewer.lockReadonly = true;
           componentViewer.overrideContextItem = templateNoteForRevision;
-          setComponentViewer(componentViewer);
+          return componentViewer;
         } else {
-          setComponentViewer(undefined);
+          return undefined;
         }
       }, [
         application.componentManager,
@@ -64,53 +55,35 @@ export const SelectedRevisionContent: FunctionComponent<SelectedRevisionContentP
         };
       }, [application.componentManager, componentViewer]);
 
-      if (!isFetchingSelectedRevision && !selectedRevision) {
-        return (
-          <div className={ABSOLUTE_CENTER_CLASSNAME}>No revision selected.</div>
-        );
-      }
-
-      if (isFetchingSelectedRevision) {
-        return (
-          <div
-            className={`sk-spinner w-5 h-5 mr-2 spinner-info ${ABSOLUTE_CENTER_CLASSNAME}`}
-          ></div>
-        );
-      }
-
-      if (selectedRevision) {
-        return (
-          <div className="flex flex-col h-full">
-            <div className="p-4 text-base font-bold w-full">
-              <div className="title">
-                {selectedRevision.payload.content.title}
-              </div>
+      return (
+        <div className="flex flex-col h-full">
+          <div className="p-4 text-base font-bold w-full">
+            <div className="title">
+              {selectedRevision.payload.content.title}
             </div>
-            {!componentViewer && (
-              <div className="relative flex-grow">
-                {selectedRevision.payload.content.text.length ? (
-                  <p className="p-4 pt-0">
-                    {selectedRevision.payload.content.text}
-                  </p>
-                ) : (
-                  <div className={ABSOLUTE_CENTER_CLASSNAME}>Empty note.</div>
-                )}
-              </div>
-            )}
-            {componentViewer && (
-              <div className="component-view">
-                <ComponentView
-                  key={componentViewer.identifier}
-                  componentViewer={componentViewer}
-                  application={application}
-                  appState={appState}
-                />
-              </div>
-            )}
           </div>
-        );
-      }
-
-      return null;
+          {!componentViewer && (
+            <div className="relative flex-grow">
+              {selectedRevision.payload.content.text.length ? (
+                <p className="p-4 pt-0">
+                  {selectedRevision.payload.content.text}
+                </p>
+              ) : (
+                <div className={ABSOLUTE_CENTER_CLASSNAME}>Empty note.</div>
+              )}
+            </div>
+          )}
+          {componentViewer && (
+            <div className="component-view">
+              <ComponentView
+                key={componentViewer.identifier}
+                componentViewer={componentViewer}
+                application={application}
+                appState={appState}
+              />
+            </div>
+          )}
+        </div>
+      );
     }
   );
