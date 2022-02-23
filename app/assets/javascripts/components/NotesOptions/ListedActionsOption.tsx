d707e72286540af29854eef3bdbeaf50159c7358
Aman Harwara <amanharwara@protonmail.com>
2022-02-23 22:55:21 +0530

    feat: add icon to listed group header

--- app/assets/javascripts/components/NotesOptions/ListedActionsOption.tsx
+++ app/assets/javascripts/components/NotesOptions/ListedActionsOption.tsx
@@ -183,11 +183,11 @@ const ListedActionsMenu: FunctionComponent<ListedActionsMenuProps> = ({
           {menuGroups.map((group, index) => (
             <Fragment key={group.account.authorId}>
               <div
-                className={`w-full px-2.5 py-2 text-input font-semibold color-text border-0 border-y-1px border-solid border-main ${
+                className={`w-full flex items-center px-2.5 py-2 text-input font-semibold color-text border-0 border-y-1px border-solid border-main ${
                   index === 0 ? 'border-t-0 mb-1' : 'my-1'
                 }`}
               >
-                {group.name}
+                <Icon type="notes" className="mr-2 color-info" /> {group.name}
               </div>
               {group.actions.length ? (
                 group.actions.map((action) => (
