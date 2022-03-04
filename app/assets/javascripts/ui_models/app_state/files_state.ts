e6579ef2cb52b7432c3b7bb6d4f1ff8d28a7144c
Aman Harwara <amanharwara@protonmail.com>
2022-03-04 19:25:03 +0530

    feat: add classic filesaver fallback for downloading
    refactor: use try-catch with async-await instead of method chaining

--- app/assets/javascripts/ui_models/app_state/files_state.ts
+++ app/assets/javascripts/ui_models/app_state/files_state.ts
@@ -2,6 +2,7 @@ import {
   ClassicFileReader,
   StreamingFileReader,
   StreamingFileSaver,
+  ClassicFileSaver,
 } from '@standardnotes/filepicker';
 import { SNFile } from '@standardnotes/snjs';
 import { addToast, dismissToast, ToastType } from '@standardnotes/stylekit';
@@ -13,17 +14,60 @@ export class FilesState {
 
   public async downloadFile(file: SNFile): Promise<void> {
     console.log('Downloading file', file.nameWithExt);
-    const saver = new StreamingFileSaver(file.nameWithExt);
-    await saver.selectFileToSaveTo();
 
-    await this.application.files.downloadFile(
-      file,
-      async (decryptedBytes: Uint8Array) => {
-        console.log(`Pushing ${decryptedBytes.length} decrypted bytes to disk`);
-        await saver.pushBytes(decryptedBytes);
+    const saver = StreamingFileSaver.available()
+      ? new StreamingFileSaver(file.nameWithExt)
+      : new ClassicFileSaver();
+
+    const isUsingStreamingSaver = saver instanceof StreamingFileSaver;
+
+    let downloadingToastId = '';
+
+    addToast({
+      type: ToastType.Regular,
+      message: `Starting download...`,
+    });
+
+    try {
+      if (isUsingStreamingSaver) {
+        await saver.selectFileToSaveTo();
+
+        downloadingToastId = addToast({
+          type: ToastType.Loading,
+          message: `Downloading file...`,
+        });
       }
-    );
-    await saver.finish();
+
+      await this.application.files.downloadFile(
+        file,
+        async (decryptedBytes: Uint8Array) => {
+          console.log(
+            `Pushing ${decryptedBytes.length} decrypted bytes to disk`
+          );
+
+          if (isUsingStreamingSaver) {
+            await saver.pushBytes(decryptedBytes);
+          } else {
+            saver.saveFile(file.nameWithExt, decryptedBytes);
+          }
+        }
+      );
+
+      if (isUsingStreamingSaver) {
+        await saver.finish();
+      }
+    } catch (error) {
+      console.error(error);
+
+      addToast({
+        type: ToastType.Error,
+        message: (error as Error).message ?? (error as Error).toString(),
+      });
+    }
+
+    if (downloadingToastId.length > 0) {
+      dismissToast(downloadingToastId);
+    }
   }
 
   public async uploadNewFile(): Promise<void> {
@@ -51,28 +95,31 @@ export class FilesState {
 
     const uploadingToastId = addToast({
       type: ToastType.Loading,
-      message: `Uploading file ${selectedFile.name}`,
+      message: `Uploading file "${selectedFile.name}"...`,
     });
 
     const fileResult = await picker.beginReadingFile();
 
-    this.application.files
-      .finishUpload(operation, fileResult.name, fileResult.ext)
-      .then((file: SNFile) => {
-        addToast({
-          type: ToastType.Success,
-          message: `Uploaded file ${file.nameWithExt},`,
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        addToast({
-          type: ToastType.Error,
-          message: err.message ?? err.toString(),
-        });
-      })
-      .finally(() => {
-        dismissToast(uploadingToastId);
+    try {
+      const uploadedFile = await this.application.files.finishUpload(
+        operation,
+        fileResult.name,
+        fileResult.ext
+      );
+
+      addToast({
+        type: ToastType.Success,
+        message: `Uploaded file "${uploadedFile.nameWithExt}"`,
       });
+    } catch (error) {
+      console.error(error);
+
+      addToast({
+        type: ToastType.Error,
+        message: (error as Error).message ?? (error as Error).toString(),
+      });
+    }
+
+    dismissToast(uploadingToastId);
   }
 }
