e1da2be83571fe92ef4fc91beed3df661ca1c171
Mo <mo@standardnotes.com>
2022-03-21 12:15:56 -0500

    fix: import

--- app/assets/javascripts/ui_models/app_state/account_menu_state.ts
+++ app/assets/javascripts/ui_models/app_state/account_menu_state.ts
@@ -6,9 +6,8 @@ import {
   observable,
   runInAction,
 } from 'mobx';
-import { ApplicationEvent, ContentType } from '@standardnotes/snjs';
+import { ApplicationEvent, ContentType, SNItem } from '@standardnotes/snjs';
 import { WebApplication } from '@/ui_models/application';
-import { SNItem } from '@standardnotes/snjs/dist/@types/models/core/item';
 import { AccountMenuPane } from '@/components/AccountMenu';
 
 type StructuredItemsCount = {
