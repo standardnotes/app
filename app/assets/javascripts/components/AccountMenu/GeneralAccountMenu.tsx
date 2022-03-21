59ffa9e52dda6c85b022bd1ceec69af04bc60554
Aman Harwara <amanharwara@protonmail.com>
2022-03-21 16:31:26 +0530

    feat: only wrap menu items with menu component

--- app/assets/javascripts/components/AccountMenu/GeneralAccountMenu.tsx
+++ app/assets/javascripts/components/AccountMenu/GeneralAccountMenu.tsx
@@ -58,12 +58,8 @@ export const GeneralAccountMenu: FunctionComponent<Props> = observer(
     const user = application.getUser();
 
     return (
-      <Menu
-        isOpen={appState.accountMenu.show}
-        a11yLabel="General account menu"
-        closeMenu={closeMenu}
-      >
-        <div className="flex items-center justify-between px-3 mt-1 mb-3">
+      <>
+        <div className="flex items-center justify-between px-3 mt-1 mb-1">
           <div className="sn-account-menu-headline">Account</div>
           <div className="flex cursor-pointer" onClick={closeMenu}>
             <Icon type="close" className="color-neutral" />
@@ -76,7 +72,7 @@ export const GeneralAccountMenu: FunctionComponent<Props> = observer(
               <div className="my-0.5 font-bold">{user.email}</div>
               <span className="color-neutral">{application.getHost()}</span>
             </div>
-            <div className="flex items-start justify-between px-3 mb-2">
+            <div className="flex items-start justify-between px-3 mb-3">
               {isSyncingInProgress ? (
                 <div className="flex items-center color-info font-semibold">
                   <div className="sk-spinner w-5 h-5 mr-2 spinner-info"></div>
@@ -101,7 +97,7 @@ export const GeneralAccountMenu: FunctionComponent<Props> = observer(
           </>
         ) : (
           <>
-            <div className="px-3 mb-1">
+            <div className="px-3 mb-2">
               <div className="mb-3 color-foreground">
                 You’re offline. Sign in to sync your notes and preferences
                 across all your devices and enable end-to-end encryption.
@@ -113,72 +109,78 @@ export const GeneralAccountMenu: FunctionComponent<Props> = observer(
             </div>
           </>
         )}
-        <AccountSwitcherOption mainApplicationGroup={mainApplicationGroup} />
-        <div className="h-1px my-2 bg-border"></div>
-        {user ? (
-          <MenuItem
-            type={MenuItemType.IconButton}
-            onClick={() => {
-              appState.accountMenu.closeAccountMenu();
-              appState.preferences.setCurrentPane('account');
-              appState.preferences.openPreferences();
-            }}
-          >
-            <Icon type="user" className={iconClassName} />
-            Account settings
-          </MenuItem>
-        ) : (
-          <>
+        <Menu
+          isOpen={appState.accountMenu.show}
+          a11yLabel="General account menu"
+          closeMenu={closeMenu}
+        >
+          <AccountSwitcherOption mainApplicationGroup={mainApplicationGroup} />
+          <div className="h-1px my-2 bg-border"></div>
+          {user ? (
             <MenuItem
               type={MenuItemType.IconButton}
               onClick={() => {
-                setMenuPane(AccountMenuPane.Register);
+                appState.accountMenu.closeAccountMenu();
+                appState.preferences.setCurrentPane('account');
+                appState.preferences.openPreferences();
               }}
             >
               <Icon type="user" className={iconClassName} />
-              Create free account
+              Account settings
             </MenuItem>
-            <MenuItem
-              type={MenuItemType.IconButton}
-              onClick={() => {
-                setMenuPane(AccountMenuPane.SignIn);
-              }}
-            >
-              <Icon type="signIn" className={iconClassName} />
-              Sign in
-            </MenuItem>
-          </>
-        )}
-        <MenuItem
-          className="justify-between"
-          type={MenuItemType.IconButton}
-          onClick={() => {
-            appState.accountMenu.closeAccountMenu();
-            appState.preferences.setCurrentPane('help-feedback');
-            appState.preferences.openPreferences();
-          }}
-        >
-          <div className="flex items-center">
-            <Icon type="help" className={iconClassName} />
-            Help &amp; feedback
-          </div>
-          <span className="color-neutral">v{appState.version}</span>
-        </MenuItem>
-        {user ? (
-          <>
-            <MenuItemSeparator />
-            <MenuItem
-              type={MenuItemType.IconButton}
-              onClick={() => {
-                appState.accountMenu.setSigningOut(true);
-              }}
-            >
-              <Icon type="signOut" className={iconClassName} />
-              Sign out and clear local data
-            </MenuItem>
-          </>
-        ) : null}
-      </Menu>
+          ) : (
+            <>
+              <MenuItem
+                type={MenuItemType.IconButton}
+                onClick={() => {
+                  setMenuPane(AccountMenuPane.Register);
+                }}
+              >
+                <Icon type="user" className={iconClassName} />
+                Create free account
+              </MenuItem>
+              <MenuItem
+                type={MenuItemType.IconButton}
+                onClick={() => {
+                  setMenuPane(AccountMenuPane.SignIn);
+                }}
+              >
+                <Icon type="signIn" className={iconClassName} />
+                Sign in
+              </MenuItem>
+            </>
+          )}
+          <MenuItem
+            className="justify-between"
+            type={MenuItemType.IconButton}
+            onClick={() => {
+              appState.accountMenu.closeAccountMenu();
+              appState.preferences.setCurrentPane('help-feedback');
+              appState.preferences.openPreferences();
+            }}
+          >
+            <div className="flex items-center">
+              <Icon type="help" className={iconClassName} />
+              Help &amp; feedback
+            </div>
+            <span className="color-neutral">v{appState.version}</span>
+          </MenuItem>
+          {user ? (
+            <>
+              <MenuItemSeparator />
+              <MenuItem
+                type={MenuItemType.IconButton}
+                onClick={() => {
+                  appState.accountMenu.setSigningOut(true);
+                }}
+              >
+                <Icon type="signOut" className={iconClassName} />
+                Sign out and clear local data
+              </MenuItem>
+            </>
+          ) : null}
+        </Menu>
+      </>
     );
   }
 );
