809183d18aa3a0a522a3550ec78b68d55636e335
Aman Harwara <amanharwara@protonmail.com>
2022-03-23 15:20:27 +0530

    feat: implement isFilePreviewable util

--- app/assets/javascripts/components/Files/isFilePreviewable.ts
+++ app/assets/javascripts/components/Files/isFilePreviewable.ts
@@ -1,4 +1,12 @@
 export const isFileTypePreviewable = (fileType: string) => {
-  /** @TODO */
-  return true;
+  const isImage = fileType.startsWith('image/');
+  const isVideo = fileType.startsWith('video/');
+  const isAudio = fileType.startsWith('audio/');
+  const isPdf = fileType === 'application/pdf';
+
+  if (isImage || isVideo || isAudio || isPdf) {
+    return true;
+  }
+
+  return false;
 };
