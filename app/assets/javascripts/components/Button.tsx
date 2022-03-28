e42e62d1832be1418c62cb8c100304a7338738b7
Aman Harwara <amanharwara@protonmail.com>
2022-03-28 16:53:11 +0530

    feat: add disabled styles

--- app/assets/javascripts/components/Button.tsx
+++ app/assets/javascripts/components/Button.tsx
@@ -12,17 +12,23 @@ const buttonClasses: { [type in ButtonType]: string } = {
   danger: `${baseClass} bg-default color-danger border-solid border-main border-1 focus:bg-contrast hover:bg-contrast`,
 };
 
+const disabledClasses: { [type in ButtonType]: string } = {
+  normal: 'color-grey-2 focus:bg-default hover:bg-default',
+  primary: 'bg-grey-2 focus:brightness-default hover:brightness-default',
+  danger: 'color-grey-2 focus:bg-default hover:bg-default',
+};
+
 type ButtonProps = JSXInternal.HTMLAttributes<HTMLButtonElement> & {
   children?: ComponentChildren;
   className?: string;
-  variant: ButtonType;
+  variant?: ButtonType;
   label?: string;
 };
 
 export const Button: FunctionComponent<ButtonProps> = forwardRef(
   (
     {
-      variant,
+      variant = 'normal',
       label,
       className = '',
       disabled = false,
@@ -33,11 +39,12 @@ export const Button: FunctionComponent<ButtonProps> = forwardRef(
   ) => {
     const buttonClass = buttonClasses[variant];
     const cursorClass = disabled ? 'cursor-not-allowed' : 'cursor-pointer';
+    const disabledClass = disabled ? disabledClasses[variant] : '';
 
     return (
       <button
         type="button"
-        className={`${buttonClass} ${cursorClass} ${className}`}
+        className={`${buttonClass} ${disabledClass} ${cursorClass} ${className}`}
         disabled={disabled}
         ref={ref}
         {...props}
