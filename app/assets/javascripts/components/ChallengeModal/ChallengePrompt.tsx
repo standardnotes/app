45d591285b59b7ac434d99ac3ae5fc52117a649b
Aman Harwara <amanharwara@protonmail.com>
2022-03-29 16:34:48 +0530

    style: remove unnecessary fragments

--- app/assets/javascripts/components/ChallengeModal/ChallengePrompt.tsx
+++ app/assets/javascripts/components/ChallengeModal/ChallengePrompt.tsx
@@ -41,58 +41,51 @@ export const ChallengeModalPrompt: FunctionComponent<Props> = ({
   return (
     <>
       {prompt.validation === ChallengeValidation.ProtectionSessionDuration ? (
-        <>
-          <div className="sk-horizontal-group mt-3">
-            <div className="sk-p sk-bold">Allow protected access for</div>
-            {ProtectionSessionDurations.map((option) => {
-              const selected =
-                option.valueInSeconds === values[prompt.id].value;
+        <div className="sk-horizontal-group mt-3">
+          <div className="sk-p sk-bold">Allow protected access for</div>
+          {ProtectionSessionDurations.map((option) => {
+            const selected = option.valueInSeconds === values[prompt.id].value;
 
-              return (
-                <label
-                  className={`cursor-pointer ${
-                    selected
-                      ? 'bg-info color-info-contrast rounded px-2 py-1.5'
-                      : 'color-info hover:underline'
-                  }`}
-                >
-                  <input
-                    type="radio"
-                    name={`session-duration-${prompt.id}`}
-                    className={'appearance-none m-0'}
-                    style={{
-                      marginRight: 0,
-                    }}
-                    checked={selected}
-                    onChange={(event) => {
-                      event.preventDefault();
-                      onValueChange(option.valueInSeconds, prompt);
-                    }}
-                  />
-                  {option.label}
-                </label>
-              );
-            })}
-          </div>
-        </>
+            return (
+              <label
+                className={`cursor-pointer ${
+                  selected
+                    ? 'bg-info color-info-contrast rounded px-2 py-1.5'
+                    : 'color-info hover:underline'
+                }`}
+              >
+                <input
+                  type="radio"
+                  name={`session-duration-${prompt.id}`}
+                  className={'appearance-none m-0'}
+                  style={{
+                    marginRight: 0,
+                  }}
+                  checked={selected}
+                  onChange={(event) => {
+                    event.preventDefault();
+                    onValueChange(option.valueInSeconds, prompt);
+                  }}
+                />
+                {option.label}
+              </label>
+            );
+          })}
+        </div>
+      ) : prompt.secureTextEntry ? (
+        <DecoratedPasswordInput
+          ref={inputRef}
+          placeholder={prompt.placeholder}
+          className={`w-full max-w-68 ${isInvalid ? 'border-danger' : ''}`}
+          onChange={(value) => onValueChange(value, prompt)}
+        />
       ) : (
-        <>
-          {prompt.secureTextEntry ? (
-            <DecoratedPasswordInput
-              ref={inputRef}
-              placeholder={prompt.placeholder}
-              className={`w-full max-w-68 ${isInvalid ? 'border-danger' : ''}`}
-              onChange={(value) => onValueChange(value, prompt)}
-            />
-          ) : (
-            <DecoratedInput
-              ref={inputRef}
-              placeholder={prompt.placeholder}
-              className={`w-full max-w-68 ${isInvalid ? 'border-danger' : ''}`}
-              onChange={(value) => onValueChange(value, prompt)}
-            />
-          )}
-        </>
+        <DecoratedInput
+          ref={inputRef}
+          placeholder={prompt.placeholder}
+          className={`w-full max-w-68 ${isInvalid ? 'border-danger' : ''}`}
+          onChange={(value) => onValueChange(value, prompt)}
+        />
       )}
       {isInvalid && (
         <div className="text-sm color-danger mt-2">
