df5eec785f064e274c7bdfb29852c927dca3d01a
Mo <mo@standardnotes.com>
2022-04-20 13:07:19 -0500

    fix: maintain aspect ratio of image when previewing

--- app/assets/javascripts/Components/Files/ImagePreview.tsx
+++ app/assets/javascripts/Components/Files/ImagePreview.tsx
@@ -1,6 +1,6 @@
 import { IconType } from '@standardnotes/snjs'
 import { FunctionComponent } from 'preact'
-import { useEffect, useRef, useState } from 'preact/hooks'
+import { useRef, useState } from 'preact/hooks'
 import { IconButton } from '../Button/IconButton'
 
 type Props = {
@@ -11,19 +11,16 @@ export const ImagePreview: FunctionComponent<Props> = ({ objectUrl }) => {
   const initialImgHeightRef = useRef<number>()
 
   const [imageZoomPercent, setImageZoomPercent] = useState(100)
-  const [imageHeight, setImageHeight] = useState(initialImgHeightRef.current)
-
-  useEffect(() => {
-    if (initialImgHeightRef.current) {
-      setImageHeight(imageZoomPercent * (initialImgHeightRef.current / 100))
-    }
-  }, [imageZoomPercent])
 
   return (
     <div className="flex items-center justify-center w-full h-full overflow-auto">
       <img
         src={objectUrl}
-        height={imageHeight}
+        style={{
+          width: '100%',
+          height: `${imageZoomPercent}%`,
+          'object-fit': 'contain',
+        }}
         ref={(imgElement) => {
           if (!initialImgHeightRef.current) {
             initialImgHeightRef.current = imgElement?.height
@@ -32,16 +29,6 @@ export const ImagePreview: FunctionComponent<Props> = ({ objectUrl }) => {
       />
       <div className="flex items-center absolute bottom-6 py-1 px-3 bg-default border-1 border-solid border-main rounded">
         <span className="mr-1.5">Zoom:</span>
-        <IconButton
-          className="hover:bg-contrast p-1 rounded"
-          icon="add"
-          title="Zoom In"
-          focusable={true}
-          onClick={() => {
-            setImageZoomPercent((percent) => percent + 10)
-          }}
-        />
-        <span className="mx-2">{imageZoomPercent}%</span>
         <IconButton
           className="hover:bg-contrast p-1 rounded"
           icon={'subtract' as IconType}
@@ -56,6 +43,16 @@ export const ImagePreview: FunctionComponent<Props> = ({ objectUrl }) => {
                 return percent
               }
             })
+          }}
+        />
+        <span className="mx-2">{imageZoomPercent}%</span>
+        <IconButton
+          className="hover:bg-contrast p-1 rounded"
+          icon="add"
+          title="Zoom In"
+          focusable={true}
+          onClick={() => {
+            setImageZoomPercent((percent) => percent + 10)
           }}
         />
       </div>
