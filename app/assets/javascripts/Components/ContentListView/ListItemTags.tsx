38f15fef872f91f24e353abd8339be7b2dfe783a
Aman Harwara <amanharwara@protonmail.com>
2022-05-25 20:58:04 +0530

    fix: react key warning

--- app/assets/javascripts/Components/ContentListView/ListItemTags.tsx
+++ app/assets/javascripts/Components/ContentListView/ListItemTags.tsx
@@ -12,7 +12,10 @@ export const ListItemTags: FunctionComponent<{
   return (
     <div className="flex flex-wrap mt-1.5 text-xs gap-2">
       {tags.map((tag) => (
-        <span className="inline-flex items-center py-1 px-1.5 bg-passive-4-opacity-variant color-foreground rounded-0.5">
+        <span
+          className="inline-flex items-center py-1 px-1.5 bg-passive-4-opacity-variant color-foreground rounded-0.5"
+          key={tag}
+        >
           <Icon type="hashtag" className="sn-icon--small color-passive-1 mr-1" />
           <span>{tag}</span>
         </span>
