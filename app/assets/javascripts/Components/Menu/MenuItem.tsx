d123731ced1f4c09be8a92a1094f76586865a6b4
Aman Harwara <amanharwara@protonmail.com>
2022-05-25 21:48:54 +0530

    refactor: menu & menu item

--- app/assets/javascripts/Components/Menu/MenuItem.tsx
+++ app/assets/javascripts/Components/Menu/MenuItem.tsx
@@ -1,6 +1,4 @@
-import { ComponentChildren, FunctionComponent, VNode } from 'react'
-import { forwardRef, Ref } from 'preact/compat'
-import { JSXInternal } from 'preact/src/jsx'
+import { forwardRef, FunctionComponent, MouseEventHandler, ReactNode, Ref } from 'react'
 import { Icon } from '@/Components/Icon/Icon'
 import { Switch, SwitchProps } from '@/Components/Switch/Switch'
 import { IconType } from '@standardnotes/snjs'
@@ -14,8 +12,8 @@ export enum MenuItemType {
 
 type MenuItemProps = {
   type: MenuItemType
-  children: ComponentChildren
-  onClick?: JSXInternal.MouseEventHandler<HTMLButtonElement>
+  children: ReactNode
+  onClick?: MouseEventHandler<HTMLButtonElement>
   onChange?: SwitchProps['onChange']
   onBlur?: (event: { relatedTarget: EventTarget | null }) => void
   className?: string
@@ -25,7 +23,7 @@ type MenuItemProps = {
   tabIndex?: number
 }
 
-export const MenuItem: FunctionComponent<MenuItemProps> = forwardRef(
+export const MenuItem = forwardRef(
   (
     {
       children,
@@ -42,63 +40,46 @@ export const MenuItem: FunctionComponent<MenuItemProps> = forwardRef(
     ref: Ref<HTMLButtonElement>,
   ) => {
     return type === MenuItemType.SwitchButton && typeof onChange === 'function' ? (
-      <button
-        className="sn-dropdown-item focus:bg-info-backdrop focus:shadow-none justify-between"
-        onClick={() => {
-          onChange(!checked)
-        }}
-        onBlur={onBlur}
-        tabIndex={typeof tabIndex === 'number' ? tabIndex : FOCUSABLE_BUT_NOT_TABBABLE}
-        role="menuitemcheckbox"
-        aria-checked={checked}
-      >
-        <span className="flex flex-grow items-center">{children}</span>
-        <Switch className="px-0" checked={checked} />
-      </button>
+      <li className="list-style-none" role="none">
+        <button
+          ref={ref}
+          className="sn-dropdown-item focus:bg-info-backdrop focus:shadow-none justify-between"
+          onClick={() => {
+            onChange(!checked)
+          }}
+          onBlur={onBlur}
+          tabIndex={typeof tabIndex === 'number' ? tabIndex : FOCUSABLE_BUT_NOT_TABBABLE}
+          role="menuitemcheckbox"
+          aria-checked={checked}
+        >
+          <span className="flex flex-grow items-center">{children}</span>
+          <Switch className="px-0" checked={checked} />
+        </button>
+      </li>
     ) : (
-      <button
-        ref={ref}
-        role={type === MenuItemType.RadioButton ? 'menuitemradio' : 'menuitem'}
-        tabIndex={typeof tabIndex === 'number' ? tabIndex : FOCUSABLE_BUT_NOT_TABBABLE}
-        className={`sn-dropdown-item focus:bg-info-backdrop focus:shadow-none ${className}`}
-        onClick={onClick}
-        onBlur={onBlur}
-        {...(type === MenuItemType.RadioButton ? { 'aria-checked': checked } : {})}
-      >
-        {type === MenuItemType.IconButton && icon ? <Icon type={icon} className={iconClassName} /> : null}
-        {type === MenuItemType.RadioButton && typeof checked === 'boolean' ? (
-          <div className={`pseudo-radio-btn ${checked ? 'pseudo-radio-btn--checked' : ''} flex-shrink-0`}></div>
-        ) : null}
-        {children}
-      </button>
+      <li className="list-style-none" role="none">
+        <button
+          ref={ref}
+          role={type === MenuItemType.RadioButton ? 'menuitemradio' : 'menuitem'}
+          tabIndex={typeof tabIndex === 'number' ? tabIndex : FOCUSABLE_BUT_NOT_TABBABLE}
+          className={`sn-dropdown-item focus:bg-info-backdrop focus:shadow-none ${className}`}
+          onClick={onClick}
+          onBlur={onBlur}
+          {...(type === MenuItemType.RadioButton ? { 'aria-checked': checked } : {})}
+        >
+          {type === MenuItemType.IconButton && icon ? <Icon type={icon} className={iconClassName} /> : null}
+          {type === MenuItemType.RadioButton && typeof checked === 'boolean' ? (
+            <div className={`pseudo-radio-btn ${checked ? 'pseudo-radio-btn--checked' : ''} flex-shrink-0`}></div>
+          ) : null}
+          {children}
+        </button>
+      </li>
     )
   },
 )
 
-export const MenuItemSeparator: FunctionComponent = () => <div role="separator" className="h-1px my-2 bg-border"></div>
-
-type ListElementProps = {
-  isFirstMenuItem: boolean
-  children: ComponentChildren
-}
-
-export const MenuItemListElement: FunctionComponent<ListElementProps> = forwardRef(
-  ({ children, isFirstMenuItem }: ListElementProps, ref: Ref<HTMLLIElement>) => {
-    const child = children as VNode<unknown>
-    return (
-      <li className="list-style-none" role="none" ref={ref}>
-        {{
-          ...child,
-          props: {
-            ...(child.props ? { ...child.props } : {}),
-            ...(child.type === MenuItem
-              ? {
-                  tabIndex: isFirstMenuItem ? 0 : -1,
-                }
-              : {}),
-          },
-        }}
-      </li>
-    )
-  },
+export const MenuItemSeparator: FunctionComponent = () => (
+  <li className="list-style-none" role="none">
+    <div role="separator" className="h-1px my-2 bg-border" />
+  </li>
 )
