9f2986c5126982ca79c1ba8b44475a054fc6ef00
Aman Harwara <amanharwara@protonmail.com>
2022-05-28 20:06:06 +0530

    feat: make application.dealloced observable

--- app/assets/javascripts/Components/DeallocateHandler/DeallocateHandler.tsx
+++ app/assets/javascripts/Components/DeallocateHandler/DeallocateHandler.tsx
@@ -1,5 +1,4 @@
 import { WebApplication } from '@/UIModels/Application'
-import { isStateDealloced } from '@/UIModels/AppState/AbstractState'
 import { observer } from 'mobx-react-lite'
 import { FunctionComponent } from 'react'
 
@@ -8,9 +7,7 @@ type Props = {
 }
 
 const DeallocateHandler: FunctionComponent<Props> = ({ application, children }) => {
-  const appState = application.getAppState()
-
-  if (application.dealloced || isStateDealloced(appState)) {
+  if (application.dealloced) {
     return null
   }
 
