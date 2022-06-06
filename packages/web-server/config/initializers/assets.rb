defe6d997bec45518e89bc419bc2813734614a51
Mo <mo@standardnotes.com>
2022-06-06 18:11:14 -0500

    refactor(wip): move web server into package

--- packages/web-server/config/initializers/assets.rb
+++ packages/web-server/config/initializers/assets.rb
@@ -1,6 +1,6 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.1'
 
-Rails.application.config.assets.paths << Rails.root.join('..', 'packages', 'web', 'dist')
+Rails.application.config.assets.paths << Rails.root.join('..', 'web', 'dist')
 
 Rails.application.config.assets.precompile += ['app.js', 'app.css', 'ionicons.eot', 'ionicons.ttf', 'ionicons.woff']
