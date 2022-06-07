7073c2f136b915f25d3b7ba3816c27bc98814c24
Karol Sójko <karolsojko@protonmail.com>
2022-06-07 14:36:13 +0200

    fix: start command in docker entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,7 +8,7 @@ case "$COMMAND" in
     echo "Prestart Step 1/1 - Removing server lock"
     rm -f /app/packages/web-server/tmp/pids/server.pid
     echo "Starting Server..."
-    yarn start
+    yarn start:web
     ;;
 
   'start-local' )
