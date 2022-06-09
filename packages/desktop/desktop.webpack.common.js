41582a9f626108073a7cd6f6f7548fc9af1b1799
Mo <mo@standardnotes.com>
2022-06-09 09:15:55 -0500

    fix: enable hoisting only for mobile package

--- packages/desktop/desktop.webpack.common.js
+++ packages/desktop/desktop.webpack.common.js
@@ -87,7 +87,7 @@ module.exports = function ({ onlyTranspileTypescript = false, experimentalFeatur
             to: 'web',
           },
           {
-            from: '../web/node_modules/@standardnotes/components/dist/',
+            from: '../../node_modules/@standardnotes/components/dist/',
             to: 'web/components',
           },
           {
