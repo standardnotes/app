fd24b473fe3da30deefcae2744a990e44bdbc3a2
Johnny Almonte <johnny243@users.noreply.github.com>
2022-06-11 01:25:51 -0400

    feat: build themes using webpack

---
+++ packages/components/theme.webpack.config.js
@@ -0,0 +1,34 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
+module.exports = {
+  mode: 'production',
+  output: {},
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: false,
+              url: false,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "dist.css"
+    }),
+    new RemoveEmptyScriptsPlugin(),
+  ],
+};
