0585501295f4e680bc4451feebc2dace3da8feff
Johnny Almonte <johnny243@users.noreply.github.com>
2022-06-14 21:05:04 -0400

    fix: ignore warnings

--- packages/components/src/org.standardnotes.advanced-checklist/config-overrides.js
+++ packages/components/src/org.standardnotes.advanced-checklist/config-overrides.js
@@ -1,11 +1,10 @@
 module.exports = function override(config, env) {
-  return {
-    ...config,
-    output: {
-      ...config.output,
-      filename: 'static/js/[name].js',
-      chunkFilename: 'static/js/[name].chunk.js',
-      assetModuleFilename: 'static/media/[name].[ext]',
-    }
+  config.ignoreWarnings = [/Failed to parse source map/]
+  config.output = {
+    ...config.output,
+    filename: 'static/js/[name].js',
+    chunkFilename: 'static/js/[name].chunk.js',
+    assetModuleFilename: 'static/media/[name].[ext]',
   }
+  return config
 }
