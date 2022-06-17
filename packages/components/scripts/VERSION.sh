e94132ff283d2bc70c1144bfa5db95422d377008
Mo <mo@standardnotes.com>
2022-06-17 08:23:27 -0500

    chore(workflow): compile components with latest version set

---
+++ packages/components/scripts/VERSION.sh
@@ -0,0 +1,10 @@
+
+echo 'Compiling components from' $(pwd)
+
+yarn components:clean && yarn components:compile
+
+echo "Packaging component assets and zips..."
+
+node scripts/package.mjs
+git add dist
+(git commit -m 'chore(release): components') || true
