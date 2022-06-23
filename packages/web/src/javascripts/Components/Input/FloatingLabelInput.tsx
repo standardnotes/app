bad3524ec3c0e1efeda5447edb63cfaa7b2d2019
Aman Harwara <amanharwara@protonmail.com>
2022-06-23 20:18:36 +0530

    feat: use tailwind classes

--- packages/web/src/javascripts/Components/Input/FloatingLabelInput.tsx
+++ packages/web/src/javascripts/Components/Input/FloatingLabelInput.tsx
@@ -39,8 +39,8 @@ const FloatingLabelInput = forwardRef(
 
     const INPUT_CLASSNAME = `w-full h-full ${
       focused || value ? 'pt-6 pb-2' : 'py-2.5'
-    } px-3 text-input border-1 border-solid border-main rounded placeholder-medium text-input focus:ring-info ${
-      isInvalid ? 'border-danger placeholder-dark-red' : ''
+    } px-3 text-input border-1 border-solid border-main rounded placeholder:font-medium text-input focus:ring-info ${
+      isInvalid ? 'border-danger placeholder:color-danger' : ''
     } ${inputClassName}`
 
     const handleFocus = () => setFocused(true)
