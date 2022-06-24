dc9485d91fbe66050a1c9702e52f0f733506dedd
Aman Harwara <amanharwara@protonmail.com>
2022-06-24 16:11:18 +0530

    fix: styles

--- packages/web/src/javascripts/Components/Preferences/PreferencesComponents/MenuItem.tsx
+++ packages/web/src/javascripts/Components/Preferences/PreferencesComponents/MenuItem.tsx
@@ -12,13 +12,15 @@ interface Props {
 
 const PreferencesMenuItem: FunctionComponent<Props> = ({ iconType, label, selected, onClick, hasBubble }) => (
   <div
-    className={`preferences-menu-item select-none ${selected ? 'selected' : ''}`}
+    className={`preferences-menu-item box-border w-auto h-auto rounded min-w-42 py-2 px-4 flex flex-row items-center justify-start text-sm cursor-pointer border-solid border select-none hover:border-border hover:bg-default ${
+      selected ? 'selected border-info text-info font-bold' : 'border-transparent'
+    }`}
     onClick={(e) => {
       e.preventDefault()
       onClick()
     }}
   >
-    <Icon className="icon" type={iconType} />
+    <Icon className={`icon text-base ${selected ? 'text-info' : 'text-neutral'}`} type={iconType} />
     <div className="min-w-1" />
     {label}
     {hasBubble && <span className="ml-1 text-warning">⚠️</span>}
