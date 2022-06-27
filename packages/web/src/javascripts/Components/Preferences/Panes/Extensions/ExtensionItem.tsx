4ff5214eaa56c247ae7cadc6c7087df1bd999970
Mo <mo@standardnotes.com>
2022-06-27 12:53:20 -0500

    Revert "fix: remove option to reset native editors"
    This reverts commit 05596a66d8c33fd26af0c998746131479cc4eff6.

--- packages/web/src/javascripts/Components/Preferences/Panes/Extensions/ExtensionItem.tsx
+++ packages/web/src/javascripts/Components/Preferences/Panes/Extensions/ExtensionItem.tsx
@@ -62,12 +62,17 @@ const ExtensionItem: FunctionComponent<ExtensionItemProps> = ({ application, ext
 
       {isThirParty && localInstallable && <UseHosted offlineOnly={offlineOnly} toggleOfflineOnly={toggleOfflineOnly} />}
 
-      <div className="min-h-2" />
-      {isThirParty && (
+      <>
+        <div className="min-h-2" />
         <div className="flex flex-row">
-          <Button className="min-w-20" variant="normal" label={'Uninstall'} onClick={() => uninstall(extension)} />
+          <Button
+            className="min-w-20"
+            variant="normal"
+            label={isThirParty ? 'Uninstall' : 'Reset'}
+            onClick={() => uninstall(extension)}
+          />
         </div>
-      )}
+      </>
     </PreferencesSegment>
   )
 }
