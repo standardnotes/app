6c907dbc04349bd19d101818ea696f8ddedbf18d
Johnny Almonte <johnny243@users.noreply.github.com>
2022-06-28 11:39:12 -0400

    fix: reorder icon color

--- packages/components/src/Packages/Editors/org.standardnotes.advanced-checklist/src/common/components/icons/ReorderIcon.tsx
+++ packages/components/src/Packages/Editors/org.standardnotes.advanced-checklist/src/common/components/icons/ReorderIcon.tsx
@@ -8,7 +8,7 @@ export const ReorderIcon: React.FC<ReorderIconProps> = ({ highlight = false }) =
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={`sn-icon block ${highlight ? 'info' : ''}`}
+      className={`sn-icon block ${highlight ? 'info' : 'neutral'}`}
       data-testid="reorder-icon"
     >
       <path d="M3 15H21V13H3V15ZM3 19H21V17H3V19ZM3 11H21V9H3V11ZM3 5V7H21V5H3Z" />
