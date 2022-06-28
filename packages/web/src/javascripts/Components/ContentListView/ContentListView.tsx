462c780344b639d5b0e37b7ac229e73438a6c747
Aman Harwara <amanharwara@protonmail.com>
2022-06-28 17:07:01 +0530

    fix: border

--- packages/web/src/javascripts/Components/ContentListView/ContentListView.tsx
+++ packages/web/src/javascripts/Components/ContentListView/ContentListView.tsx
@@ -173,7 +173,7 @@ const ContentListView: FunctionComponent<Props> = ({
       ref={itemsViewPanelRef}
     >
       <div className="content">
-        <div id="items-title-bar" className="section-title-bar">
+        <div id="items-title-bar" className="section-title-bar border-border border-b border-solid">
           <div id="items-title-bar-container">
             <ContentListHeader
               application={application}
