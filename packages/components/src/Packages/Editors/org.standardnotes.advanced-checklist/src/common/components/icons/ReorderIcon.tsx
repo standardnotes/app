c6471edd241c7dae07e436950be18dcc667aa908
Johnny Almonte <johnny243@users.noreply.github.com>
2022-06-28 11:56:58 -0400

    fix: change reorder icon design

--- packages/components/src/Packages/Editors/org.standardnotes.advanced-checklist/src/common/components/icons/ReorderIcon.tsx
+++ packages/components/src/Packages/Editors/org.standardnotes.advanced-checklist/src/common/components/icons/ReorderIcon.tsx
@@ -5,13 +5,13 @@ type ReorderIconProps = {
 export const ReorderIcon: React.FC<ReorderIconProps> = ({ highlight = false }) => {
   return (
     <svg
-      viewBox="0 0 24 24"
+      viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={`sn-icon block ${highlight ? 'info' : 'neutral'}`}
       data-testid="reorder-icon"
     >
-      <path d="M3 15H21V13H3V15ZM3 19H21V17H3V19ZM3 11H21V9H3V11ZM3 5V7H21V5H3Z" />
+      <path d="M17 5V6.66667H3V5H17ZM3 15H17V13.3333H3V15ZM3 10.8333H17V9.16667H3V10.8333Z" />
     </svg>
   )
 }
