37c3f4a360a5bd27b6206b31edde0497370f8874
Mo <mo@standardnotes.com>
2022-07-04 15:27:13 -0500

    chore: use gh-pages branch approach instead

--- docs/assets/js/6ebcf8c8.e99403d4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[39],{110:function(e,t,n){"use strict";n.r(t);var s=n(0),c=n.n(s),i=(n(143),n(22));t.default=function(){return c.a.createElement(i.Redirect,{to:"/listed/styles"})}}}]);
--- docs/assets/js/78e28ad6.00edc84f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[41],{112:function(e,t,n){"use strict";n.r(t);var o=n(0),a=n.n(o),c=(n(143),n(22));t.default=function(){return a.a.createElement(c.Redirect,{to:"/listed/about"})}}}]);
