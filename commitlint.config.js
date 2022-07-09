f3341fa176e40ade583a2215a5819155891ff765
Zhengqbbb <1074059947@qq.com>
2022-07-09 22:48:34 +0800

    chore: add `cz-git` as commitizen adapter and CLI

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,37 @@
+const fs = require('fs')
+const path = require('path')
+const { execSync } = require('child_process')
+
+const packages = fs
+  .readdirSync(path.resolve(__dirname, 'packages'), { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name.replace(/^sncrypto-/, ''))
+
+// precomputed scope
+const scopeComplete = execSync('git status --porcelain || true')
+  .toString()
+  .trim()
+  .split('\n')
+  .find((r) => ~r.indexOf('M  packages'))
+  ?.replace(/(\/)/g, '%%')
+  ?.match(/packages%%((\w|-)*)/)?.[1]
+  ?.replace(/^sncrypto-/, '')
+
+/** @type {import('cz-git').UserConfig} */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [0],
+  },
+  prompt: {
+    alias: {
+      f: 'docs: fix typos',
+      b: 'chore: bump dep version',
+    },
+    scopes: [...new Set(packages), 'scripts', 'docker'],
+    customScopesAlign: !scopeComplete ? 'top' : 'bottom',
+    defaultScope: scopeComplete,
+    allowEmptyIssuePrefixs: false,
+    allowCustomIssuePrefixs: false,
   },
 }
