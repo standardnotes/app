62f5ae6bd3916f422d323a808ed0fc66c02795d0
Aman Harwara <amanharwara@protonmail.com>
2022-07-27 16:31:19 +0530

    feat: init popover context

--- packages/web/src/javascripts/Components/Popover/Popover.tsx
+++ packages/web/src/javascripts/Components/Popover/Popover.tsx
@@ -1,5 +1,14 @@
+import { UuidGenerator } from '@standardnotes/snjs'
+import { createContext, useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react'
 import PositionedPopoverContent from './PositionedPopoverContent'
 import { PopoverProps } from './Types'
+
+type PopoverContextData = {
+  registerChildPopover: (id: string) => void
+  unregisterChildPopover: (id: string) => void
+}
+
+const PopoverContext = createContext<PopoverContextData | null>(null)
 
 type Props = PopoverProps & {
   open: boolean
@@ -16,8 +25,49 @@ const Popover = ({
   side,
   togglePopover,
 }: Props) => {
+  const popoverId = useRef(UuidGenerator.GenerateUuid())
+
+  const parentPopoverContext = useContext(PopoverContext)
+
+  useEffect(() => {
+    const currentId = popoverId.current
+
+    if (parentPopoverContext) {
+      parentPopoverContext.registerChildPopover(currentId)
+    }
+
+    return () => {
+      parentPopoverContext?.unregisterChildPopover(currentId)
+    }
+  }, [parentPopoverContext])
+
+  const [childPopovers, setChildPopovers] = useState<Set<string>>(new Set())
+
+  useEffect(() => {
+    console.log(childPopovers)
+  }, [childPopovers])
+
+  const registerChildPopover = useCallback((id: string) => {
+    setChildPopovers((childPopovers) => new Set(childPopovers.add(id)))
+  }, [])
+
+  const unregisterChildPopover = useCallback((id: string) => {
+    setChildPopovers((childPopovers) => {
+      childPopovers.delete(id)
+      return new Set(childPopovers)
+    })
+  }, [])
+
+  const contextValue = useMemo(
+    () => ({
+      registerChildPopover,
+      unregisterChildPopover,
+    }),
+    [registerChildPopover, unregisterChildPopover],
+  )
+
   return open ? (
-    <>
+    <PopoverContext.Provider value={contextValue}>
       <PositionedPopoverContent
         align={align}
         anchorElement={anchorElement}
@@ -29,7 +79,7 @@ const Popover = ({
       >
         {children}
       </PositionedPopoverContent>
-    </>
+    </PopoverContext.Provider>
   ) : null
 }
 
