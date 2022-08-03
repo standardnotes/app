483cac90cfddb13854be1998e7b96b50aee86464
Karol Sójko <karolsojko@protonmail.com>
2022-08-03 14:51:59 +0200

    feat(web): remove web-server and change web to a static website deployment (#1360)
    * feat(web): add deploying static app
    
    * fix(web): linter ignore patterns
    
    * fix(web): cloudfront invalidation and favicons placement
    
    * feat(web-server): remove web-server package in favour of web
    
    * fix(web): remove linter parallel jobs due to oom issue
    
    * fix(mobile): eslintignore paths
    
    * fix(web): copy of components
    
    * fix(web): include building components in building all process
    
    * fix(web): copying of components

--- packages/web-server/config/initializers/lograge.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.configure do
-  config.lograge.enabled = true
-
-  # Generate log in JSON
-  config.lograge.formatter = Lograge::Formatters::Json.new
-  config.lograge.ignore_actions = ['HealthCheckController#index']
-end
--- packages/web-server/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  get "/healthcheck" => "health_check#index"
-
-  get '*unmatched_route', to: 'application#route_not_found'
-
-  root 'application#app'
-end
