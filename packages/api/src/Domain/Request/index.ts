a1845c98ae01650c26413e33b169c44cde32986d
Karol Sójko <karolsojko@protonmail.com>
2022-08-31 15:21:56 +0200

    feat(api): add subscriptions invitation operations on server side

--- packages/api/src/Domain/Request/index.ts
+++ packages/api/src/Domain/Request/index.ts
@@ -1,10 +1,7 @@
 export * from './ApiEndpointParam'
-<<<<<<< HEAD
 export * from './Subscription/SubscriptionInviteAcceptRequestParams'
 export * from './Subscription/SubscriptionInviteCancelRequestParams'
 export * from './Subscription/SubscriptionInviteDeclineRequestParams'
 export * from './Subscription/SubscriptionInviteListRequestParams'
-=======
->>>>>>> 8f40b81b (feat(api): add subscription server and client services and interfaces)
 export * from './Subscription/SubscriptionInviteRequestParams'
 export * from './User/UserRegistrationRequestParams'
