053f542fadeb34dd931ee98b05592f8291b54fb1
Vardan Hakobyan <vardan_live@live.com>
2022-09-12 12:49:58 +0400

    chore: better variable naming

--- packages/web/src/javascripts/Utils/ManageSubscription.ts
+++ packages/web/src/javascripts/Utils/ManageSubscription.ts
@@ -1,14 +1,14 @@
 import { SNApplication } from '@standardnotes/snjs'
 
 export function openSubscriptionDashboard(application: SNApplication): void {
-  const winOpen = window.open('', '_blank')
+  const windowProxy = window.open('', '_blank')
   application
     .getNewSubscriptionToken()
     .then((token) => {
       if (!token) {
         return
       }
-      ;(winOpen as WindowProxy).location = `${window.dashboardUrl}?subscription_token=${token}`
+      ;(windowProxy as WindowProxy).location = `${window.dashboardUrl}?subscription_token=${token}`
     })
     .catch(console.error)
 }
