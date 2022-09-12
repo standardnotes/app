a161fca8e4302b166bdd841405893293d4edd229
Vardan Hakobyan <vardan_live@live.com>
2022-09-12 12:03:47 +0400

    fix: ability to open a new tab on iOS Safari

--- packages/web/src/javascripts/Utils/ManageSubscription.ts
+++ packages/web/src/javascripts/Utils/ManageSubscription.ts
@@ -1,13 +1,14 @@
 import { SNApplication } from '@standardnotes/snjs'
 
 export function openSubscriptionDashboard(application: SNApplication): void {
+  const winOpen = window.open('', '_blank')
   application
     .getNewSubscriptionToken()
     .then((token) => {
       if (!token) {
         return
       }
-      window.open(`${window.dashboardUrl}?subscription_token=${token}`)
+      ;(winOpen as WindowProxy).location = `${window.dashboardUrl}?subscription_token=${token}`
     })
     .catch(console.error)
 }
