2d0ee10226687df1d24926b9408ce270557a5b57
Karol Sójko <karolsojko@protonmail.com>
2022-09-15 12:00:29 +0200

    feat: sharing subscriptions UI (#1567)
    * feat(web): add ui for subscription sharing
    
    * fix(web): add missing triggers
    
    * fix(snjs): setting authorization token on http service
    
    * fix(web): add alert upon invite failure
    
    * fix(web): display invitations list
    
    * fix(web): canceling subscription invitations
    
    * fix(web): fonts
    
    * fix(web): linter issues
    
    * fix: click event handler
    
    * fix: styles
    
    * feat: update styles
    
    * feat: don't show bottom separator if all invites used
    
    * fix(web): references to alert service
    
    * fix(web): remove usebeforeunload
    
    Co-authored-by: Aman Harwara <amanharwara@protonmail.com>

---
+++ packages/web/src/javascripts/Components/Preferences/Panes/Account/SubscriptionSharing/Invite/Invite.tsx
@@ -0,0 +1,128 @@
+import { FunctionComponent, useState } from 'react'
+
+import ModalDialog from '@/Components/Shared/ModalDialog'
+import ModalDialogButtons from '@/Components/Shared/ModalDialogButtons'
+import ModalDialogDescription from '@/Components/Shared/ModalDialogDescription'
+import ModalDialogLabel from '@/Components/Shared/ModalDialogLabel'
+import Button from '@/Components/Button/Button'
+import { WebApplication } from '@/Application/Application'
+import { isEmailValid } from '@/Utils'
+import { SubscriptionController } from '@/Controllers/Subscription/SubscriptionController'
+
+import InviteForm from './InviteForm'
+import InviteSuccess from './InviteSuccess'
+
+enum SubmitButtonTitles {
+  Default = 'Send Invitation',
+  Sending = 'Sending...',
+  Finish = 'Finish',
+}
+
+enum Steps {
+  InitialStep,
+  FinishStep,
+}
+
+type Props = {
+  onCloseDialog: () => void
+  application: WebApplication
+  subscriptionState: SubscriptionController
+}
+
+const Invite: FunctionComponent<Props> = ({ onCloseDialog, application, subscriptionState }) => {
+  const [submitButtonTitle, setSubmitButtonTitle] = useState(SubmitButtonTitles.Default)
+  const [inviteeEmail, setInviteeEmail] = useState('')
+  const [isContinuing, setIsContinuing] = useState(false)
+  const [lockContinue, setLockContinue] = useState(false)
+  const [currentStep, setCurrentStep] = useState(Steps.InitialStep)
+
+  const validateInviteeEmail = async () => {
+    if (!isEmailValid(inviteeEmail)) {
+      application.alertService
+        .alert('The email you entered has an invalid format. Please review your input and try again.')
+        .catch(console.error)
+
+      return false
+    }
+
+    return true
+  }
+
+  const handleDialogClose = () => {
+    if (lockContinue) {
+      application.alertService.alert('Cannot close window until pending tasks are complete.').catch(console.error)
+    } else {
+      onCloseDialog()
+    }
+  }
+
+  const resetProgressState = () => {
+    setSubmitButtonTitle(SubmitButtonTitles.Default)
+    setIsContinuing(false)
+  }
+
+  const processInvite = async () => {
+    setLockContinue(true)
+
+    const success = await subscriptionState.sendSubscriptionInvitation(inviteeEmail)
+
+    setLockContinue(false)
+
+    return success
+  }
+
+  const handleSubmit = async () => {
+    if (lockContinue || isContinuing) {
+      return
+    }
+
+    if (currentStep === Steps.FinishStep) {
+      handleDialogClose()
+
+      return
+    }
+
+    setIsContinuing(true)
+    setSubmitButtonTitle(SubmitButtonTitles.Sending)
+
+    const valid = await validateInviteeEmail()
+
+    if (!valid) {
+      resetProgressState()
+
+      return
+    }
+
+    const success = await processInvite()
+    if (!success) {
+      application.alertService
+        .alert('We could not send the invitation. Please try again or contact support if the issue persists.')
+        .catch(console.error)
+
+      resetProgressState()
+
+      return
+    }
+
+    setIsContinuing(false)
+    setSubmitButtonTitle(SubmitButtonTitles.Finish)
+    setCurrentStep(Steps.FinishStep)
+  }
+
+  return (
+    <div>
+      <ModalDialog>
+        <ModalDialogLabel closeDialog={handleDialogClose}>Invite</ModalDialogLabel>
+        <ModalDialogDescription className="flex flex-row items-center px-4.5">
+          {currentStep === Steps.InitialStep && <InviteForm setInviteeEmail={setInviteeEmail} />}
+          {currentStep === Steps.FinishStep && <InviteSuccess />}
+        </ModalDialogDescription>
+        <ModalDialogButtons className="px-4.5">
+          <Button className="min-w-20" primary label={submitButtonTitle} onClick={handleSubmit} />
+        </ModalDialogButtons>
+      </ModalDialog>
+    </div>
+  )
+}
+
+export default Invite
--- packages/web/src/javascripts/Controllers/Subscription/SubscriptionController.ts
+++ packages/web/src/javascripts/Controllers/Subscription/SubscriptionController.ts
@@ -4,6 +4,10 @@ import {
   ClientDisplayableError,
   convertTimestampToMilliseconds,
   InternalEventBus,
+  Invitation,
+  InvitationStatus,
+  SubscriptionClientInterface,
+  Uuid,
 } from '@standardnotes/snjs'
 import { action, computed, makeObservable, observable } from 'mobx'
 import { WebApplication } from '../../Application/Application'
@@ -12,28 +16,40 @@ import { AvailableSubscriptions } from './AvailableSubscriptionsType'
 import { Subscription } from './SubscriptionType'
 
 export class SubscriptionController extends AbstractViewController {
+  private readonly ALLOWED_SUBSCRIPTION_INVITATIONS = 5
+
   userSubscription: Subscription | undefined = undefined
   availableSubscriptions: AvailableSubscriptions | undefined = undefined
+  subscriptionInvitations: Invitation[] | undefined = undefined
 
   override deinit() {
     super.deinit()
     ;(this.userSubscription as unknown) = undefined
     ;(this.availableSubscriptions as unknown) = undefined
+    ;(this.subscriptionInvitations as unknown) = undefined
 
     destroyAllObjectProperties(this)
   }
 
-  constructor(application: WebApplication, eventBus: InternalEventBus) {
+  constructor(
+    application: WebApplication,
+    eventBus: InternalEventBus,
+    private subscriptionManager: SubscriptionClientInterface,
+  ) {
     super(application, eventBus)
 
     makeObservable(this, {
       userSubscription: observable,
       availableSubscriptions: observable,
+      subscriptionInvitations: observable,
 
       userSubscriptionName: computed,
       userSubscriptionExpirationDate: computed,
       isUserSubscriptionExpired: computed,
       isUserSubscriptionCanceled: computed,
+      usedInvitationsCount: computed,
+      allowedInvitationsCount: computed,
+      allInvitationsUsed: computed,
 
       setUserSubscription: action,
       setAvailableSubscriptions: action,
@@ -43,6 +59,7 @@ export class SubscriptionController extends AbstractViewController {
       application.addEventObserver(async () => {
         if (application.hasAccount()) {
           this.getSubscriptionInfo().catch(console.error)
+          this.reloadSubscriptionInvitations().catch(console.error)
         }
       }, ApplicationEvent.Launched),
     )
@@ -50,12 +67,14 @@ export class SubscriptionController extends AbstractViewController {
     this.disposers.push(
       application.addEventObserver(async () => {
         this.getSubscriptionInfo().catch(console.error)
+        this.reloadSubscriptionInvitations().catch(console.error)
       }, ApplicationEvent.SignedIn),
     )
 
     this.disposers.push(
       application.addEventObserver(async () => {
         this.getSubscriptionInfo().catch(console.error)
+        this.reloadSubscriptionInvitations().catch(console.error)
       }, ApplicationEvent.UserRolesChanged),
     )
   }
@@ -91,12 +110,48 @@ export class SubscriptionController extends AbstractViewController {
     return Boolean(this.userSubscription?.cancelled)
   }
 
+  get usedInvitationsCount(): number {
+    return (
+      this.subscriptionInvitations?.filter((invitation) =>
+        [InvitationStatus.Accepted, InvitationStatus.Sent].includes(invitation.status),
+      ).length ?? 0
+    )
+  }
+
+  get allowedInvitationsCount(): number {
+    return this.ALLOWED_SUBSCRIPTION_INVITATIONS
+  }
+
+  get allInvitationsUsed(): boolean {
+    return this.usedInvitationsCount === this.ALLOWED_SUBSCRIPTION_INVITATIONS
+  }
+
   public setUserSubscription(subscription: Subscription): void {
     this.userSubscription = subscription
   }
 
   public setAvailableSubscriptions(subscriptions: AvailableSubscriptions): void {
     this.availableSubscriptions = subscriptions
+  }
+
+  async sendSubscriptionInvitation(inviteeEmail: string): Promise<boolean> {
+    const success = await this.subscriptionManager.inviteToSubscription(inviteeEmail)
+
+    if (success) {
+      await this.reloadSubscriptionInvitations()
+    }
+
+    return success
+  }
+
+  async cancelSubscriptionInvitation(invitationUuid: Uuid): Promise<boolean> {
+    const success = await this.subscriptionManager.cancelInvitation(invitationUuid)
+
+    if (success) {
+      await this.reloadSubscriptionInvitations()
+    }
+
+    return success
   }
 
   private async getAvailableSubscriptions() {
@@ -124,5 +179,9 @@ export class SubscriptionController extends AbstractViewController {
   private async getSubscriptionInfo() {
     await this.getSubscription()
     await this.getAvailableSubscriptions()
+  }
+
+  private async reloadSubscriptionInvitations(): Promise<void> {
+    this.subscriptionInvitations = await this.subscriptionManager.listSubscriptionInvitations()
   }
 }
