805ecede1fe41dab1fe543aea9f321878bbdcf7b
Aman Harwara <amanharwara@protonmail.com>
2022-09-23 15:41:51 +0530

    fix: protected overlay on mobile

--- packages/web/src/javascripts/Components/ProtectedItemOverlay/ProtectedItemOverlay.tsx
+++ packages/web/src/javascripts/Components/ProtectedItemOverlay/ProtectedItemOverlay.tsx
@@ -1,5 +1,6 @@
 import { ViewControllerManager } from '@/Controllers/ViewControllerManager'
 import Button from '@/Components/Button/Button'
+import MobileItemsListButton from '../NoteGroupView/MobileItemsListButton'
 
 type Props = {
   viewControllerManager: ViewControllerManager
@@ -14,24 +15,31 @@ const ProtectedItemOverlay = ({ viewControllerManager, onViewItem, hasProtection
     : `Add a passcode or create an account to require authentication to view this ${itemType}.`
 
   return (
-    <div className="flex max-w-md flex-col items-center justify-center text-center">
-      <h1 className="m-0 w-full text-2xl font-bold">This {itemType} is protected</h1>
-      <p className="mt-2 w-full text-lg">{instructionText}</p>
-      <div className="mt-4 flex gap-3">
-        {!hasProtectionSources && (
-          <Button
-            primary
-            small
-            onClick={() => {
-              viewControllerManager.accountMenuController.setShow(true)
-            }}
-          >
-            Open account menu
-          </Button>
-        )}
-        <Button small onClick={onViewItem}>
-          {hasProtectionSources ? 'Authenticate' : `View ${itemType}`}
-        </Button>
+    <div aria-label="Protected overlay" className="section editor sn-component">
+      <div className="flex h-full flex-grow flex-col justify-center md:flex-row md:items-center">
+        <div className="mb-auto p-4 md:hidden">
+          <MobileItemsListButton />
+        </div>
+        <div className="mb-auto flex max-w-md flex-col items-center justify-center text-center md:mb-0">
+          <h1 className="m-0 w-full text-2xl font-bold">This {itemType} is protected</h1>
+          <p className="mt-2 w-full text-lg">{instructionText}</p>
+          <div className="mt-4 flex gap-3">
+            {!hasProtectionSources && (
+              <Button
+                primary
+                small
+                onClick={() => {
+                  viewControllerManager.accountMenuController.setShow(true)
+                }}
+              >
+                Open account menu
+              </Button>
+            )}
+            <Button small onClick={onViewItem}>
+              {hasProtectionSources ? 'Authenticate' : `View ${itemType}`}
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   )
