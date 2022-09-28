47b54db816de4e8d44384b13114ac39c5934ad81
Aman Harwara <amanharwara@protonmail.com>
2022-09-29 00:49:09 +0530

    fix: only show nav menu button when necessary

--- packages/web/src/javascripts/Components/NavigationMenu/NavigationMenu.tsx
+++ packages/web/src/javascripts/Components/NavigationMenu/NavigationMenu.tsx
@@ -7,7 +7,7 @@ export const NavigationMenuButton = () => {
 
   return (
     <button
-      className="bg-text-padding mr-3 inline-flex h-8 min-w-8 cursor-pointer items-center justify-center rounded-full border border-solid border-border align-middle text-neutral hover:bg-contrast focus:bg-contrast lg:hidden"
+      className="bg-text-padding mr-3 inline-flex h-8 min-w-8 cursor-pointer items-center justify-center rounded-full border border-solid border-border align-middle text-neutral hover:bg-contrast focus:bg-contrast md:hidden pointer-coarse:md-only:inline-flex pointer-coarse:lg-only:inline-flex"
       onClick={() => {
         if (selectedPane === AppPaneId.Items || selectedPane === AppPaneId.Editor) {
           toggleAppPane(AppPaneId.Navigation)
