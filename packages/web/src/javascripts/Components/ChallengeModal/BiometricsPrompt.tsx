3f9d3ceffac206645ff9e5b66e68bf2e7b9cd0e2
Aman Harwara <amanharwara@protonmail.com>
2022-09-28 21:26:20 +0530

    fix: improve biometrics input on mobile webview (#1658)

---
+++ packages/web/src/javascripts/Components/ChallengeModal/BiometricsPrompt.tsx
@@ -0,0 +1,44 @@
+import { WebApplication } from '@/Application/Application'
+import { ChallengePrompt } from '@standardnotes/services'
+import { RefObject, useState } from 'react'
+import Button from '../Button/Button'
+import Icon from '../Icon/Icon'
+import { InputValue } from './InputValue'
+
+type Props = {
+  application: WebApplication
+  onValueChange: (value: InputValue['value'], prompt: ChallengePrompt) => void
+  prompt: ChallengePrompt
+  buttonRef: RefObject<HTMLButtonElement>
+}
+
+const BiometricsPrompt = ({ application, onValueChange, prompt, buttonRef }: Props) => {
+  const [authenticated, setAuthenticated] = useState(false)
+
+  return (
+    <div className="min-w-76">
+      <Button
+        primary
+        fullWidth
+        colorStyle={authenticated ? 'success' : 'info'}
+        onClick={async () => {
+          const authenticated = await application.mobileDevice.authenticateWithBiometrics()
+          setAuthenticated(authenticated)
+          onValueChange(authenticated, prompt)
+        }}
+        ref={buttonRef}
+      >
+        {authenticated ? (
+          <span className="flex items-center justify-center gap-3">
+            <Icon type="check-circle" />
+            Biometrics successful
+          </span>
+        ) : (
+          'Tap to use biometrics'
+        )}
+      </Button>
+    </div>
+  )
+}
+
+export default BiometricsPrompt
