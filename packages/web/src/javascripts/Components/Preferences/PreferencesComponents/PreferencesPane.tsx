2acea7b877ccbc46c08ed4ccfbd5860687f22e4a
Aman Harwara <amanharwara@protonmail.com>
2022-10-04 17:02:09 +0530

    fix: preferences on mobile webview when zoomed in

--- packages/web/src/javascripts/Components/Preferences/PreferencesComponents/PreferencesPane.tsx
+++ packages/web/src/javascripts/Components/Preferences/PreferencesComponents/PreferencesPane.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent, ReactNode } from 'react'
 const PreferencesPane: FunctionComponent<{ children?: ReactNode }> = ({ children }) => (
   <div className="flex min-h-0 flex-grow flex-col overflow-y-auto text-foreground md:flex-row">
     <div className="flex flex-grow flex-col items-center px-3 py-6 md:px-0">
-      <div className="flex flex-col md:w-125 md:max-w-125">
+      <div className="flex max-w-full flex-col md:w-125 md:max-w-125">
         {children != undefined && Array.isArray(children) ? children.filter((child) => child != undefined) : children}
       </div>
     </div>
