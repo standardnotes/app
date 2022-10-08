aff4c6e5904e0fce01a9176bc1b12e5105de392a
Aman Harwara <amanharwara@protonmail.com>
2022-10-08 18:17:49 +0530

    feat: move dark theme to top

--- packages/web/src/javascripts/Utils/SortThemes.ts
+++ packages/web/src/javascripts/Utils/SortThemes.ts
@@ -1,4 +1,7 @@
 import { ThemeItem } from '@/Components/QuickSettingsMenu/ThemeItem'
+import { FeatureIdentifier } from '@standardnotes/snjs'
+
+const isDarkModeTheme = (theme: ThemeItem) => theme.identifier === FeatureIdentifier.FocusedTheme
 
 export const sortThemes = (a: ThemeItem, b: ThemeItem) => {
   const aIsLayerable = a.component?.isLayerable()
@@ -8,6 +11,8 @@ export const sortThemes = (a: ThemeItem, b: ThemeItem) => {
     return 1
   } else if (!aIsLayerable && bIsLayerable) {
     return -1
+  } else if (!isDarkModeTheme(a) && isDarkModeTheme(b)) {
+    return 1
   } else {
     return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
   }
