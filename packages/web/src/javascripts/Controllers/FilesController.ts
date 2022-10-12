806955d7450e09ac9af67efd17280929cbd54fb5
Aman Harwara <amanharwara@protonmail.com>
2022-10-12 20:59:56 +0530

    fix: lint error

--- packages/web/src/javascripts/Controllers/FilesController.ts
+++ packages/web/src/javascripts/Controllers/FilesController.ts
@@ -99,7 +99,7 @@ export class FilesController extends AbstractViewController {
   reloadAttachedFiles = () => {
     const note = this.notesController.firstSelectedNote
     if (note) {
-      this.attachedFiles = this.application.items.getSortedFilesForItem(note)
+      this.attachedFiles = this.application.items.getSortedFilesLinkingToItem(note)
     }
   }
 
