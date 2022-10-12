2c24b5e66657f6b53f78fc493f939020be69e775
Aman Harwara <amanharwara@protonmail.com>
2022-10-12 19:37:29 +0530

    feat: status

--- packages/web/src/javascripts/Components/LinkedItems/LinkedItemBubble.tsx
+++ packages/web/src/javascripts/Components/LinkedItems/LinkedItemBubble.tsx
@@ -102,7 +102,9 @@ const LinkedItemBubble = ({
       <span className="max-w-290px flex items-center overflow-hidden overflow-ellipsis whitespace-nowrap">
         {tagTitle && <span className="text-passive-1">{tagTitle.titlePrefix}</span>}
         <span className="flex items-center gap-1">
-          {isBidirectional ? (link.relationWithSelectedItem === 'direct' ? 'Linked: ' : 'Linked By: ') : null}
+          <span className={!isBidirectional ? 'hidden group-focus:block' : ''}>
+            {link.relationWithSelectedItem === 'direct' ? 'Linked: ' : 'Linked By: '}
+          </span>
           {link.item.title}
         </span>
       </span>
