d0c413cd150ea835a4cb22aefadd6d9e477b1039
Aman Harwara <amanharwara@protonmail.com>
2022-10-14 18:33:43 +0530

    fix: don't render editor view on mobile if editor pane not selected (#1790)

--- packages/web/src/javascripts/Controllers/PaneController.ts
+++ packages/web/src/javascripts/Controllers/PaneController.ts
@@ -1,19 +1,39 @@
 import { AppPaneId } from './../Components/ResponsivePane/AppPaneMetadata'
 import { isMobileScreen } from '@/Utils'
 import { makeObservable, observable, action } from 'mobx'
+import { Disposer } from '@/Types/Disposer'
+import { MediaQueryBreakpoints } from '@/Hooks/useMediaQuery'
 
 export class PaneController {
   currentPane: AppPaneId = isMobileScreen() ? AppPaneId.Items : AppPaneId.Editor
   previousPane: AppPaneId = isMobileScreen() ? AppPaneId.Items : AppPaneId.Editor
+  isInMobileView = isMobileScreen()
+  protected disposers: Disposer[] = []
 
   constructor() {
     makeObservable(this, {
       currentPane: observable,
       previousPane: observable,
+      isInMobileView: observable,
 
       setCurrentPane: action,
       setPreviousPane: action,
+      setIsInMobileView: action,
     })
+
+    window.matchMedia(MediaQueryBreakpoints.md).addEventListener('change', this.mediumScreenMQHandler)
+  }
+
+  deinit() {
+    window.matchMedia(MediaQueryBreakpoints.md).removeEventListener('change', this.mediumScreenMQHandler)
+  }
+
+  mediumScreenMQHandler = (event: MediaQueryListEvent) => {
+    if (event.matches) {
+      this.isInMobileView = false
+    } else {
+      this.isInMobileView = true
+    }
   }
 
   setCurrentPane(pane: AppPaneId): void {
@@ -22,5 +42,9 @@ export class PaneController {
 
   setPreviousPane(pane: AppPaneId): void {
     this.previousPane = pane
+  }
+
+  setIsInMobileView(isInMobileView: boolean) {
+    this.isInMobileView = isInMobileView
   }
 }
