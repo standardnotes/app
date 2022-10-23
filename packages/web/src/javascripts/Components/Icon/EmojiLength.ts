79a254e4fd5847a806bd3dfdcf9c6bbbe67bcea8
Mo <mo@standardnotes.com>
2022-10-23 10:44:04 -0500

    chore: remove log

--- packages/web/src/javascripts/Components/Icon/EmojiLength.ts
+++ packages/web/src/javascripts/Components/Icon/EmojiLength.ts
@@ -2,7 +2,6 @@ export function getEmojiLength(emoji: string): number {
   try {
     return [...new Intl.Segmenter().segment(emoji)].length
   } catch (error) {
-    console.log('getEmojiLength > error', error)
     return [...emoji].length
   }
 }
