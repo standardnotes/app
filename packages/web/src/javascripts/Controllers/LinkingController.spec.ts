55072a90d436f6a9bd8d6f43bae54298e1f588f1
Aman Harwara <amanharwara@protonmail.com>
2022-10-25 19:25:37 +0530

    feat: new tests

--- packages/web/src/javascripts/Controllers/LinkingController.spec.ts
+++ packages/web/src/javascripts/Controllers/LinkingController.spec.ts
@@ -1,5 +1,13 @@
 import { WebApplication } from '@/Application/Application'
-import { FileItem, InternalEventBus } from '@standardnotes/snjs'
+import {
+  AnonymousReference,
+  ContentReferenceType,
+  ContentType,
+  FileItem,
+  FileToNoteReference,
+  InternalEventBus,
+  SNNote,
+} from '@standardnotes/snjs'
 import { FilesController } from './FilesController'
 import { ItemListController } from './ItemList/ItemListController'
 import { LinkingController } from './LinkingController'
@@ -7,12 +15,25 @@ import { NavigationController } from './Navigation/NavigationController'
 import { SelectedItemsController } from './SelectedItemsController'
 import { SubscriptionController } from './Subscription/SubscriptionController'
 
-const createFile = (name: string, archived = false, trashed = false, uuid?: string) => {
+const createNote = (name: string, options?: Partial<SNNote>) => {
   return {
     title: name,
-    archived,
-    trashed,
-    uuid: uuid ? uuid : String(Math.random()),
+    archived: false,
+    trashed: false,
+    uuid: String(Math.random()),
+    content_type: ContentType.Note,
+    ...options,
+  } as jest.Mocked<SNNote>
+}
+
+const createFile = (name: string, options?: Partial<FileItem>) => {
+  return {
+    title: name,
+    archived: false,
+    trashed: false,
+    uuid: String(Math.random()),
+    content_type: ContentType.File,
+    ...options,
   } as jest.Mocked<FileItem>
 }
 
@@ -47,50 +68,154 @@ describe('LinkingController', () => {
     linkingController.setServicesPostConstruction(itemListController, filesController, subscriptionController)
   })
 
-  it("should not be valid result if it doesn't match query", () => {
-    const searchQuery = 'test'
+  describe('isValidSearchResult', () => {
+    it("should not be valid result if it doesn't match query", () => {
+      const searchQuery = 'test'
 
-    const file = createFile('anotherFile')
+      const file = createFile('anotherFile')
 
-    const isFileValidResult = linkingController.isValidSearchResult(file, searchQuery)
+      const isFileValidResult = linkingController.isValidSearchResult(file, searchQuery)
 
-    expect(isFileValidResult).toBeFalsy()
+      expect(isFileValidResult).toBeFalsy()
+    })
+
+    it('should not be valid result if item is archived or trashed', () => {
+      const searchQuery = 'test'
+
+      const archived = createFile('test', { archived: true })
+
+      const trashed = createFile('test', { trashed: true })
+
+      const isArchivedFileValidResult = linkingController.isValidSearchResult(archived, searchQuery)
+      expect(isArchivedFileValidResult).toBeFalsy()
+
+      const isTrashedFileValidResult = linkingController.isValidSearchResult(trashed, searchQuery)
+      expect(isTrashedFileValidResult).toBeFalsy()
+    })
+
+    it('should not be valid result if result is active item', () => {
+      const searchQuery = 'test'
+
+      const activeItem = createFile('test', { uuid: 'same-uuid' })
+
+      Object.defineProperty(itemListController, 'activeControllerItem', { value: activeItem })
+
+      const result = createFile('test', { uuid: 'same-uuid' })
+
+      const isFileValidResult = linkingController.isValidSearchResult(result, searchQuery)
+      expect(isFileValidResult).toBeFalsy()
+    })
+
+    it('should be valid result if it matches query even case insensitive', () => {
+      const searchQuery = 'test'
+
+      const file = createFile('TeSt')
+
+      const isFileValidResult = linkingController.isValidSearchResult(file, searchQuery)
+
+      expect(isFileValidResult).toBeTruthy()
+    })
   })
 
-  it('should not be valid result if item is archived or trashed', () => {
-    const searchQuery = 'test'
+  describe('isSearchResultAlreadyLinked', () => {
+    it('should be true if active item & result are same content type & active item references result', () => {
+      const activeItem = createFile('test', {
+        uuid: 'active-item',
+        references: [
+          {
+            reference_type: ContentReferenceType.FileToFile,
+            uuid: 'result',
+          } as AnonymousReference,
+        ],
+      })
+      const result = createFile('test', { uuid: 'result', references: [] })
 
-    const archived = createFile('test', true)
+      Object.defineProperty(itemListController, 'activeControllerItem', { value: activeItem })
 
-    const trashed = createFile('test', false, true)
+      const isFileAlreadyLinked = linkingController.isSearchResultAlreadyLinked(result)
+      expect(isFileAlreadyLinked).toBeTruthy()
+    })
 
-    const isArchivedFileValidResult = linkingController.isValidSearchResult(archived, searchQuery)
-    expect(isArchivedFileValidResult).toBeFalsy()
+    it('should be false if active item & result are same content type & result references active item', () => {
+      const activeItem = createFile('test', {
+        uuid: 'active-item',
+        references: [],
+      })
+      const result = createFile('test', {
+        uuid: 'result',
+        references: [
+          {
+            reference_type: ContentReferenceType.FileToFile,
+            uuid: 'active-item',
+          } as AnonymousReference,
+        ],
+      })
 
-    const isTrashedFileValidResult = linkingController.isValidSearchResult(trashed, searchQuery)
-    expect(isTrashedFileValidResult).toBeFalsy()
-  })
+      Object.defineProperty(itemListController, 'activeControllerItem', { value: activeItem })
 
-  it('should not be valid result if result is active item', () => {
-    const searchQuery = 'test'
+      const isFileAlreadyLinked = linkingController.isSearchResultAlreadyLinked(result)
+      expect(isFileAlreadyLinked).toBeFalsy()
+    })
 
-    const activeItem = createFile('test', false, false, 'same-uuid')
+    it('should be true if active item & result are different content type & result references active item', () => {
+      const activeNote = createNote('test', {
+        uuid: 'active-note',
+        references: [],
+      })
 
-    Object.defineProperty(itemListController, 'activeControllerItem', { value: activeItem })
+      const fileResult = createFile('test', {
+        uuid: 'file-result',
+        references: [
+          {
+            reference_type: ContentReferenceType.FileToNote,
+            uuid: 'active-note',
+          } as FileToNoteReference,
+        ],
+      })
 
-    const result = createFile('test', false, false, 'same-uuid')
+      Object.defineProperty(itemListController, 'activeControllerItem', { value: activeNote })
 
-    const isFileValidResult = linkingController.isValidSearchResult(result, searchQuery)
-    expect(isFileValidResult).toBeFalsy()
-  })
+      const isFileResultAlreadyLinked = linkingController.isSearchResultAlreadyLinked(fileResult)
+      expect(isFileResultAlreadyLinked).toBeTruthy()
+    })
 
-  it('should be valid result if it matches query even case insensitive', () => {
-    const searchQuery = 'test'
+    it('should be true if active item & result are different content type & active item references result', () => {
+      const activeFile = createNote('test', {
+        uuid: 'active-file',
+        references: [
+          {
+            reference_type: ContentReferenceType.FileToNote,
+            uuid: 'note-result',
+          } as FileToNoteReference,
+        ],
+      })
 
-    const file = createFile('TeSt')
+      const noteResult = createFile('test', {
+        uuid: 'note-result',
+        references: [],
+      })
 
-    const isFileValidResult = linkingController.isValidSearchResult(file, searchQuery)
+      Object.defineProperty(itemListController, 'activeControllerItem', { value: activeFile })
 
-    expect(isFileValidResult).toBeTruthy()
+      const isNoteResultAlreadyLinked = linkingController.isSearchResultAlreadyLinked(noteResult)
+      expect(isNoteResultAlreadyLinked).toBeTruthy()
+    })
+
+    it('should be false if active item & result are different content type & neither references the other', () => {
+      const activeFile = createNote('test', {
+        uuid: 'active-file',
+        references: [],
+      })
+
+      const noteResult = createFile('test', {
+        uuid: 'note-result',
+        references: [],
+      })
+
+      Object.defineProperty(itemListController, 'activeControllerItem', { value: activeFile })
+
+      const isNoteResultAlreadyLinked = linkingController.isSearchResultAlreadyLinked(noteResult)
+      expect(isNoteResultAlreadyLinked).toBeFalsy()
+    })
   })
 })
