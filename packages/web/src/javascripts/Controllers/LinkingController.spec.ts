9d4aabe80bb011194de809543b2e53cc0180358b
Aman Harwara <amanharwara@protonmail.com>
2022-10-25 17:50:05 +0530

    fix: tests

--- packages/web/src/javascripts/Controllers/LinkingController.spec.ts
+++ packages/web/src/javascripts/Controllers/LinkingController.spec.ts
@@ -8,9 +8,12 @@ import {
   InternalEventBus,
   PayloadTimestampDefaults,
 } from '@standardnotes/snjs'
+import { FilesController } from './FilesController'
+import { ItemListController } from './ItemList/ItemListController'
 import { LinkingController } from './LinkingController'
 import { NavigationController } from './Navigation/NavigationController'
 import { SelectedItemsController } from './SelectedItemsController'
+import { SubscriptionController } from './Subscription/SubscriptionController'
 
 const createFile = (name: string) => {
   return new FileItem(
@@ -32,8 +35,15 @@ describe('LinkingController', () => {
   let selectionController: SelectedItemsController
   let eventBus: InternalEventBus
 
+  let itemListController: ItemListController
+  let filesController: FilesController
+  let subscriptionController: SubscriptionController
+
   beforeEach(() => {
     application = {} as jest.Mocked<WebApplication>
+    application.getPreference = jest.fn()
+    application.addSingleEventObserver = jest.fn()
+    application.streamItems = jest.fn()
 
     navigationController = {} as jest.Mocked<NavigationController>
 
@@ -41,7 +51,12 @@ describe('LinkingController', () => {
 
     eventBus = {} as jest.Mocked<InternalEventBus>
 
+    itemListController = {} as jest.Mocked<ItemListController>
+    filesController = {} as jest.Mocked<FilesController>
+    subscriptionController = {} as jest.Mocked<SubscriptionController>
+
     linkingController = new LinkingController(application, navigationController, selectionController, eventBus)
+    linkingController.setServicesPostConstruction(itemListController, filesController, subscriptionController)
   })
 
   it("should not be valid result if it doesn't match query", () => {
