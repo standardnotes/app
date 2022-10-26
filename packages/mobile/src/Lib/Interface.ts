ade4fa1d0d714b4e5322118fe12a2631f74cbac0
Aman Harwara <amanharwara@protonmail.com>
2022-10-27 01:40:09 +0530

    feat: add explicit type

--- packages/mobile/src/Lib/Interface.ts
+++ packages/mobile/src/Lib/Interface.ts
@@ -13,7 +13,7 @@ import {
   TransferPayload,
   UuidString,
 } from '@standardnotes/snjs'
-import { Alert, AppState, Linking, PermissionsAndroid, Platform, StatusBar } from 'react-native'
+import { Alert, AppState, AppStateStatus, Linking, PermissionsAndroid, Platform, StatusBar } from 'react-native'
 import FileViewer from 'react-native-file-viewer'
 import FingerprintScanner from 'react-native-fingerprint-scanner'
 import FlagSecure from 'react-native-flag-secure-android'
@@ -611,7 +611,7 @@ export class MobileDevice implements MobileDeviceInterface {
     return Array.from(this.componentUrls.values()).includes(url)
   }
 
-  async getAppState() {
+  async getAppState(): Promise<AppStateStatus> {
     return AppState.currentState
   }
 }
