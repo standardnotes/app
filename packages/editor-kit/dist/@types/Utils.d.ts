65486049a5612c36706a72be76ac5e31cdc1c2e7
Mo <mo@standardnotes.com>
2022-11-03 14:39:27 -0500

    chore: remove dist

--- packages/editor-kit/dist/@types/Utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const htmlToText: (html: string) => string;
-export declare const truncateString: (text: string, limit?: number) => string;
-export declare const sleep: (seconds: number) => Promise<void>;
