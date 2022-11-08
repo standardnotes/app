833e6ffa775ba7433201400102869dea86cfb6e7
Mo <mo@standardnotes.com>
2022-11-08 11:27:02 -0600

    chore: lint

--- packages/web/src/javascripts/Components/BlockEditor/Plugins/BlockPickerPlugin/Blocks.tsx
+++
@@ -1,7 +0,0 @@
-import { BlockPickerOption } from './BlockPickerOption'
-import { LexicalEditor, $createParagraphNode, $getSelection, $isRangeSelection, FORMAT_ELEMENT_COMMAND } from 'lexical'
-import { $wrapNodes } from '@lexical/selection'
-
-export function GetBlockPickerOptions(editor: LexicalEditor): BlockPickerOption[] {
-
-}
