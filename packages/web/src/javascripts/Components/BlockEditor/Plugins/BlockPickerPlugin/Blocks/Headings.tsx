0cf3381fb59c1db97574bf1c12b7f9b557a643fa
Mo <mo@standardnotes.com>
2022-11-09 08:53:05 -0600

    chore: lint

--- packages/web/src/javascripts/Components/BlockEditor/Plugins/BlockPickerPlugin/Blocks/Headings.tsx
+++ packages/web/src/javascripts/Components/BlockEditor/Plugins/BlockPickerPlugin/Blocks/Headings.tsx
@@ -2,12 +2,13 @@ import { BlockPickerOption } from '../BlockPickerOption'
 import { $wrapNodes } from '@lexical/selection'
 import { $getSelection, $isRangeSelection, LexicalEditor } from 'lexical'
 import { $createHeadingNode, HeadingTagType } from '@lexical/rich-text'
+import { LexicalIconName } from '@/Components/Icon/LexicalIcons'
 
 export function GetHeadingsBlocks(editor: LexicalEditor) {
   return Array.from({ length: 3 }, (_, i) => i + 1).map(
     (n) =>
       new BlockPickerOption(`Heading ${n}`, {
-        iconName: `h${n}`,
+        iconName: `h${n}` as LexicalIconName,
         keywords: ['heading', 'header', `h${n}`],
         onSelect: () =>
           editor.update(() => {
