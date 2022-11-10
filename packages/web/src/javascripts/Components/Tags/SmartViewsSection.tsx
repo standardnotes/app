b0a068ef016793929e8d14b8f59d481b1c6b0d92
Aman Harwara <amanharwara@protonmail.com>
2022-11-10 23:28:04 +0530

    feat: new smart view button in list

--- packages/web/src/javascripts/Components/Tags/SmartViewsSection.tsx
+++ packages/web/src/javascripts/Components/Tags/SmartViewsSection.tsx
@@ -1,16 +1,52 @@
+import { WebApplication } from '@/Application/Application'
+import { SMART_TAGS_FEATURE_NAME } from '@/Constants/Constants'
 import { ViewControllerManager } from '@/Controllers/ViewControllerManager'
+import { usePremiumModal } from '@/Hooks/usePremiumModal'
 import { observer } from 'mobx-react-lite'
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useCallback, useMemo } from 'react'
+import IconButton from '../Button/IconButton'
+import AddSmartViewModal from '../SmartViewBuilder/AddSmartViewModal'
+import { AddSmartViewModalController } from '../SmartViewBuilder/AddSmartViewModalController'
 import SmartViewsList from './SmartViewsList'
 
 type Props = {
+  application: WebApplication
   viewControllerManager: ViewControllerManager
 }
 
-const SmartViewsSection: FunctionComponent<Props> = ({ viewControllerManager }) => {
+const SmartViewsSection: FunctionComponent<Props> = ({ application, viewControllerManager }) => {
+  const premiumModal = usePremiumModal()
+  const addSmartViewModalController = useMemo(() => new AddSmartViewModalController(application), [application])
+
+  const createNewSmartView = useCallback(() => {
+    if (!viewControllerManager.featuresController.hasSmartViews) {
+      premiumModal.activate(SMART_TAGS_FEATURE_NAME)
+      return
+    }
+
+    addSmartViewModalController.setIsAddingSmartView(true)
+  }, [addSmartViewModalController, premiumModal, viewControllerManager.featuresController.hasSmartViews])
+
   return (
     <section>
+      <div className={'section-title-bar'}>
+        <div className="section-title-bar-header">
+          <div className="title text-base md:text-sm">
+            <span className="font-bold">Views</span>
+          </div>
+          <IconButton
+            focusable={true}
+            icon="add"
+            title="Create a new smart view"
+            className="p-0 text-neutral"
+            onClick={createNewSmartView}
+          />
+        </div>
+      </div>
       <SmartViewsList viewControllerManager={viewControllerManager} />
+      {addSmartViewModalController.isAddingSmartView && (
+        <AddSmartViewModal controller={addSmartViewModalController} platform={application.platform} />
+      )}
     </section>
   )
 }
