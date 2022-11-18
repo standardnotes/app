c639f88ea5906b5cd11bee9d6c24f05aaa2d1d07
Aman Harwara <amanharwara@protonmail.com>
2022-11-18 20:22:11 +0530

    fix: drag button visibility

--- packages/blocks-editor/src/Lexical/Plugins/DraggableBlockPlugin/index.tsx
+++ packages/blocks-editor/src/Lexical/Plugins/DraggableBlockPlugin/index.tsx
@@ -347,7 +347,19 @@ function useDraggableBlockMenu(
         draggable={true}
         onDragStart={onDragStart}
         onDragEnd={onDragEnd}>
-        <div className={isEditable ? 'icon' : ''} />
+        <div className={isEditable ? 'icon' : ''}>
+          <svg
+            className="text-text pointer-events-none"
+            xmlns="http://www.w3.org/2000/svg"
+            data-name="Layer 1"
+            viewBox="0 0 24 24"
+            fill="currentColor">
+            <path
+              stroke="currentColor"
+              d="M8.5 10a2 2 0 1 0 2 2 2 2 0 0 0-2-2Zm0 7a2 2 0 1 0 2 2 2 2 0 0 0-2-2Zm7-10a2 2 0 1 0-2-2 2 2 0 0 0 2 2Zm-7-4a2 2 0 1 0 2 2 2 2 0 0 0-2-2Zm7 14a2 2 0 1 0 2 2 2 2 0 0 0-2-2Zm0-7a2 2 0 1 0 2 2 2 2 0 0 0-2-2Z"
+            />
+          </svg>
+        </div>
       </div>
       <div className="draggable-block-target-line" ref={targetLineRef} />
     </>,
