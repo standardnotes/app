3117e4ba3bc9d234ce7e2cf4db085b7b35eade4d
Aman Harwara <amanharwara@protonmail.com>
2022-11-22 22:30:40 +0530

    fix: super editor don't load all files by default

--- packages/web/src/javascripts/Components/NoteView/SuperEditor/Plugins/EncryptedFilePlugin/Nodes/FileComponent.tsx
+++ packages/web/src/javascripts/Components/NoteView/SuperEditor/Plugins/EncryptedFilePlugin/Nodes/FileComponent.tsx
@@ -1,9 +1,12 @@
 import { BlockWithAlignableContents } from '@lexical/react/LexicalBlockWithAlignableContents'
-import { useMemo } from 'react'
-import { ElementFormatType, NodeKey } from 'lexical'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { COMMAND_PRIORITY_EDITOR, ElementFormatType, NodeKey } from 'lexical'
 import { useApplication } from '@/Components/ApplicationView/ApplicationProvider'
 import FilePreview from '@/Components/FilePreview/FilePreview'
 import { FileItem } from '@standardnotes/snjs'
+import Button from '@/Components/Button/Button'
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+import { LOAD_ALL_FILES_COMMAND, LOAD_FILE_COMMAND } from '../../Commands'
 
 export type FileComponentProps = Readonly<{
   className: Readonly<{
@@ -19,13 +22,59 @@ export function FileComponent({ className, format, nodeKey, fileUuid }: FileComp
   const application = useApplication()
   const file = useMemo(() => application.items.findItem<FileItem>(fileUuid), [application, fileUuid])
 
+  const [editor] = useLexicalComposerContext()
+  const [canLoad, setCanLoad] = useState(false)
+
+  useEffect(() => {
+    editor.registerCommand(
+      LOAD_FILE_COMMAND,
+      (payload) => {
+        if (payload === fileUuid) {
+          setCanLoad(true)
+          return true
+        }
+        return false
+      },
+      COMMAND_PRIORITY_EDITOR,
+    )
+  }, [editor, fileUuid])
+
+  useEffect(() => {
+    editor.registerCommand(
+      LOAD_ALL_FILES_COMMAND,
+      () => {
+        setCanLoad(true)
+        return false
+      },
+      COMMAND_PRIORITY_EDITOR,
+    )
+  }, [editor])
+
+  const loadCurrentFile = useCallback(() => {
+    editor.dispatchCommand(LOAD_FILE_COMMAND, fileUuid)
+  }, [editor, fileUuid])
+
+  const loadAllFiles = useCallback(() => {
+    editor.dispatchCommand(LOAD_ALL_FILES_COMMAND, '')
+  }, [editor])
+
   if (!file) {
     return <div>Unable to find file {fileUuid}</div>
   }
 
   return (
     <BlockWithAlignableContents className={className} format={format} nodeKey={nodeKey}>
-      <FilePreview file={file} application={application} />
+      {canLoad ? (
+        <FilePreview file={file} application={application} />
+      ) : (
+        <div className="mb-4 flex h-full flex-grow flex-col items-center justify-center gap-2">
+          <h1 className="m-0 text-2xl font-bold">{file.name}</h1>
+          <div className="flex gap-3">
+            <Button onClick={loadCurrentFile}>Load file</Button>
+            <Button onClick={loadAllFiles}>Load all files</Button>
+          </div>
+        </div>
+      )}
     </BlockWithAlignableContents>
   )
 }
