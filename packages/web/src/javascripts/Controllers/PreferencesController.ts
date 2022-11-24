a6ad5caf6e3ce340247392c092ccaeb32a32e7fd
Mo <mo@standardnotes.com>
2022-11-24 05:33:43 -0600

    chore: remove testing code

--- packages/web/src/javascripts/Controllers/PreferencesController.ts
+++ packages/web/src/javascripts/Controllers/PreferencesController.ts
@@ -4,10 +4,10 @@ import { PreferenceId, RootQueryParam } from '@standardnotes/ui-services'
 import { AbstractViewController } from './Abstract/AbstractViewController'
 import { WebApplication } from '@/Application/Application'
 
-const DEFAULT_PANE: PreferenceId = 'whats-new'
+const DEFAULT_PANE: PreferenceId = 'account'
 
 export class PreferencesController extends AbstractViewController {
-  private _open = true
+  private _open = false
   currentPane: PreferenceId = DEFAULT_PANE
 
   constructor(application: WebApplication, eventBus: InternalEventBus) {
