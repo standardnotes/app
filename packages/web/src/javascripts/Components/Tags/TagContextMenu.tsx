195cbdae454a00e3886e24a5f22483de4d9f306e
Aman Harwara <amanharwara@protonmail.com>
2022-11-27 21:27:15 +0530

    fix: types

--- packages/web/src/javascripts/Components/Tags/TagContextMenu.tsx
+++ packages/web/src/javascripts/Components/Tags/TagContextMenu.tsx
@@ -3,7 +3,6 @@ import { useCallback, useRef, useMemo } from 'react'
 import Icon from '@/Components/Icon/Icon'
 import Menu from '@/Components/Menu/Menu'
 import MenuItem from '@/Components/Menu/MenuItem'
-import { MenuItemType } from '@/Components/Menu/MenuItemType'
 import { usePremiumModal } from '@/Hooks/usePremiumModal'
 import { SNTag, VectorIconNameOrEmoji, DefaultTagIconName } from '@standardnotes/snjs'
 import { useCloseOnClickOutside } from '@/Hooks/useCloseOnClickOutside'
@@ -82,24 +81,24 @@ const TagContextMenu = ({ navigationController, isEntitledToFolders, selectedTag
             iconGridClassName="max-h-30"
           />
           <HorizontalSeparator classes="my-2" />
-          <MenuItem type={MenuItemType.IconButton} className={'justify-between py-1.5'} onClick={onClickStar}>
+          <MenuItem className={'justify-between py-1.5'} onClick={onClickStar}>
             <div className="flex items-center">
               <Icon type="star" className="mr-2 text-neutral" />
               {selectedTag.starred ? 'Unfavorite' : 'Favorite'}
             </div>
           </MenuItem>
-          <MenuItem type={MenuItemType.IconButton} className={'justify-between py-1.5'} onClick={onClickAddSubtag}>
+          <MenuItem className={'justify-between py-1.5'} onClick={onClickAddSubtag}>
             <div className="flex items-center">
               <Icon type="add" className="mr-2 text-neutral" />
               Add subtag
             </div>
             {!isEntitledToFolders && <Icon type={PremiumFeatureIconName} className={PremiumFeatureIconClass} />}
           </MenuItem>
-          <MenuItem type={MenuItemType.IconButton} className={'py-1.5'} onClick={onClickRename}>
+          <MenuItem className={'py-1.5'} onClick={onClickRename}>
             <Icon type="pencil-filled" className="mr-2 text-neutral" />
             Rename
           </MenuItem>
-          <MenuItem type={MenuItemType.IconButton} className={'py-1.5'} onClick={onClickDelete}>
+          <MenuItem className={'py-1.5'} onClick={onClickDelete}>
             <Icon type="trash" className="mr-2 text-danger" />
             <span className="text-danger">Delete</span>
           </MenuItem>
