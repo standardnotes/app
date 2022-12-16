e40c6b06a93df5a9405b9b5b840aa62ea719933d
Aman Harwara <amanharwara@protonmail.com>
2022-12-16 21:59:30 +0530

    feat: Added "Whats New" indicator to Preferences button

--- packages/ui-services/src/Changelog/ChangelogServiceInterface.ts
+++ packages/ui-services/src/Changelog/ChangelogServiceInterface.ts
@@ -1,6 +1,9 @@
 import { Changelog, ChangelogVersion } from './Changelog'
 
+export type ChangelogLastReadVersionListener = (version: string) => void
+
 export interface ChangelogServiceInterface {
+  addLastReadChangeListener(listener: ChangelogLastReadVersionListener): () => void
   getChangelog(): Promise<Changelog>
   getVersions(): Promise<ChangelogVersion[]>
   getDesktopDownloadsUrl(version: string): string
