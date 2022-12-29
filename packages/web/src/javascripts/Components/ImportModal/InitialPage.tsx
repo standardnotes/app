5d17da22ee36fe40ef1b2f9e10acbdc298ae1698
Aman Harwara <amanharwara@protonmail.com>
2022-12-29 20:18:30 +0530

    feat: drag-n-drop

--- packages/web/src/javascripts/Components/ImportModal/InitialPage.tsx
+++ packages/web/src/javascripts/Components/ImportModal/InitialPage.tsx
@@ -40,6 +40,16 @@ const ImportModalInitialPage = ({ dispatch }: Props) => {
       <button
         onClick={() => selectFiles()}
         className="flex min-h-[30vh] w-full flex-col items-center justify-center gap-2 rounded border-2 border-dashed border-info p-2 hover:border-4"
+        onDragStart={(e) => e.preventDefault()}
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={(e) => {
+          e.preventDefault()
+          const files = Array.from(e.dataTransfer.files)
+          dispatch({
+            type: 'setFiles',
+            files,
+          })
+        }}
       >
         <div className="text-lg font-semibold">Drag and drop files to auto-detect and import</div>
         <div className="text-sm">Or click to open file picker</div>
