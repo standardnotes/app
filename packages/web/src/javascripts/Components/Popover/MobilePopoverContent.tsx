968c16b4ec8fceb4abe24fe0f0c8435d0e265232
Aman Harwara <amanharwara@protonmail.com>
2023-01-10 19:05:39 +0530

    fix: scroll

--- packages/web/src/javascripts/Components/Popover/MobilePopoverContent.tsx
+++ packages/web/src/javascripts/Components/Popover/MobilePopoverContent.tsx
@@ -107,7 +107,9 @@ const MobilePopoverContent = ({
     <Portal>
       <div
         ref={mergeRefs([popoverElement, animationCallbackRef])}
-        className={classNames('absolute top-0 left-0 z-modal h-full w-full origin-bottom bg-default opacity-0')}
+        className={classNames(
+          'absolute top-0 left-0 z-modal h-full w-full origin-bottom overflow-y-auto bg-default opacity-0',
+        )}
       >
         <div className="flex items-center justify-between border-b border-border py-2.5 px-3 text-base">
           <div />
