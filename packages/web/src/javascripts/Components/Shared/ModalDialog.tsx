ad796a42160c02f1c0a45ac84445a2c3093f8ce3
Aman Harwara <amanharwara@protonmail.com>
2023-01-10 19:18:10 +0530

    refactor: remove unusde

--- packages/web/src/javascripts/Components/Shared/ModalDialog.tsx
+++ packages/web/src/javascripts/Components/Shared/ModalDialog.tsx
@@ -1,7 +1,6 @@
 import { useRef, ReactNode } from 'react'
 import { AlertDialogContent, AlertDialogOverlay } from '@reach/alert-dialog'
 import { classNames } from '@standardnotes/utils'
-import { MutuallyExclusiveMediaQueryBreakpoints, useMediaQuery } from '@/Hooks/useMediaQuery'
 
 type Props = {
   children: ReactNode
@@ -11,8 +10,6 @@ type Props = {
 
 const ModalDialog = ({ children, onDismiss, className }: Props) => {
   const ldRef = useRef<HTMLButtonElement>(null)
-
-  const isMobileScreen = useMediaQuery(MutuallyExclusiveMediaQueryBreakpoints.sm)
 
   return (
     <AlertDialogOverlay className="px-4 md:px-0" leastDestructiveRef={ldRef} onDismiss={onDismiss}>
