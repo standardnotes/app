61c71808b5609a2752633a2311600da22ea614b6
Aman Harwara <amanharwara@protonmail.com>
2023-01-17 18:44:40 +0530

    refactor: empty files view

---
+++ packages/web/src/javascripts/Components/ContentListView/EmptyFilesView.tsx
@@ -0,0 +1,23 @@
+import { FilesIllustration } from '@standardnotes/icons'
+import Button from '../Button/Button'
+
+type Props = {
+  addNewItem: () => void
+}
+
+const EmptyFilesView = ({ addNewItem }: Props) => {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <FilesIllustration className="h-32 w-32" />
+      <div className="mt-4 mb-2 text-lg font-bold">You don't have any files yet</div>
+      <div className="mb-4 max-w-[35ch] text-center text-sm text-passive-0">
+        Files attached to your notes appear here. You can also upload files directly from this page.
+      </div>
+      <Button primary onClick={addNewItem}>
+        Upload files
+      </Button>
+    </div>
+  )
+}
+
+export default EmptyFilesView
