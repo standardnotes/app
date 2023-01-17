533ce93d3c60c3b24a5b54c1261f20fcc61c6e05
Aman Harwara <amanharwara@protonmail.com>
2023-01-17 19:31:40 +0530

    Restore packages/web/src/javascripts/Components/Shared/ModalDialog.tsx

--- packages/web/src/javascripts/Components/Shared/ModalDialog.tsx
+++ packages/web/src/javascripts/Components/Shared/ModalDialog.tsx
@@ -1,35 +1,18 @@
 import { useRef, ReactNode } from 'react'
 import { AlertDialogContent, AlertDialogOverlay } from '@reach/alert-dialog'
 import { classNames } from '@standardnotes/utils'
-import { MutuallyExclusiveMediaQueryBreakpoints, useMediaQuery } from '@/Hooks/useMediaQuery'
-import MobileModalDialog from './MobileModalDialog'
 
 type Props = {
   children: ReactNode
-  isOpen: boolean
-  close: () => void
+  onDismiss?: () => void
   className?: string
 }
 
-const ModalDialog = ({ isOpen, children, close, className }: Props) => {
+const ModalDialog = ({ children, onDismiss, className }: Props) => {
   const ldRef = useRef<HTMLButtonElement>(null)
 
-  const isMobileScreen = useMediaQuery(MutuallyExclusiveMediaQueryBreakpoints.sm)
-
-  if (isMobileScreen) {
-    return (
-      <MobileModalDialog isOpen={isOpen} close={close} className={className}>
-        {children}
-      </MobileModalDialog>
-    )
-  }
-
-  if (!isOpen) {
-    return null
-  }
-
   return (
-    <AlertDialogOverlay className="px-4 md:px-0" leastDestructiveRef={ldRef} onDismiss={close}>
+    <AlertDialogOverlay className="px-4 md:px-0" leastDestructiveRef={ldRef} onDismiss={onDismiss}>
       <AlertDialogContent
         tabIndex={0}
         className={classNames(
