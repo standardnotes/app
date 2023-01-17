12641b2cfa276d31fddc0990d21ab690128c56ff
Aman Harwara <amanharwara@protonmail.com>
2023-01-17 19:40:18 +0530

    Restore packages/web/src/javascripts/Components/Preferences/Panes/Security/TwoFactorAuth/ScanQRCode.tsx

--- packages/web/src/javascripts/Components/Preferences/Panes/Security/TwoFactorAuth/ScanQRCode.tsx
+++ packages/web/src/javascripts/Components/Preferences/Panes/Security/TwoFactorAuth/ScanQRCode.tsx
@@ -5,6 +5,7 @@ import DecoratedInput from '@/Components/Input/DecoratedInput'
 import Button from '@/Components/Button/Button'
 import { TwoFactorActivation } from './TwoFactorActivation'
 import AuthAppInfoTooltip from './AuthAppInfoPopup'
+import ModalDialog from '@/Components/Shared/ModalDialog'
 import ModalDialogButtons from '@/Components/Shared/ModalDialogButtons'
 import ModalDialogDescription from '@/Components/Shared/ModalDialogDescription'
 import ModalDialogLabel from '@/Components/Shared/ModalDialogLabel'
@@ -17,7 +18,7 @@ type Props = {
 
 const ScanQRCode: FunctionComponent<Props> = ({ activation: act }) => {
   return (
-    <>
+    <ModalDialog>
       <ModalDialogLabel closeDialog={act.cancelActivation}>Step 1 of 3 - Scan QR code</ModalDialogLabel>
       <ModalDialogDescription className="h-33 flex flex-col items-center gap-5 md:flex-row">
         <div className="w-25 h-25 flex items-center justify-center bg-info">
@@ -52,7 +53,7 @@ const ScanQRCode: FunctionComponent<Props> = ({ activation: act }) => {
         <Button className="min-w-20" label="Cancel" onClick={() => act.cancelActivation()} />
         <Button className="min-w-20" primary label="Next" onClick={() => act.openSaveSecretKey()} />
       </ModalDialogButtons>
-    </>
+    </ModalDialog>
   )
 }
 
