1b5fdbbe719daaf10a22bc025fb207336f17a0a6
Aman Harwara <amanharwara@protonmail.com>
2023-01-20 17:19:19 +0530

    fix: overflow

--- packages/web/src/javascripts/Components/Preferences/Panes/General/SmartViews/SmartViewItem.tsx
+++ packages/web/src/javascripts/Components/Preferences/Panes/General/SmartViews/SmartViewItem.tsx
@@ -14,8 +14,8 @@ const SmartViewItem = ({ view, onEdit, onDelete }: Props) => {
 
   return (
     <div className="flex items-center gap-2 py-1.5">
-      <Icon type={view.iconString} size="custom" className="h-5.5 w-5.5" />
-      <span className="mr-auto text-sm">{view.title}</span>
+      <Icon type={view.iconString} size="custom" className="h-5.5 w-5.5 flex-shrink-0" />
+      <span className="mr-auto overflow-hidden text-ellipsis text-sm">{view.title}</span>
       <Button small onClick={onEdit}>
         Edit
       </Button>
