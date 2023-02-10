033e73b18938620a4865aabe9650afd2d663ed75
Aman Harwara <amanharwara@protonmail.com>
2023-02-10 15:56:34 +0530

    feat: Added markdown horizontal rule syntax to Super notes

--- packages/blocks-editor/src/Editor/MarkdownTransformers.ts
+++ packages/blocks-editor/src/Editor/MarkdownTransformers.ts
@@ -1,8 +1,42 @@
-import { CHECK_LIST, ELEMENT_TRANSFORMERS, TEXT_FORMAT_TRANSFORMERS, TEXT_MATCH_TRANSFORMERS } from '@lexical/markdown'
+import {
+  CHECK_LIST,
+  ELEMENT_TRANSFORMERS,
+  ElementTransformer,
+  TEXT_FORMAT_TRANSFORMERS,
+  TEXT_MATCH_TRANSFORMERS,
+} from '@lexical/markdown'
+
+import {
+  HorizontalRuleNode,
+  $createHorizontalRuleNode,
+  $isHorizontalRuleNode,
+} from '@lexical/react/LexicalHorizontalRuleNode'
+import { LexicalNode } from 'lexical'
+
+const HorizontalRule: ElementTransformer = {
+  dependencies: [HorizontalRuleNode],
+  export: (node: LexicalNode) => {
+    return $isHorizontalRuleNode(node) ? '***' : null
+  },
+  regExp: /^(---|\*\*\*|___)\s?$/,
+  replace: (parentNode, _1, _2, isImport) => {
+    const line = $createHorizontalRuleNode()
+
+    if (isImport || parentNode.getNextSibling() != null) {
+      parentNode.replace(line)
+    } else {
+      parentNode.insertBefore(line)
+    }
+
+    line.selectNext()
+  },
+  type: 'element',
+}
 
 export const MarkdownTransformers = [
   CHECK_LIST,
   ...ELEMENT_TRANSFORMERS,
   ...TEXT_FORMAT_TRANSFORMERS,
   ...TEXT_MATCH_TRANSFORMERS,
+  HorizontalRule,
 ]
