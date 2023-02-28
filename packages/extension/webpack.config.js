1f350b548f32fdcbc9f15d6ea757f8e65f4ec654
Aman Harwara <amanharwara@protonmail.com>
2023-03-01 01:32:53 +0530

    chore: init

---
+++ packages/extension/webpack.config.js
@@ -0,0 +1,26 @@
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './src/background/background.js',
+  output: {
+    filename: './background.js',
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: '../web/dist',
+          to: './web',
+        },
+        { from: './src/manifest.json' },
+        {
+          from: './src/popup/index.html',
+          to: './popup/index.html',
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.js'],
+  },
+}
