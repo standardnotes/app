5198c4398c03e4a1fa6daee59a286aa058203978
Aman Harwara <amanharwara@protonmail.com>
2023-04-20 19:09:21 +0530

    refactor: replace radix with ariakit

--- packages/web/src/javascripts/Components/StyledTooltip/StyledTooltip.tsx
+++ packages/web/src/javascripts/Components/StyledTooltip/StyledTooltip.tsx
@@ -1,24 +1,26 @@
-import * as Tooltip from '@radix-ui/react-tooltip'
 import { classNames } from '@standardnotes/snjs'
 import { ReactNode } from 'react'
+import { Tooltip, TooltipAnchor, useTooltipStore } from '@ariakit/react'
+import { Slot } from '@radix-ui/react-slot'
 
 const StyledTooltip = ({ children, className, label }: { children: ReactNode; className?: string; label: string }) => {
+  const tooltip = useTooltipStore()
+
   return (
-    <Tooltip.Root>
-      <Tooltip.Trigger asChild>{children}</Tooltip.Trigger>
-      <Tooltip.Portal>
-        <Tooltip.Content
-          className={classNames(
-            'z-tooltip rounded border border-border bg-contrast py-1.5 px-3 text-sm text-foreground shadow',
-            className,
-          )}
-          collisionPadding={5}
-          sideOffset={5}
-        >
-          {label}
-        </Tooltip.Content>
-      </Tooltip.Portal>
-    </Tooltip.Root>
+    <>
+      <TooltipAnchor store={tooltip} as={Slot}>
+        {children}
+      </TooltipAnchor>
+      <Tooltip
+        store={tooltip}
+        className={classNames(
+          'z-tooltip rounded border border-border bg-contrast py-1.5 px-3 text-sm text-foreground shadow',
+          className,
+        )}
+      >
+        {label}
+      </Tooltip>
+    </>
   )
 }
 
