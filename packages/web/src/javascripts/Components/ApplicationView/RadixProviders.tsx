fc80e20c81c036d0ce8c3190a02cdb181f614d4c
Aman Harwara <amanharwara@protonmail.com>
2023-04-20 19:14:24 +0530

    refactor: remove unnecessary provider

--- packages/web/src/javascripts/Components/ApplicationView/RadixProviders.tsx
+++
@@ -1,8 +0,0 @@
-import { Provider as TooltipProvider } from '@radix-ui/react-tooltip'
-import { ReactNode } from 'react'
-
-const RadixProviders = ({ children }: { children: ReactNode }) => {
-  return <TooltipProvider delayDuration={350}>{children}</TooltipProvider>
-}
-
-export default RadixProviders
