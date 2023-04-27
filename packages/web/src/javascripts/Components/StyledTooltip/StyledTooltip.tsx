d6587695316e55369bacb3c34cbaa3614be289bd
Aman Harwara <amanharwara@protonmail.com>
2023-04-27 17:37:31 +0530

    chore: tooltips

--- packages/web/src/javascripts/Components/StyledTooltip/StyledTooltip.tsx
+++ packages/web/src/javascripts/Components/StyledTooltip/StyledTooltip.tsx
@@ -58,7 +58,7 @@ const StyledTooltip = ({
       <Tooltip
         store={tooltip}
         className={classNames(
-          'z-tooltip rounded border border-border bg-contrast py-1.5 px-3 text-sm text-foreground shadow',
+          'z-tooltip max-w-max rounded border border-border bg-contrast py-1.5 px-3 text-sm text-foreground shadow',
           className,
         )}
       >
