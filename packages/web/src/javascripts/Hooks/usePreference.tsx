01c71d79e74930b14b35631f309899d25d763828
Aman Harwara <amanharwara@protonmail.com>
2023-05-11 14:52:15 +0530

    chore: improve usePreference hook + typescript tooling

--- packages/web/src/javascripts/Hooks/usePreference.tsx
+++ packages/web/src/javascripts/Hooks/usePreference.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react'
 export default function usePreference<Key extends PrefKey, Value extends PrefValue[Key]>(preference: Key) {
   const application = useApplication()
 
-  const [value, setValue] = useState<Value>(application.getPreference(preference, PrefDefaults[preference]) as Value)
+  const [value, setValue] = useState(application.getPreference(preference, PrefDefaults[preference]))
 
   useEffect(() => {
     return application.addEventObserver(async () => {
