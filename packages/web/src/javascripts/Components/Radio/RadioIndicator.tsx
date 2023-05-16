a137ca6d70410763e3a21266ce9697be23e1c148
Aman Harwara <amanharwara@protonmail.com>
2023-05-16 15:14:51 +0530

    chore: fix radio button on big screen

--- packages/web/src/javascripts/Components/Radio/RadioIndicator.tsx
+++ packages/web/src/javascripts/Components/Radio/RadioIndicator.tsx
@@ -8,7 +8,7 @@ const RadioIndicator = ({ checked, className, disabled }: Props) => (
   <div
     className={`relative h-5 w-5 rounded-full border-2 border-solid md:h-4 md:w-4 ${disabled ? 'opacity-50' : ''} ${
       checked
-        ? 'border-info after:absolute after:top-1/2 after:left-1/2 after:h-3 after:w-3 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-info'
+        ? 'border-info after:absolute after:top-1/2 after:left-1/2 after:h-3 after:w-3 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-info md:after:h-2 md:after:w-2'
         : 'border-passive-1'
     } ${className}`}
   ></div>
