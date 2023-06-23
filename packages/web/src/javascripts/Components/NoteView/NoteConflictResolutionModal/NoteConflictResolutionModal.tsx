972d167044b06e0e03aecc1331045962c34fafa5
Aman Harwara <amanharwara@protonmail.com>
2023-06-23 19:20:58 +0530

    refactor: split files

--- packages/web/src/javascripts/Components/NoteView/NoteConflictResolutionModal/NoteConflictResolutionModal.tsx
+++ packages/web/src/javascripts/Components/NoteView/NoteConflictResolutionModal/NoteConflictResolutionModal.tsx
@@ -1,13 +1,10 @@
 import { NoteType, SNNote, classNames } from '@standardnotes/snjs'
 import Modal, { ModalAction } from '../../Modal/Modal'
-import { MouseEventHandler, useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { MutuallyExclusiveMediaQueryBreakpoints, useMediaQuery } from '@/Hooks/useMediaQuery'
-import { FOCUSABLE_BUT_NOT_TABBABLE } from '@/Constants/Constants'
 import { useApplication } from '../../ApplicationProvider'
 import { confirmDialog } from '@standardnotes/ui-services'
 import { useListKeyboardNavigation } from '@/Hooks/useListKeyboardNavigation'
-import { useNoteAttributes } from '../../NotesOptions/NoteAttributes'
-import CheckIndicator from '../../Checkbox/CheckIndicator'
 import ModalDialogButtons from '../../Modal/ModalDialogButtons'
 import {
   Checkbox,
@@ -17,7 +14,6 @@ import {
   SelectList,
   Toolbar,
   ToolbarItem,
-  VisuallyHidden,
   useSelectStore,
   useToolbarStore,
 } from '@ariakit/react'
@@ -29,93 +25,7 @@ import Switch from '../../Switch/Switch'
 import StyledTooltip from '../../StyledTooltip/StyledTooltip'
 import { DiffView } from './DiffView'
 import { NoteContent } from './NoteContent'
-
-const ConflictListItem = ({
-  isSelected,
-  onClick,
-  title,
-  note,
-  disabled,
-}: {
-  isSelected: boolean
-  disabled: boolean
-  onClick: MouseEventHandler<HTMLButtonElement>
-  title: string
-  note: SNNote
-}) => {
-  const application = useApplication()
-  const { words, characters, paragraphs, dateLastModified, dateCreated, format } = useNoteAttributes(application, note)
-
-  return (
-    <button
-      tabIndex={FOCUSABLE_BUT_NOT_TABBABLE}
-      className={classNames(
-        'flex w-full select-none flex-col overflow-hidden border-l-2 bg-transparent px-3 py-2.5 pl-4 text-left text-sm text-text',
-        isSelected ? 'border-info bg-info-backdrop' : 'border-transparent',
-        disabled
-          ? 'cursor-not-allowed opacity-75'
-          : 'cursor-pointer hover:bg-contrast hover:text-foreground focus:bg-info-backdrop focus:shadow-none',
-      )}
-      onClick={onClick}
-      data-selected={isSelected}
-      disabled={disabled}
-    >
-      <div className="mb-1.5 flex items-center gap-2">
-        <CheckIndicator checked={isSelected} />
-        <div className="font-semibold">{title}</div>
-      </div>
-      <div className="w-full text-sm text-neutral lg:text-xs">
-        <div className="mb-1.5 flex items-center gap-2">
-          <StyledTooltip gutter={8} label="Last modified" className="!z-modal">
-            <div className="flex-shrink-0">
-              <Icon type="restore" size="medium" />
-            </div>
-          </StyledTooltip>
-          <VisuallyHidden>Last modified</VisuallyHidden> {dateLastModified}
-        </div>
-        <div className="mb-1.5 flex items-center gap-2">
-          <StyledTooltip gutter={8} label="Created" className="!z-modal">
-            <div className="flex-shrink-0">
-              <Icon type="pencil-filled" size="medium" />
-            </div>
-          </StyledTooltip>
-          <VisuallyHidden>Created</VisuallyHidden> {dateCreated}
-        </div>
-        <div className="mb-1.5 flex items-center gap-2 overflow-hidden">
-          <StyledTooltip gutter={8} label="Note ID" className="!z-modal">
-            <div className="flex-shrink-0">
-              <Icon type="info" size="medium" />
-            </div>
-          </StyledTooltip>
-          <VisuallyHidden>Note ID</VisuallyHidden>
-          <div className="overflow-hidden text-ellipsis whitespace-nowrap">{note.uuid}</div>
-        </div>
-        {typeof words === 'number' && (format === 'txt' || format === 'md') ? (
-          <div className="flex items-center gap-2">
-            <StyledTooltip gutter={8} label={`${words} words`} className="!z-modal">
-              <div className="flex items-center gap-1">
-                <Icon type="line-width" size="medium" />
-                {words}
-              </div>
-            </StyledTooltip>
-            <StyledTooltip gutter={8} label={`${characters} characters`} className="!z-modal">
-              <div className="flex items-center gap-1">
-                <Icon type="bold" size="small" />
-                <span>{characters}</span>
-              </div>
-            </StyledTooltip>
-            <StyledTooltip gutter={8} label={`${paragraphs} paragraphs`} className="!z-modal">
-              <div className="flex items-center gap-1">
-                <Icon type="paragraph" size="medium" />
-                <span>{paragraphs}</span>
-              </div>
-            </StyledTooltip>
-          </div>
-        ) : null}
-      </div>
-    </button>
-  )
-}
+import { ConflictListItem } from './ConflictListItem'
 
 type ConflictAction = 'move-to-trash' | 'delete-permanently'
 type MultipleSelectionMode = 'preview' | 'diff'
