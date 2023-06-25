352dcea90379d845570e02e1963028e23edc809a
Aman Harwara <amanharwara@protonmail.com>
2023-06-25 14:02:13 +0530

    refactor: diff

--- packages/web/src/javascripts/Components/NoteView/NoteConflictResolutionModal/DiffView.tsx
+++ packages/web/src/javascripts/Components/NoteView/NoteConflictResolutionModal/DiffView.tsx
@@ -3,6 +3,19 @@ import { useEffect, useState } from 'react'
 import fastdiff from 'fast-diff'
 import { HeadlessSuperConverter } from '../../SuperEditor/Tools/HeadlessSuperConverter'
 
+const DiffSpan = ({ state, text }: { state: fastdiff.Diff[0]; text: fastdiff.Diff[1] }) => (
+  <span
+    data-diff={state !== fastdiff.EQUAL ? state : undefined}
+    className={classNames(
+      'whitespace-pre-wrap',
+      state === fastdiff.INSERT && 'bg-success text-success-contrast',
+      state === fastdiff.DELETE && 'bg-danger text-danger-contrast',
+    )}
+  >
+    {text}
+  </span>
+)
+
 export const DiffView = ({
   selectedNotes,
   convertSuperToMarkdown,
@@ -95,41 +108,17 @@ export const DiffView = ({
   return (
     <div className="force-custom-scrollbar relative flex flex-grow flex-col overflow-hidden">
       <div className="w-full px-4 py-4 text-base font-bold">
-        {titleDiff.map(([state, text], index) => {
-          return (
-            <span
-              data-diff={state !== fastdiff.EQUAL ? state : undefined}
-              className={classNames(
-                'whitespace-pre-wrap',
-                state === fastdiff.INSERT && 'bg-success text-success-contrast',
-                state === fastdiff.DELETE && 'bg-danger text-danger-contrast',
-              )}
-              key={index}
-            >
-              {text}
-            </span>
-          )
-        })}
+        {titleDiff.map(([state, text], index) => (
+          <DiffSpan state={state} text={text} key={index} />
+        ))}
       </div>
       <pre
         className="font-editor min-h-0 w-full flex-grow overflow-y-auto whitespace-pre-wrap p-4 pt-0 text-editor [&::-webkit-scrollbar]:bg-transparent"
         ref={setPreElement}
       >
-        {textDiff.map(([state, text], index) => {
-          return (
-            <span
-              data-diff={state !== fastdiff.EQUAL ? state : undefined}
-              className={classNames(
-                'whitespace-pre-wrap',
-                state === fastdiff.INSERT && 'bg-success text-success-contrast',
-                state === fastdiff.DELETE && 'bg-danger text-danger-contrast',
-              )}
-              key={index}
-            >
-              {text}
-            </span>
-          )
-        })}
+        {textDiff.map(([state, text], index) => (
+          <DiffSpan state={state} text={text} key={index} />
+        ))}
       </pre>
       {hasOverflow && (
         <div className="absolute top-0 right-0 z-[-1] h-full w-[19px] border-l border-border" ref={setDiffVisualizer} />
