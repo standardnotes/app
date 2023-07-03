932695631e5053988a9177c0ee15152da5a169cf
Mo <mo@standardnotes.com>
2023-07-03 05:47:33 -0500

    fix: supress error alert on download first sync for older accounts

--- packages/encryption/src/Domain/Service/Functions.ts
+++ packages/encryption/src/Domain/Service/Functions.ts
@@ -9,16 +9,22 @@ export function findDefaultItemsKey(itemsKeys: ItemsKeyInterface[]): ItemsKeyInt
     return key.isDefault
   })
 
-  if (defaultKeys.length > 1) {
-    /**
-     * Prioritize one that is synced, as neverSynced keys will likely be deleted after
-     * DownloadFirst sync.
-     */
-    const syncedKeys = defaultKeys.filter((key) => !key.neverSynced)
-    if (syncedKeys.length > 0) {
-      return syncedKeys[0]
-    }
+  if (defaultKeys.length === 0) {
+    return undefined
   }
 
-  return defaultKeys[0]
+  if (defaultKeys.length === 1) {
+    return defaultKeys[0]
+  }
+
+  /**
+   * Prioritize one that is synced, as neverSynced keys will likely be deleted after
+   * DownloadFirst sync.
+   */
+  const syncedKeys = defaultKeys.filter((key) => !key.neverSynced)
+  if (syncedKeys.length > 0) {
+    return syncedKeys[0]
+  }
+
+  return undefined
 }
--- packages/services/src/Domain/Sync/SyncSource.ts
+++ packages/services/src/Domain/Sync/SyncSource.ts
@@ -1,11 +1,12 @@
 /* istanbul ignore file */
 
 export enum SyncSource {
-  External = 1,
-  SpawnQueue = 2,
-  ResolveQueue = 3,
-  MoreDirtyItems = 4,
-  AfterDownloadFirst = 5,
-  IntegrityCheck = 6,
-  ResolveOutOfSync = 7,
+  External = 'External',
+  SpawnQueue = 'SpawnQueue',
+  ResolveQueue = 'ResolveQueue',
+  MoreDirtyItems = 'MoreDirtyItems',
+  DownloadFirst = 'DownloadFirst',
+  AfterDownloadFirst = 'AfterDownloadFirst',
+  IntegrityCheck = 'IntegrityCheck',
+  ResolveOutOfSync = 'ResolveOutOfSync',
 }
