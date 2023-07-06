730d0e98f0aedd95a7ae290e42c8ff735fe63372
Aman Harwara <amanharwara@protonmail.com>
2023-07-06 22:13:34 +0530

    chore: fix PageUp and PageDown not scrolling in preferences view

--- packages/web/src/javascripts/Components/Preferences/PreferencesCanvas.tsx
+++ packages/web/src/javascripts/Components/Preferences/PreferencesCanvas.tsx
@@ -4,11 +4,12 @@ import { PreferencesSessionController } from './Controller/PreferencesSessionCon
 import PreferencesMenuView from './PreferencesMenuView'
 import PaneSelector from './PaneSelector'
 import { PreferencesProps } from './PreferencesProps'
+import { FOCUSABLE_BUT_NOT_TABBABLE } from '@/Constants/Constants'
 
 const PreferencesCanvas: FunctionComponent<PreferencesProps & { menu: PreferencesSessionController }> = (props) => (
   <div className="flex min-h-0 flex-grow flex-col md:flex-row md:justify-between">
     <PreferencesMenuView menu={props.menu} />
-    <div className="min-h-0 flex-grow overflow-auto bg-contrast">
+    <div className="min-h-0 flex-grow overflow-auto bg-contrast" tabIndex={FOCUSABLE_BUT_NOT_TABBABLE}>
       <PaneSelector {...props} />
     </div>
   </div>
