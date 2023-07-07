12adc5b59a53488acd1d46f32327ba07b13a409b
Aman Harwara <amanharwara@protonmail.com>
2023-07-07 13:55:04 +0530

    fix: response body

--- packages/api/src/Domain/Http/FetchRequestHandler.ts
+++ packages/api/src/Domain/Http/FetchRequestHandler.ts
@@ -94,7 +94,7 @@ export class FetchRequestHandler implements RequestHandlerInterface {
         if (contentTypeHeader?.includes('application/json')) {
           body = JSON.parse(await fetchResponse.text())
         } else {
-          body = fetchResponse.body
+          body = fetchResponse.type === 'basic' ? await fetchResponse.text() : await fetchResponse.arrayBuffer()
         }
         /**
          * v0 APIs do not have a `data` top-level object. In such cases, mimic
