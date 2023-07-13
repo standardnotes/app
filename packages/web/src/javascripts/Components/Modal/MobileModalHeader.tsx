ecc5b5e503918a41a8663ced506f373010477a5e
Aman Harwara <amanharwara@protonmail.com>
2023-07-13 15:46:29 +0530

    chore: make modal header text unselectable

--- packages/web/src/javascripts/Components/Modal/MobileModalHeader.tsx
+++ packages/web/src/javascripts/Components/Modal/MobileModalHeader.tsx
@@ -7,7 +7,11 @@ type Props = {
 }
 
 const MobileModalHeader = ({ className, children }: Props) => {
-  return <div className={classNames('grid w-full grid-cols-[0.35fr_1fr_0.35fr] gap-2', className)}>{children}</div>
+  return (
+    <div className={classNames('grid w-full select-none grid-cols-[0.35fr_1fr_0.35fr] gap-2', className)}>
+      {children}
+    </div>
+  )
 }
 
 export default MobileModalHeader
