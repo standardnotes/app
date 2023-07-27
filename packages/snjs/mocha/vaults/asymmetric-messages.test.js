4f6ca220cb5366b3931a918d8209fc72d4a75be3
Karol Sójko <karolsojko@proton.me>
2023-07-27 11:27:20 +0200

    Revert "chore: skip test"
    This reverts commit 26bb876cf55e2c4fa9eeea56f73b3c2917a26f5c.

--- packages/snjs/mocha/vaults/asymmetric-messages.test.js
+++ packages/snjs/mocha/vaults/asymmetric-messages.test.js
@@ -387,7 +387,7 @@ describe('asymmetric messages', function () {
     await deinitContactContext()
   })
 
-  it.skip('should delete all inbound messages after changing user password', async () => {
+  it('should delete all inbound messages after changing user password', async () => {
     /** Messages to user are encrypted with old keypair and are no longer decryptable */
 
     const { sharedVault, contactContext, deinitContactContext } =
