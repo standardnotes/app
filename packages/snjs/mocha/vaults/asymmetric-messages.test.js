ab3eb4dfc2cb5e84f30ae159bfbf21c472a60d50
Mo <mo@standardnotes.com>
2023-07-28 14:39:59 -0500

    chore: trigger run

--- packages/snjs/mocha/vaults/asymmetric-messages.test.js
+++ packages/snjs/mocha/vaults/asymmetric-messages.test.js
@@ -233,7 +233,7 @@ describe('asymmetric messages', function () {
     await deinitContactContext()
   })
 
-  it.only('should send sender keypair changed message to trusted contacts 3', async () => {
+  it.only('should send sender keypair changed message to trusted contacts 4', async () => {
     const { contactContext, deinitContactContext } = await Collaboration.createSharedVaultWithAcceptedInvite(context)
 
     contactContext.lockSyncing()
