fab2f97928aef7cbe70d5849151c02ee3f3b1ea8
Aman Harwara <amanharwara@protonmail.com>
2023-08-26 00:21:08 +0530

    fix: positioning

--- packages/web/src/javascripts/Components/Preferences/PreferencesComponents/MenuItem.tsx
+++ packages/web/src/javascripts/Components/Preferences/PreferencesComponents/MenuItem.tsx
@@ -32,7 +32,7 @@ const PreferencesMenuItem: FunctionComponent<Props> = ({
     <div className="relative mr-1">
       <Icon className={classNames('text-base', selected ? 'text-info' : 'text-neutral')} type={iconType} />
       {bubbleCount ? (
-        <div className="absolute bottom-full left-full flex aspect-square h-4 w-4 -translate-x-3 translate-y-2 items-center justify-center rounded-full border border-info-contrast bg-info text-[0.5rem] font-bold text-info-contrast">
+        <div className="absolute bottom-full right-full flex aspect-square h-4 w-4 translate-x-2 translate-y-2 items-center justify-center rounded-full border border-info-contrast bg-info text-[0.5rem] font-bold text-info-contrast">
           {bubbleCount}
         </div>
       ) : null}
