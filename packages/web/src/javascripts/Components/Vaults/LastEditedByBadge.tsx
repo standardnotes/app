ffc86c34a5de0b887c953091c8d5c882ceba37e2
Aman Harwara <amanharwara@protonmail.com>
2023-09-08 16:28:30 +0530

    fix: badge styles on mobile

--- packages/web/src/javascripts/Components/Vaults/LastEditedByBadge.tsx
+++ packages/web/src/javascripts/Components/Vaults/LastEditedByBadge.tsx
@@ -3,9 +3,12 @@ import Icon from '../Icon/Icon'
 
 const LastEditedByBadge = ({ contact }: { contact: TrustedContactInterface }) => {
   return (
-    <div title="Last edited by" className="flex select-none rounded bg-info px-1.5 py-1 text-info-contrast">
+    <div
+      title="Last edited by"
+      className="flex select-none items-center rounded bg-info px-1.5 py-1 text-info-contrast"
+    >
       <Icon ariaLabel="Shared by" type="pencil" className="mr-1 text-info-contrast" size="medium" />
-      <span className="mr-auto overflow-hidden text-ellipsis text-xs">{contact.name}</span>
+      <span className="mr-auto overflow-hidden text-ellipsis text-sm font-semibold lg:text-xs">{contact.name}</span>
     </div>
   )
 }
--- packages/web/src/javascripts/Components/Vaults/SharedByContactBadge.tsx
+++ packages/web/src/javascripts/Components/Vaults/SharedByContactBadge.tsx
@@ -3,11 +3,9 @@ import Icon from '../Icon/Icon'
 
 const SharedByContactBadge = ({ contact }: { contact: TrustedContactInterface }) => {
   return (
-    <div title="Shared by contact" className="rounded bg-info px-1.5 py-1 text-neutral-contrast">
-      <span className="flex items-center" title="Shared by contact">
-        <Icon ariaLabel="Shared by contact" type="archive" className="mr-1 text-info-contrast" size="medium" />
-        <div className="text-center text-xs font-bold">{contact.name}</div>
-      </span>
+    <div title="Shared by contact" className="flex items-center rounded bg-info px-1.5 py-1 text-neutral-contrast">
+      <Icon ariaLabel="Shared by contact" type="archive" className="mr-1 text-info-contrast" size="medium" />
+      <div className="text-center text-sm font-semibold lg:text-xs">{contact.name}</div>
     </div>
   )
 }
