9a51c0039a3cb3d335df069a351d9d04f1221f83
Aman Harwara <amanharwara@protonmail.com>
2023-09-14 19:29:11 +0530

    refactor: common actions into hook

--- packages/web/src/javascripts/Components/VaultSelectionMenu/VaultOptionsMenu.tsx
+++ packages/web/src/javascripts/Components/VaultSelectionMenu/VaultOptionsMenu.tsx
@@ -1,28 +1,19 @@
-import { VaultListingInterface, VaultLockServiceEvent } from '@standardnotes/snjs'
+import { VaultListingInterface } from '@standardnotes/snjs'
 import Menu from '../Menu/Menu'
 import MenuItem from '../Menu/MenuItem'
 import Icon from '../Icon/Icon'
-import { useApplication } from '../ApplicationProvider'
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useCallback } from 'react'
 import EditVaultModal from '../Preferences/Panes/Vaults/Vaults/VaultModal/EditVaultModal'
+import { useVault } from '@/Hooks/useVault'
 
 type Props = {
   vault: VaultListingInterface
 }
 
 const VaultOptionsMenu = ({ vault }: Props) => {
-  const application = useApplication()
+  const { canShowLockOption, isLocked, toggleLock, ensureVaultIsUnlocked } = useVault(vault)
 
   const [isVaultModalOpen, setIsVaultModalOpen] = useState(false)
-
-  const ensureVaultIsUnlocked = useCallback(async () => {
-    if (!application.vaultLocks.isVaultLocked(vault)) {
-      return true
-    }
-    const unlocked = await application.vaultDisplayService.unlockVault(vault)
-    return unlocked
-  }, [application, vault])
-
   const openEditModal = useCallback(async () => {
     if (!(await ensureVaultIsUnlocked())) {
       return
@@ -31,26 +22,6 @@ const VaultOptionsMenu = ({ vault }: Props) => {
     setIsVaultModalOpen(true)
   }, [ensureVaultIsUnlocked])
 
-  const isVaultLockable = application.vaultLocks.isVaultLockable(vault)
-  const [isVaultLocked, setIsVaultLocked] = useState(() => application.vaultLocks.isVaultLocked(vault))
-  useEffect(() => {
-    return application.vaultLocks.addEventObserver((event) => {
-      if (event === VaultLockServiceEvent.VaultLocked || event === VaultLockServiceEvent.VaultUnlocked) {
-        setIsVaultLocked(application.vaultLocks.isVaultLocked(vault))
-      }
-    })
-  }, [application.vaultLocks, vault])
-  const toggleLock = useCallback(async () => {
-    if (!isVaultLockable) {
-      return
-    }
-    if (isVaultLocked) {
-      application.vaultDisplayService.unlockVault(vault).catch(console.error)
-    } else {
-      application.vaultLocks.lockNonPersistentVault(vault).catch(console.error)
-    }
-  }, [application.vaultDisplayService, application.vaultLocks, isVaultLockable, isVaultLocked, vault])
-
   return (
     <>
       <Menu a11yLabel="Vault options menu" isOpen>
@@ -58,10 +29,10 @@ const VaultOptionsMenu = ({ vault }: Props) => {
           <Icon type="pencil-filled" className="mr-2" />
           Edit vault
         </MenuItem>
-        {isVaultLockable && (
+        {canShowLockOption && (
           <MenuItem onClick={toggleLock}>
             <Icon type="lock" className="mr-2" />
-            {isVaultLocked ? 'Unlock' : 'Lock'} vault
+            {isLocked ? 'Unlock' : 'Lock'} vault
           </MenuItem>
         )}
       </Menu>
---
+++ packages/web/src/javascripts/Hooks/useVault.ts
@@ -0,0 +1,58 @@
+import { useApplication } from '@/Components/ApplicationProvider'
+import {
+  KeySystemPasswordType,
+  KeySystemRootKeyStorageMode,
+  VaultListingInterface,
+  VaultLockServiceEvent,
+} from '@standardnotes/snjs'
+import { useState, useEffect, useCallback } from 'react'
+
+export const useVault = (vault: VaultListingInterface) => {
+  const application = useApplication()
+
+  const canShowLockOption =
+    vault.keyPasswordType === KeySystemPasswordType.UserInputted &&
+    vault.keyStorageMode === KeySystemRootKeyStorageMode.Ephemeral
+
+  const [isLocked, setIsLocked] = useState(() => application.vaultLocks.isVaultLocked(vault))
+
+  useEffect(() => {
+    return application.vaultLocks.addEventObserver((event) => {
+      if (event === VaultLockServiceEvent.VaultLocked || event === VaultLockServiceEvent.VaultUnlocked) {
+        setIsLocked(application.vaultLocks.isVaultLocked(vault))
+      }
+    })
+  }, [application.vaultLocks, vault])
+
+  const toggleLock = useCallback(async () => {
+    if (!canShowLockOption) {
+      return
+    }
+
+    if (isLocked) {
+      application.vaultDisplayService.unlockVault(vault).catch(console.error)
+    } else {
+      application.vaultLocks.lockNonPersistentVault(vault).catch(console.error)
+    }
+  }, [application.vaultDisplayService, application.vaultLocks, canShowLockOption, isLocked, vault])
+
+  const isCurrentUserAdmin = !vault.isSharedVaultListing()
+    ? true
+    : application.vaultUsers.isCurrentUserSharedVaultAdmin(vault)
+
+  const ensureVaultIsUnlocked = useCallback(async () => {
+    if (!application.vaultLocks.isVaultLocked(vault)) {
+      return true
+    }
+    const unlocked = await application.vaultDisplayService.unlockVault(vault)
+    return unlocked
+  }, [application, vault])
+
+  return {
+    canShowLockOption,
+    isLocked,
+    toggleLock,
+    ensureVaultIsUnlocked,
+    isCurrentUserAdmin,
+  }
+}
