db6151e253e05bf64c443d981b88c546b3e504c6
Karol Sójko <karolsojko@gmail.com>
2023-12-12 11:47:14 +0100

    chore: activate paid subscriptions for revisions e2e suites (#2693)

--- packages/snjs/mocha/history.test.js
+++ packages/snjs/mocha/history.test.js
@@ -238,6 +238,12 @@ describe('history manager', () => {
 
       await context.launch()
       await context.register()
+
+      /**
+       * Free user revisions are limited to 1 per day. This is to ensure that
+       * we don't hit that limit during testing.
+       */
+      await context.activatePaidSubscriptionForUser()
     })
 
     afterEach(async function () {
