cc14e8c8c0afffa8408a0d255dd0e739effd3913
Aman Harwara <amanharwara@protonmail.com>
2023-12-22 13:37:42 +0530

    feat: add image example

--- packages/web/src/javascripts/Components/PremiumFeaturesModal/Subviews/SuperDemoInitialValue.ts
+++ packages/web/src/javascripts/Components/PremiumFeaturesModal/Subviews/SuperDemoInitialValue.ts
@@ -5,6 +5,12 @@ const InitialHTML = `<div>
 	<p><br></p>
 	<p>Super notes are our new <b>rich text</b> experience. With Super notes, you can create <b>rich</b>, <i>dynamic</i> text with powerful options.</p>
   <p><br></p>
+	<h2><span>Images</span></h2>
+  <p><br></p>
+  <p>You can add images to your note by selecting the "Image from URL" option from the <code spellcheck="false"><span>/</span></code> menu or Insert menu in the toolbar.</p>
+  <p><br></p>
+  <p><img src="https://standardnotes.com/static/292c6ba50c69a3ae4f8b1883e7f505f6/1f7f6/vault-wide.jpg" /></p>
+  <p><br></p>
 	<h2><span>Lists</span></h2>
   <p><br></p>
   <ul>
