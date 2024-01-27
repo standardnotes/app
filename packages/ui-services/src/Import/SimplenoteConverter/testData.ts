c858e516bb20096d15ef1bcd4fa9c5c60ae8301d
Mae B. Morella <4561733+mmorella-dev@users.noreply.github.com>
2024-01-27 05:38:35 -0500

    fix: Fixed issue with Simplenote importer not titling multi-line documents (#2798)

--- packages/ui-services/src/Import/SimplenoteConverter/testData.ts
+++ packages/ui-services/src/Import/SimplenoteConverter/testData.ts
@@ -2,7 +2,7 @@ const data = {
   activeNotes: [
     {
       id: '43349052-4efa-48c2-bdd6-8323124451b1',
-      content: "Testing 2\r\nThis is the 2nd note's content.",
+      content: "Testing 2\r\nThis is...\r\nthe 2nd note's content.",
       creationDate: '2020-06-08T21:28:43.856Z',
       lastModified: '2021-04-16T06:21:53.124Z',
     },
