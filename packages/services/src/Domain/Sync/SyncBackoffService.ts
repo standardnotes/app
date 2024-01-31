09a1e407adab254c0221dd3a71cf840ed390d4dd
Karol Sójko <karolsojko@gmail.com>
2024-01-31 11:42:05 +0100

    remove successfully uploaded payloads from backoff

--- packages/services/src/Domain/Sync/SyncBackoffService.ts
+++ packages/services/src/Domain/Sync/SyncBackoffService.ts
@@ -1,13 +1,36 @@
-import { AnyItemInterface } from '@standardnotes/models'
+import { AnyItemInterface, ServerSyncPushContextualPayload } from '@standardnotes/models'
+import {
+  AbstractService,
+  ApplicationEvent,
+  InternalEventBusInterface,
+  InternalEventHandlerInterface,
+  InternalEventInterface,
+} from '@standardnotes/snjs'
+
 import { SyncBackoffServiceInterface } from './SyncBackoffServiceInterface'
 
-export class SyncBackoffService implements SyncBackoffServiceInterface {
+export class SyncBackoffService
+  extends AbstractService
+  implements SyncBackoffServiceInterface, InternalEventHandlerInterface
+{
   private backoffPenalties: Map<string, number>
   private backoffStartTimestamps: Map<string, number>
 
-  constructor() {
+  constructor(protected override internalEventBus: InternalEventBusInterface) {
+    super(internalEventBus)
+
     this.backoffPenalties = new Map<string, number>()
     this.backoffStartTimestamps = new Map<string, number>()
+  }
+
+  async handleEvent(event: InternalEventInterface): Promise<void> {
+    if (event.type === ApplicationEvent.CompletedIncrementalSync) {
+      for (const payload of (event.payload as Record<string, unknown>)
+        .uploadedPayloads as ServerSyncPushContextualPayload[]) {
+        this.backoffPenalties.delete(payload.uuid)
+        this.backoffStartTimestamps.delete(payload.uuid)
+      }
+    }
   }
 
   isItemInBackoff(item: AnyItemInterface): boolean {
