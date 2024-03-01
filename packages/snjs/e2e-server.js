1651a24370351f7d96d5da2ef588af4e5fa1e8a5
Karol Sójko <karolsojko@gmail.com>
2024-03-01 14:41:30 +0100

    chore: replace sncrypto web with a published version - skip e2e

--- packages/snjs/e2e-server.js
+++ packages/snjs/e2e-server.js
@@ -3,21 +3,9 @@
 /* Used for running mocha tests */
 const connect = require('connect')
 const serveStatic = require('serve-static')
-const fs = require('fs')
 
 const isDev = process.argv[2] === '--dev'
 const port = isDev ? 9002 : 9001
-
-const snCryptoDistFilePath = `${__dirname}/../sncrypto-web/dist/sncrypto-web.js`
-if (!fs.existsSync(snCryptoDistFilePath)) {
-  console.error(
-    `Could not find sncrypto dist file under: ${snCryptoDistFilePath}. Please consider building the project first`,
-  )
-
-  process.exit(1)
-}
-
-fs.copyFileSync(snCryptoDistFilePath, `${__dirname}/mocha/vendor/sncrypto-web.js`)
 
 connect()
   .use(serveStatic(__dirname))
