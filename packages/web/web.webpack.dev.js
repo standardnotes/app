a162294d04176d5986d4ce8cda3c1df41804dd80
Aman Harwara <amanharwara@protonmail.com>
2024-03-23 00:25:02 +0530

    chore: fix csp errors in dev server

--- packages/web/web.webpack.dev.js
+++ packages/web/web.webpack.dev.js
@@ -20,7 +20,7 @@ module.exports = (env, argv) => {
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Security-Policy':
-          "default-src https: 'self'; base-uri 'self'; child-src * blob:; connect-src * data: blob:; font-src * data:; form-action 'self'; frame-ancestors * file:; frame-src * blob:; img-src 'self' * data: blob:; manifest-src 'self'; media-src 'self' blob: *.standardnotes.com; object-src 'self' blob: *.standardnotes.com; script-src 'self' 'sha256-r26E+iPOhx7KM7cKn4trOSoD8u5E7wL7wwJ8UrR+rGs=' 'wasm-unsafe-eval'; style-src *;",
+          "default-src https: 'self'; base-uri 'self'; child-src * blob:; connect-src * data: blob:; font-src * data:; form-action 'self'; frame-ancestors * file:; frame-src * blob:; img-src 'self' * data: blob:; manifest-src 'self'; media-src 'self' blob: *.standardnotes.com; object-src 'self' blob: *.standardnotes.com; script-src 'self' 'sha256-r26E+iPOhx7KM7cKn4trOSoD8u5E7wL7wwJ8UrR+rGs=' 'unsafe-eval' 'wasm-unsafe-eval'; style-src *;",
       },
       hot: true,
       static: './dist',
