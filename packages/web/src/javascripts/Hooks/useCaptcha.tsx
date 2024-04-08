b4d6233c3381b8dbea70ee4df61ad45c56ee6641
Aman Harwara <amanharwara@protonmail.com>
2024-04-08 21:15:26 +0530

    refactor: add origin validation

--- packages/web/src/javascripts/Hooks/useCaptcha.tsx
+++ packages/web/src/javascripts/Hooks/useCaptcha.tsx
@@ -3,6 +3,14 @@ import { useEffect } from 'react'
 export const useCaptcha = (captchaURL: string, callback: (token: string) => void) => {
   useEffect(() => {
     function handleCaptchaEvent(event: any) {
+      if (!captchaURL) {
+        return
+      }
+
+      if (event.origin !== new URL(captchaURL).origin) {
+        return
+      }
+
       if (event?.data?.type?.includes('captcha')) {
         callback(event.data.token)
       }
