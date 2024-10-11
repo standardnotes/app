2cd0c263bc904da1e956f2af4b3eb22f87c32a93
moughxyz <mo@standardnotes.com>
2024-10-11 13:05:02 -0500

    chore(ci): verbose build [skip e2e]

--- packages/desktop/scripts/windowsSign.js
+++ packages/desktop/scripts/windowsSign.js
@@ -1,7 +1,10 @@
-exports.default = async function(configuration) {
-  if(configuration.path){
-    require("child_process").execSync(
-      `smctl sign --keypair-alias=snkeypair --input "${String(configuration.path)}"`
-    );
+exports.default = async function (configuration) {
+  if (configuration.path) {
+    require('child_process').execSync(
+      `smctl sign --keypair-alias=snkeypair --input "${String(configuration.path)}" --verbose`,
+      {
+        stdio: 'inherit',
+      },
+    )
   }
-};
+}
