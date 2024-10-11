d1782135ee97f772012e0d79b6a0368f4a4140fb
moughxyz <mo@standardnotes.com>
2024-10-11 13:59:42 -0500

    chore(ci): correct keypair alias [skip e2e]

--- packages/desktop/scripts/windowsSign.js
+++ packages/desktop/scripts/windowsSign.js
@@ -1,7 +1,9 @@
 exports.default = async function (configuration) {
   if (configuration.path) {
+    const keypairAlias = process.env.SM_KEYPAIR_ALIAS
+
     require('child_process').execSync(
-      `smctl sign --keypair-alias=snkeypair --input "${String(configuration.path)}" --verbose`,
+      `smctl sign --keypair-alias="${keypairAlias}" --input "${String(configuration.path)}" --verbose`,
       {
         stdio: 'inherit',
       },
