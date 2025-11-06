fb3c166e786639994589f18dcda31fd3edc07069
Antonella Sgarlatta <anto@standardnotes.com>
2025-11-06 13:48:38 -0300

    chore: increment editor debounce rate

--- packages/web/src/javascripts/Components/NoteView/Controller/EditorSaveTimeoutDebounce.ts
+++ packages/web/src/javascripts/Components/NoteView/Controller/EditorSaveTimeoutDebounce.ts
@@ -1,7 +1,7 @@
 import { MILLISECONDS_IN_A_SECOND } from '@/Constants/Constants'
 
 export const EditorSaveTimeoutDebounce = {
-  Desktop: 350,
+  Desktop: 700,
   ImmediateChange: 100,
   NativeMobileWeb: 700,
   LargeNote: 60 * MILLISECONDS_IN_A_SECOND,
