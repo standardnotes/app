6b4ebce89316f0935d7ee0d323e06c5d2ad003f8
Antonella Sgarlatta <anto@standardnotes.com>
2026-05-18 23:59:23 -0300

    fix: Fixes Super pdf export code block formatting issues

--- packages/web/src/javascripts/Components/SuperEditor/Lexical/Utils/PDFExport/PDFLayoutConstants.ts
+++ packages/web/src/javascripts/Components/SuperEditor/Lexical/Utils/PDFExport/PDFLayoutConstants.ts
@@ -1,5 +1,7 @@
 export const PDF_BASE_FONT_SIZE = 12
 export const PDF_LINE_HEIGHT_MULTIPLIER = 1.5
+export const PDF_CODE_BLOCK_FONT_SIZE = 11
+export const PDF_CODE_TAB_SIZE = 2
 export const PDF_BLOCK_GAP = 4
 export const PDF_LIST_ITEM_GAP = 0
 export const PDF_QUOTE_INNER_GAP = 4
