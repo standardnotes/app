c2b8b32572aaba52c40dec805d9eb29a701aa59c
Antonella Sgarlatta <anto@standardnotes.com>
2026-08-05 15:31:22 -0300

    chore: fix lint errors [skip ci]

--- packages/web/src/javascripts/Components/ApplicationView/DotOrgNotice.tsx
+++ packages/web/src/javascripts/Components/ApplicationView/DotOrgNotice.tsx
@@ -6,9 +6,7 @@ const DotOrgNotice: FunctionComponent = () => {
   return (
     <div className="z-modal flex h-30 w-full items-center bg-danger text-center text-info-contrast">
       <div className="w-full text-center text-xl font-bold">
-        {jtString(
-          c('B2.NavSharedUI.Warning').jt`${LegacyAppHost} is no longer maintained. Please switch to`,
-        )}{' '}
+        {jtString(c('B2.NavSharedUI.Warning').jt`${LegacyAppHost} is no longer maintained. Please switch to`)}{' '}
         <a className="underline" href={AppUrl}>
           {AppHost}
         </a>
