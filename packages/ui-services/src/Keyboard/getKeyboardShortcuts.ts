be3dbbdbbd1b37f33d31f558dca5d26da6850d6a
Antonella Sgarlatta <anto@standardnotes.com>
2026-09-10 19:04:13 -0300

    fix: Fixes command palette keyboard shortcut for AZERTY

--- packages/ui-services/src/Keyboard/getKeyboardShortcuts.ts
+++ packages/ui-services/src/Keyboard/getKeyboardShortcuts.ts
@@ -233,8 +233,9 @@ export function getKeyboardShortcuts(platform: Platform, _environment: Environme
     },
     {
       command: TOGGLE_COMMAND_PALETTE,
-      key: ':',
+      code: 'Semicolon',
       modifiers: [primaryModifier, KeyboardModifier.Shift],
+      preventDefault: true,
     },
   ]
 }
