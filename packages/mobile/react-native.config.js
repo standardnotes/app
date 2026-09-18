67069dc558b34a384861d3168c53c11ffb4965d0
Antonella Sgarlatta <anto@standardnotes.com>
2026-09-18 12:03:46 -0300

    chore: exclude iap library in android

--- packages/mobile/react-native.config.js
+++ packages/mobile/react-native.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  dependencies: {
+    'react-native-iap': {
+      platforms: {
+        // IAP is iOS-only (Apple StoreKit). Excluding Android avoids bundling
+        // Google Play Billing Library, which triggers Play Console policy checks.
+        android: null,
+      },
+    },
+  },
   project: {
     ios: {
       automaticPodsInstallation: true,
